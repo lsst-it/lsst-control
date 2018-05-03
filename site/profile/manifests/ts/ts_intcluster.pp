650cd28539f986b2b18bfb1e84a07e7017e24e73
Andres Villalobose <aevr007@gmail.com>
2018-05-03 12:24:56 -0300

    Adding virtualbox module support for the ts-intcluster

--- site/profile/manifests/ts/ts_intcluster.pp
+++ site/profile/manifests/ts/ts_intcluster.pp
@@ -1,14 +1,36 @@
 class profile::ts::ts_intcluster {
 	include profile::it::ssh_server
-	
+	include virtualbox
+	/*
 	package{"virtualbox":
 		ensure => installed,
 		source => "https://download.virtualbox.org/virtualbox/5.2.10/VirtualBox-5.2-5.2.10_122088_el7-1.x86_64.rpm",
 		provider => rpm
 	}
+	*/
 	package{"vagrant":
 		ensure => installed,
 		source => "https://releases.hashicorp.com/vagrant/2.0.4/vagrant_2.0.4_x86_64.rpm",
 		provider => rpm
 	}
+	
+	package{"kernel-devel":
+		ensure => installed
+	}
+	
+	package{"kernel-devel-3.10.0-693.el7.x86_64":
+		ensure => installed
+	}
+	
+	package{"xauth":
+		ensure => installed
+	}
+	
+	/*
+	exec{"execute vboxconfig":
+		command => "/sbin/vboxconfig",
+		require => [Package["kernel-devel"] , Package["kernel-devel-3.10.0-693.el7.x86_64"]]
+	}
+	*/
+
 }

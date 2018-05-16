ec63b6ef42b5759303439b3abf54d51ae79e6acb
Andres Villalobose <aevr007@gmail.com>
2018-05-16 17:54:09 -0400

    Missing test command on onlyif condition concatenation

--- site/profile/manifests/ts/ts_dome.pp
+++ site/profile/manifests/ts/ts_dome.pp
@@ -14,7 +14,7 @@ class profile::ts::ts_dome{
 			path => ["/bin","/usr/sbin", "/usr/bin"],
 			command => "mkdir /tmp/lvruntime2015 && /usr/bin/wget http://download.ni.com/support/softlib/labview/labview_runtime/2015/Linux/f3/LabVIEW2015RTE_Linux.tgz -O /tmp/lvruntime2015/LabVIEW2015RTE_Linux.tgz",
 			notify => Exec["install_labview"],
-			onlyif => "test ! -d /tmp/lvruntime2015 && ! -d /etc/natinst/ && -d /usr/local/natinst/"
+			onlyif => "test ! -d /tmp/lvruntime2015 && test ! -d /etc/natinst/ && test ! -d /usr/local/natinst/"
 		}
 		
 	}elsif $labview_version == "2016"{
@@ -22,7 +22,7 @@ class profile::ts::ts_dome{
 			path => ["/bin","/usr/sbin"],
 			command => "mkdir /tmp/lvruntime2016 && /usr/bin/wget http://download.ni.com/support/softlib/labview/labview_runtime/2016/Linux/f5/64-bit/LabVIEW2016RTE_Linux.tgz -O /tmp/lvruntime2016/LabVIEW2016RTE_Linux.tgz",
 			notify => Exec["install_labview"],
-			onlyif => "test ! -d /tmp/lvruntime2016 && ! -d /etc/natinst/ && -d /usr/local/natinst/"
+			onlyif => "test ! -d /tmp/lvruntime2016 && test ! -d /etc/natinst/ && test ! -d /usr/local/natinst/"
 		}
 	}
 	
@@ -30,7 +30,7 @@ class profile::ts::ts_dome{
 		path => ["/bin","/usr/sbin", "/usr/bin"],
 		command => "cd /tmp/lvruntime${$labview_version}/ && tar -xvzf LabVIEW${$labview_version}RTE_Linux.tgz && ./INSTALL",
 		require => Exec["get_labview"],
-		onlyif => "test ! -f /tmp/lvruntime${$labview_version}/LabVIEW${$labview_version}RTE_Linux.tgz && ! -d /etc/natinst/ && -d /usr/local/natinst/"
+		onlyif => "test ! -f /tmp/lvruntime${$labview_version}/LabVIEW${$labview_version}RTE_Linux.tgz && test ! -d /etc/natinst/ && test -d /usr/local/natinst/"
 	}
 	
 }

29917e2309bbaf3c65ea62b9a922293eb9436943
Andres Villalobose <aevr007@gmail.com>
2018-08-31 16:56:58 -0300

    Remove firewall configuration from static config to Hiera

--- site/profile/manifests/default.pp
+++ site/profile/manifests/default.pp
@@ -105,23 +105,7 @@ class profile::default {
 		service_ensure => lookup("firewalld_status"),
 		default_zone => $lsst_firewall_default_zone,
 	}
-	
-	firewalld_zone { $lsst_firewall_default_zone:
-		ensure => present,
-		target => lookup("lsst_firewall_default_target"),
-		sources => lookup("lsst_firewall_default_sources")
-	}
-	
-	firewalld_service { 'Enable SSH':
-		ensure  => 'present',
-		service => 'ssh',
-	}
 
-	firewalld_service { 'Enable DHCP':
-		ensure  => 'present',
-		service => 'dhcpv6-client',
-	}
-	
 	exec{"enable_icmp":
 		provider => "shell",
 		command => "/usr/bin/firewall-cmd --add-protocol=icmp --permanent && /usr/bin/firewall-cmd --reload",

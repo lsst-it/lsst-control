e7f15fb4547e471b9ac3e992ca923f751b3fa477
Andres Villalobose <aevr007@gmail.com>
2018-11-29 13:37:12 -0300

    Moving the nodes classification into a puppet ENC

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,9 +1,10 @@
-#TODO This have to be changed by an ENC script
-
+# This is the very default configuration, in case no match found in ENC
 node default {
 	include role::default
 }
 
+# There is one situation, when deploying a new puppet master, in where the ENC doesn't exists in the puppet environment
+# that's why, puppet-master is the only definition here.
 node /puppet-master/ {
 	include role::it::puppet_master
 }

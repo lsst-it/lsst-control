29c8e9c283c0b68018205a45bd861ea0a488964a
Andres Villalobose <aevr007@gmail.com>
2018-11-29 16:44:36 -0300

    Removed extra profiles and adding only the specific one

--- site/role/manifests/ccs/ats_ccs_default.pp
+++ site/role/manifests/ccs/ats_ccs_default.pp
@@ -1,5 +1,5 @@
 class role::ccs::ats_ccs_default {
-	include profile::default
-	include profile::it::ssh_server
+	#include profile::default
+	#include profile::it::ssh_server
 	include profile::ccs::ccs
 }
--- site/role/manifests/ccs/ats_shutter_hcu.pp
+++ site/role/manifests/ccs/ats_shutter_hcu.pp
@@ -1,5 +1,5 @@
 class role::ccs::ats_shutter_hcu {
-	include profile::default
-	include profile::it::ssh_server
+	#include profile::default
+	#include profile::it::ssh_server
 	include profile::ccs::ccs
 }

c2a8962151b982534d6b1543a208f25340ac73b3
Andres Villalobose <aevr007@gmail.com>
2018-11-30 09:09:53 -0300

    Moving to monitoring block from default to monitoring script

--- site/profile/manifests/default.pp
+++ site/profile/manifests/default.pp
@@ -4,37 +4,6 @@ class profile::default {
  	include profile::it::ssh_server
  	# All telegraf configuration came from Hiera
  	
- 	if lookup("monitoring_enabled"){
-		include telegraf
-		#TODO Until telegraf's puppet module doesn't allow multiple procstat definitions from hiera, a fixed script/variable is defined to configure the required services to be monitored. 
-		$monitored_services = lookup("monitored_services")
-		$monitored_services.each | $service | {
-			telegraf::input { $service:
-				plugin_type => 'procstat',
-				options =>
-					{
-						'systemd_unit' => "${service}.service",
-					},
-			}
-		}
-		#Remote Logging
-		class{'rsyslog::client':
-			log_local => true,
-			remote_servers => [
-				{
-					host => lookup("rsyslog_host"),
-					port => lookup("rsyslog_port"),
-					protocol  => lookup("rsyslog_proto"),
-					pattern => lookup("rsyslog_patterns")
-				},
-			],
-			imfiles => lookup("rsyslog::imfiles", {default_value => undef})
-		}
-	}else{
-		service{"telegraf":
-			ensure => stopped,
-		}
-	}
 	package { 'nmap':
 		ensure => installed,
 	}
@@ -102,22 +71,6 @@ class profile::default {
 	
 	package{ "sssd-krb5":
 		ensure => installed
-	}
-
-	if ! $is_virtual {
-		package { 'smartmontools':
-			ensure => installed,
-		}
-		package { 'lm_sensors':
-			ensure => installed,
-		}
-		file_line { 'Adding smartcl permissions to sudoers':
-			ensure => present,
-			path  => '/etc/sudoers',
-			line => 'telegraf  ALL= NOPASSWD: /usr/sbin/smartctl',
-			match => '^telegraf',
-			require => Package["sudo"]
-		}
 	}
 
 # Firewall and security measurements

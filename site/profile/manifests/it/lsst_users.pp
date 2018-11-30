060be279986f4a3989528f35a2ee647be8c4fda4
Andres Villalobose <aevr007@gmail.com>
2018-11-30 09:40:14 -0300

    Isolating user's configuration from default profile

---
+++ site/profile/manifests/it/lsst_users.pp
@@ -0,0 +1,41 @@
+class profile::it::lsst_users {
+ # group/user creation
+
+	# as per /etc/login.defs, max uid is 999, so we have set 777 as the default group admin account
+	group { 'sysadmin':
+		ensure => present,
+		gid => 777,
+		auth_membership => true,
+		members => ['sysadmin']
+	}
+
+	#current user for sudo access is wheel, in centos 7 it has GID=10
+	group { 'wheel':
+		ensure => present,
+		gid => 10,
+		auth_membership => true,
+		members => ['sysadmin'],
+		require => Group['sysadmin'],
+	}
+
+	# as per /etc/login.defs, max uid is 999, so we have set 777 as the default group admin account
+	user{ 'sysadmin':
+		ensure => 'present',
+		uid => '777' ,
+		gid => '777',
+		home => '/home/sysadmin',
+		managehome => true,
+		password => lookup("lsst_sysadmin_pwd")
+	}
+
+	file{ '/home/sysadmin':
+		ensure => directory,
+		mode => '700',
+		require => User['sysadmin'],
+	}
+
+	user{'root':
+		password => lookup("root_pwd"),
+	}
+ 
+}

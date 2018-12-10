6635eb86962903d734036441832d14094be83770
Andres Villalobose <aevr007@gmail.com>
2018-12-10 16:09:13 -0300

    Improving and grouping systemd units

--- site/profile/manifests/ts/efd/ts_efd_writers.pp
+++ site/profile/manifests/ts/efd/ts_efd_writers.pp
@@ -51,13 +51,20 @@ class profile::ts::efd::ts_efd_writers {
 	$ts_xml_subsystems.each | String $subsystem | {
 		$ts_efd_writers.each | String $writer | {
 			file { "/etc/systemd/system/${subsystem}_${writer}_efdwriter.service":
+        ensure => present,
 				mode    => '0644',
 				owner   => 'root',
 				group   => 'root',
 				content => epp('profile/ts/tsSystemdUnitTemplate.epp', 
-					{ 'serviceDescription' => "EFD ${subsystem} ${writer} writer",
-					  'startDemoPath' => "${ts_sal_path}/${ts_xml_build_dir}/${subsystem}/cpp/src" , 
-					  'serviceCommand' => "/bin/bash -c 'source ${ts_sal_path}/setup.env && ./sacpp_${subsystem}_${writer}_efdwriter'"}),
+					{ 'serviceDescription' => "EFD - ${subsystem} ${writer} writer",
+					  'startPath' => "${ts_sal_path}/${ts_xml_build_dir}/${subsystem}/cpp/src" , 
+					  'serviceCommand' => "/bin/bash -c 'source ${ts_sal_path}/setup.env && ./sacpp_${subsystem}_${writer}_efdwriter'",
+            'wantedBy' => "${subsystem}_efdwriter.service",
+            'partOf' => "${subsystem}_efdwriter.service",
+            'after' => "${subsystem}_efdwriter.service"
+          }
+        ),
+        before => File["/etc/systemd/system/${subsystem}_efdwriter.service"]
 			}
 			service { "${subsystem}_${writer}_efdwriter":
 				ensure => running,
@@ -68,6 +75,63 @@ class profile::ts::efd::ts_efd_writers {
 		
 		}
 	}
+  $ts_xml_subsystems.each | String $subsystem | {
+    file{ "/etc/systemd/system/${subsystem}_efdwriter.service":
+      ensure => present,
+      mode => '0644',
+      owner   => 'root',
+      group   => 'root',
+      notify => Exec["Create $subsystem general systemd unit"],
+      content => epp('profile/ts/tsSystemdUnitTemplate.epp', 
+        { 'serviceDescription' => "EFD - ${subsystem} efdwriter control unit",
+          'serviceCommand' => "/bin/true",
+          'wantedBy' => "efdwriters.service",
+          'partOf' => "efdwriters.service",
+          'after' => "efdwriters.service",
+          'systemdUnitType' => "oneshot" 
+        }
+      ),
+      before => File["/etc/systemd/system/efdwriters.service"]
+    } ~> 
+      exec{ "Create $subsystem general systemd unit":
+        path  => [ '/usr/bin', '/bin', '/usr/sbin' , '/usr/local/bin'], 
+        cwd => "/etc/systemd/system/",
+        command => "ls -x ${subsystem}_* | xargs -I X sed -i \"s/Wants=.*/Wants=X/\" ${subsystem}_efdwriter.service",
+        #onlyif => "test ! -f /etc/systemd/system/${subsystem}_efdwriter.service",
+        require => File["/etc/systemd/system/${subsystem}_efdwriter.service"],
+        refreshonly => true,
+        notify => Exec["Systemd daemon reload"]
+      }
+  }
+
+  file{"/etc/systemd/system/efdwriters.service":
+    ensure => present,
+    mode => '0644',
+    owner   => 'root',
+    group   => 'root',
+    notify => Exec["Generic EFD Writers systemd unit"],
+    content => epp('profile/ts/tsSystemdUnitTemplate.epp', 
+      { 'serviceDescription' => "EFD - efdwriter control unit",
+        'serviceCommand' => "/bin/true",
+        'systemdUnitType' => "oneshot" 
+      }
+    ),
+  } ~> 
+  exec{ "Generic EFD Writers systemd unit":
+    path  => [ '/usr/bin', '/bin', '/usr/sbin' , '/usr/local/bin'], 
+    cwd => "/etc/systemd/system/",
+    command => "find ./ -maxdepth 1 -regex './[a-zA-Z]*_efdwriter.service' -printf \"%f \" | xargs -I X sed -i \"s/Wants=.*/Wants=X/\" efdwriters.service",
+    #onlyif => "test ! -f /etc/systemd/system/efdwriter.service",
+    require => File["/etc/systemd/system/efdwriters.service"],
+    refreshonly => true,
+    notify => Exec["Systemd daemon reload"]
+  }
+
+  exec{"Systemd daemon reload":
+    path  => [ '/usr/bin', '/bin', '/usr/sbin' , '/usr/local/bin'], 
+    command => "systemctl daemon-reload",
+    refreshonly => true
+  }
 	
 	file_line{ "Add LSST_EFD_HOST variable" :
 		path => "${ts_sal_path}/setup.env",

a7790623c9cbed566f36744af499061a567be0b7
Andres Villalobose <aevr007@gmail.com>
2019-01-09 09:44:00 -0300

    Adding backup outputs using output class.

--- site/profile/manifests/it/monitoring.pp
+++ site/profile/manifests/it/monitoring.pp
@@ -1,6 +1,15 @@
 class profile::it::monitoring {
  	if lookup("monitoring_enabled"){
 		include telegraf
+		$backup_outputs = lookup("backup_outputs", {default_value => undef})
+		if $backup_outputs {
+			$backup_outputs.each | $output | {
+				telegraf::output{ $output["name"]:
+					plugin_type => $output["plugintype"],
+					options  => $output["options"]
+				}
+			}
+		}
 		#TODO Until telegraf's puppet module doesn't allow multiple procstat definitions from hiera, a fixed script/variable is defined to configure the required services to be monitored. 
 		$monitored_services = lookup("monitored_services")
 		$monitored_services.each | $service | {

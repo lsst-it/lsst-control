4498f1c3c9b448cc967621060ffce5e26cb04d86
Andres Villalobose <aevr007@gmail.com>
2019-03-17 01:01:21 -0300

    Puppet code best practices

--- site/profile/manifests/it/gs_dhcp.pp
+++ site/profile/manifests/it/gs_dhcp.pp
@@ -1,16 +1,17 @@
+# This class deploy a DHCP server, configuration comes from hiera.
 class profile::it::gs_dhcp{
-	class { 'dhcp':
-		service_ensure => running,
-		dnsdomain      => lookup("dnsdomain"),
-		dnssearchdomains => lookup("dnssearchdomains"),
-		nameservers  => lookup("nameservers"),
-		ntpservers   => lookup("ntpservers"),
-		interfaces   => lookup("dhcp_interfaces"), # This interface is later configured in /etc/systemd/system/dhcpd.service
-		omapi_port   => 7911,
-		extra_config => [lookup("failover_configuration")],
-		dhcp_conf_extra => "INTERNAL_TEMPLATE",
-		pools => lookup("dhcp_pools"),
-		ignoredsubnets => lookup("ignored_subnets"),
-		hosts => lookup({"name" => "dhcp_hosts", "default_value" => {}})
-	}
+  class { 'dhcp':
+    service_ensure   => running,
+    dnsdomain        => lookup('dnsdomain'),
+    dnssearchdomains => lookup('dnssearchdomains'),
+    nameservers      => lookup('nameservers'),
+    ntpservers       => lookup('ntpservers'),
+    interfaces       => lookup('dhcp_interfaces'), # This interface is later configured in /etc/systemd/system/dhcpd.service
+    omapi_port       => 7911,
+    extra_config     => [lookup('failover_configuration')],
+    dhcp_conf_extra  => 'INTERNAL_TEMPLATE',
+    pools            => lookup('dhcp_pools'),
+    ignoredsubnets   => lookup('ignored_subnets'),
+    hosts            => lookup({'name' => 'dhcp_hosts', 'default_value' => {}})
+  }
 }

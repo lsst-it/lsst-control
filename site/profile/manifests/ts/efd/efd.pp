f00d37704bc5b22f917147a836cfff1a351e8220
Andres Villalobose <aevr007@gmail.com>
2019-03-17 01:05:14 -0300

    Puppet code best practices

--- site/profile/manifests/ts/efd/efd.pp
+++ site/profile/manifests/ts/efd/efd.pp
@@ -1,93 +1,96 @@
+# EFD based on mysql server
 class profile::ts::efd::efd{
 
-	$efd_user = lookup("ts::efd::user")
-	$efd_user_pwd = lookup("ts::efd::user_pwd")
-	$ts_sal_path = lookup("ts_sal::ts_sal_path")
-	$ts_xml_subsystems = lookup("ts::efd::ts_xml_subsystems")
-	$ts_efd_writers = lookup("ts::efd::ts_efd_writers")
-	#Parameters for SAL module comes from HIERA
-	include ts_sal
+  $efd_user = lookup('ts::efd::user')
+  $efd_user_pwd = lookup('ts::efd::user_pwd')
+  $ts_sal_path = lookup('ts_sal::ts_sal_path')
+  $ts_xml_subsystems = lookup('ts::efd::ts_xml_subsystems')
+  $ts_efd_writers = lookup('ts::efd::ts_efd_writers')
+  #Parameters for SAL module comes from HIERA
+  include ts_sal
 
-	class{"ts_xml":
-		ts_xml_path => lookup("ts_xml::ts_xml_path"),
-		ts_xml_subsystems => $ts_xml_subsystems,
-		ts_xml_languages => lookup("ts::efd::ts_xml_languages"),
-		ts_sal_path => $ts_sal_path,
-		before => Exec["gengenericefd"]
-	}
+  class{'ts_xml':
+    ts_xml_path       => lookup('ts_xml::ts_xml_path'),
+    ts_xml_subsystems => $ts_xml_subsystems,
+    ts_xml_languages  => lookup('ts::efd::ts_xml_languages'),
+    ts_sal_path       => $ts_sal_path,
+    before            => Exec['gengenericefd']
+  }
 
-	exec{ "gengenericefd" :
-		user => "salmgr",
-		group => "lsst",
-		path => '/bin:/usr/bin:/usr/sbin',
-		command => "/bin/bash -c 'source ${ts_sal_path}/setup.env ; echo \"source ${ts_sal_path}/lsstsal/scripts/gengenericefd.tcl ; updateefdschema\" | tclsh'",
-		require => Class["ts_xml"],
-		onlyif => "test $(find ${ts_sal_path}/test/ -name sacpp_*efdwriter | wc -l) -eq 0"
-	}
+  exec{ 'gengenericefd' :
+    user    => 'salmgr',
+    group   => 'lsst',
+    path    => '/bin:/usr/bin:/usr/sbin',
+    command => "/bin/bash -c 'source ${ts_sal_path}/setup.env ; \
+                echo \"source ${ts_sal_path}/lsstsal/scripts/gengenericefd.tcl ; \
+                updateefdschema\" | tclsh'",
+    require => Class['ts_xml'],
+    onlyif  => "test $(find ${ts_sal_path}/test/ -name sacpp_*efdwriter | wc -l) -eq 0"
+  }
 
-	package { 'mariadb':
-		ensure => installed,
-	}
+  package { 'mariadb':
+    ensure => installed,
+  }
 
-	package { 'mariadb-server':
-		ensure => installed,
-	}
+  package { 'mariadb-server':
+    ensure => installed,
+  }
 
-	file{ "/etc/my.cnf.d/efd.cnf" :
-		ensure => present,
-		content => "[mysql]\nuser=${efd_user}\npassword=${efd_user_pwd}\n",
-		require => [Package["mariadb"], Package["mariadb-server"]]
-	}
+  file{ '/etc/my.cnf.d/efd.cnf' :
+    ensure  => present,
+    content => "[mysql]\nuser=${efd_user}\npassword=${efd_user_pwd}\n",
+    require => [Package['mariadb'], Package['mariadb-server']]
+  }
 
-	package { 'mariadb-devel':
-		ensure => installed,
-	}
+  package { 'mariadb-devel':
+    ensure => installed,
+  }
 
-	# Services definition
+  # Services definition
 
-	# The service must start just after the schema is downloaded.
-	service { 'mariadb':
-		ensure => running,
-		enable => true,
-		require => [File["/etc/my.cnf.d/efd.cnf"], Exec["schema_download"]]
-	}
-	firewalld_service { 'Allow mysql port on firewalld':
-		ensure  => 'present',
-		service => 'mysql',
-	}
-	
-	#Creates a unit file with the efd writers for each subsystem
-	$ts_xml_subsystems.each | String $subsystem | {
-		$ts_efd_writers.each | String $writer | {
-			file { "/etc/systemd/system/${subsystem}_${writer}_efdwriter.service":
-				mode    => '0644',
-				owner   => 'root',
-				group   => 'root',
-				content => epp('profile/ts/tsSystemdUnitTemplate.epp', 
-					{ 'serviceDescription' => "EFD ${subsystem} ${writer} writer",
-					  'startDemoPath' => "${ts_sal_path}/test/${subsystem}/cpp/src" , 
-					  'serviceCommand' => "/bin/bash -c 'source ${ts_sal_path}/setup.env && ./sacpp_${subsystem}_${writer}_efdwriter'"}),
-			}
-			service { "${subsystem}_${writer}_efdwriter":
-				ensure => running,
-				enable => true,
-				require => [File["/etc/systemd/system/${subsystem}_${writer}_efdwriter.service"], Service["mariadb"]]
-			}
-		
-		}
-	}
+  # The service must start just after the schema is downloaded.
+  service { 'mariadb':
+    ensure  => running,
+    enable  => true,
+    require => [File['/etc/my.cnf.d/efd.cnf'], Exec['schema_download']]
+  }
+  firewalld_service { 'Allow mysql port on firewalld':
+    ensure  => 'present',
+    service => 'mysql',
+  }
 
-	# Download DB schema
-	exec { 'schema_download':
-		path    => '/usr/bin:/usr/sbin',
-		command => 'cd /var/lib/mysql/ ; wget ftp://ftp.noao.edu/pub/dmills/efd-bootstrap.tgz ; tar xvzpPf efd-bootstrap.tgz',
-		timeout => 0,
-		onlyif => "test ! -f /var/lib/mysql/efd-bootstrap.tgz"
-	}
-	
-	
-	file_line{ "Add LSST_EFD_HOST variable" :
-		path => "${ts_sal_path}/setup.env",
-		line => "export LSST_EFD_HOST=localhost",
-	}
+  #Creates a unit file with the efd writers for each subsystem
+  $ts_xml_subsystems.each | String $subsystem | {
+    $ts_efd_writers.each | String $writer | {
+      file { "/etc/systemd/system/${subsystem}_${writer}_efdwriter.service":
+        mode    => '0644',
+        owner   => 'root',
+        group   => 'root',
+        content => epp('profile/ts/tsSystemdUnitTemplate.epp',
+          { 'serviceDescription' => "EFD ${subsystem} ${writer} writer",
+            'startDemoPath'      => "${ts_sal_path}/test/${subsystem}/cpp/src" ,
+            'serviceCommand'     => "/bin/bash -c 'source ${ts_sal_path}/setup.env && ./sacpp_${subsystem}_${writer}_efdwriter'"}),
+      }
+      service { "${subsystem}_${writer}_efdwriter":
+        ensure  => running,
+        enable  => true,
+        require => [File["/etc/systemd/system/${subsystem}_${writer}_efdwriter.service"], Service['mariadb']]
+      }
+
+    }
+  }
+
+  # Download DB schema
+  exec { 'schema_download':
+    path    => '/usr/bin:/usr/sbin',
+    command => 'cd /var/lib/mysql/ ; wget ftp://ftp.noao.edu/pub/dmills/efd-bootstrap.tgz ; tar xvzpPf efd-bootstrap.tgz',
+    timeout => 0,
+    onlyif  => 'test ! -f /var/lib/mysql/efd-bootstrap.tgz'
+  }
+
+
+  file_line{ 'Add LSST_EFD_HOST variable' :
+    path => "${ts_sal_path}/setup.env",
+    line => 'export LSST_EFD_HOST=localhost',
+  }
 }

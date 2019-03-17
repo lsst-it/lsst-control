7bf322a1c8997a0a8a4f04039f0b6f4c68850a50
Andres Villalobose <aevr007@gmail.com>
2019-03-17 01:02:28 -0300

    Puppet code best practices

--- site/profile/manifests/it/lsst_users.pp
+++ site/profile/manifests/it/lsst_users.pp
@@ -1,48 +1,49 @@
+# Class responsible for the LSST Users creation
 class profile::it::lsst_users {
- # group/user creation
+  # group/user creation
 
-	# as per /etc/login.defs, max uid is 999, so we have set 777 as the default group admin account
-	group { 'sysadmin':
-		ensure => present,
-		gid => 777,
-		auth_membership => true,
-		members => ['sysadmin']
-	}
+  # as per /etc/login.defs, max uid is 999, so we have set 777 as the default group admin account
+  group { 'sysadmin':
+    ensure          => present,
+    gid             => 777,
+    auth_membership => true,
+    members         => ['sysadmin']
+  }
 
-	#current user for sudo access is wheel, in centos 7 it has GID=10
-	group { 'wheel':
-		ensure => present,
-		gid => 10,
-		auth_membership => true,
-		members => ['sysadmin'],
-		require => Group['sysadmin'],
-	}
+  #current user for sudo access is wheel, in centos 7 it has GID=10
+  group { 'wheel':
+    ensure          => present,
+    gid             => 10,
+    auth_membership => true,
+    members         => ['sysadmin'],
+    require         => Group['sysadmin'],
+  }
 
-	# as per /etc/login.defs, max uid is 999, so we have set 777 as the default group admin account
-	user{ 'sysadmin':
-		ensure => 'present',
-		uid => '777' ,
-		gid => '777',
-		home => '/home/sysadmin',
-		managehome => true,
-		password => lookup("lsst_sysadmin_pwd")
-	}
+  # as per /etc/login.defs, max uid is 999, so we have set 777 as the default group admin account
+  user{ 'sysadmin':
+    ensure     => 'present',
+    uid        => '777' ,
+    gid        => '777',
+    home       => '/home/sysadmin',
+    managehome => true,
+    password   => lookup('lsst_sysadmin_pwd')
+  }
 
-	file{ '/home/sysadmin':
-		ensure => directory,
-		mode => '700',
-		require => User['sysadmin'],
-	}
+  file{ '/home/sysadmin':
+    ensure  => directory,
+    mode    => '0700',
+    require => User['sysadmin'],
+  }
 
-	user{'root':
-		password => lookup("root_pwd"),
-	}
- 
-	group { 'lsst':
-		ensure => present,
-		gid => 500,
-		auth_membership => true,
-		members => ['sysadmin'],
-	}
+  user{'root':
+    password => lookup('root_pwd'),
+  }
+
+  group { 'lsst':
+    ensure          => present,
+    gid             => 500,
+    auth_membership => true,
+    members         => ['sysadmin'],
+  }
 
 }

6bb2139922398b6419a8387e0fd28e574c3cee7c
Joshua Hoblitt <josh@hoblitt.com>
2019-07-25 16:48:46 -0700

    add support for installing files from hiera

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,1 +1,11 @@
 lookup('classes', Array[String], 'unique').include
+
+$files = lookup(
+  name          => 'files',
+  value_type    => Variant[Hash[String, Hash], Undef],
+  default_value => undef,
+)
+
+if ($files) {
+  ensure_resources('file', $files)
+}

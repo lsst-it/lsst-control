f6f8ca95f47f161cf5da585549858199b684d548
Joshua Hoblitt <josh@hoblitt.com>
2019-07-25 10:25:04 -0700

    add comcam cc_{camera,daq,diag} profiles

---
+++ site/profile/manifests/comcam/cc_daq.pp
@@ -0,0 +1,2 @@
+class profile::comcam::cc_daq {
+}
---
+++ site/profile/manifests/comcam/common.pp
@@ -0,0 +1,30 @@
+class profile::comcam::common {
+  include timezone
+  include tuned
+  include chrony
+  include selinux
+  include firewall
+  include irqbalance
+  include sysstat
+  include epel
+  include sudo
+  include accounts
+
+  # XXX write some glue code and move this to hiera -JCH
+  sshd_config { "PermitRootLogin":
+    ensure => present,
+    value  => "without-password",
+  }
+  sshd_config { "PasswordAuthentication":
+    ensure => present,
+    value  => "no",
+  }
+  sshd_config { "PrintMotd":
+    ensure => present,
+    value  => "no",
+  }
+  sshd_config { "UseDNS":
+    ensure => present,
+    value  => "no",
+  }
+}

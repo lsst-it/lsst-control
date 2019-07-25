29feeae1678ca486df87dfb7df9162c054112b55
Joshua Hoblitt <josh@hoblitt.com>
2019-07-25 16:12:52 -0700

    migrate from ghoneycutt/ssh -> saz/ssh
    saz/ssh supports configuring sshd_config with hash key names that are in the
    same camelcase as the literal conf file. This is much less error prone.

--- site/profile/manifests/comcam/common.pp
+++ site/profile/manifests/comcam/common.pp
@@ -11,22 +11,5 @@ class profile::comcam::common {
   include accounts
   include puppet_agent
   include resolv_conf
-
-  # XXX write some glue code and move this to hiera -JCH
-  sshd_config { "PermitRootLogin":
-    ensure => present,
-    value  => "without-password",
-  }
-  sshd_config { "PasswordAuthentication":
-    ensure => present,
-    value  => "no",
-  }
-  sshd_config { "PrintMotd":
-    ensure => present,
-    value  => "no",
-  }
-  sshd_config { "UseDNS":
-    ensure => present,
-    value  => "no",
-  }
+  include ssh
 }

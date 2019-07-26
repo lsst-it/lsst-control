3a5a06058c7a3d2d4a70eb45b0253969bb4262a0
Joshua Hoblitt <josh@hoblitt.com>
2019-07-26 14:27:20 -0700

    enable r10k webhook

--- site/profile/manifests/comcam/cc_daq.pp
+++ site/profile/manifests/comcam/cc_daq.pp
@@ -1,4 +1,8 @@
 class profile::comcam::cc_daq {
   include cron
   include r10k
+  include r10k::webhook
+  include r10k::webhook::config
+
+  Class['r10k::webhook::config'] -> Class['r10k::webhook']
 }

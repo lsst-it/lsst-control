5fd089c93250d716d612903f3099a480d5fd4d1b
Joshua Hoblitt <josh@hoblitt.com>
2019-09-30 15:31:37 -0700

    add profile::core::puppet_master
    cp of profile::comcam::puppet_master

---
+++ site/profile/manifests/core/puppet_master.pp
@@ -0,0 +1,56 @@
+class profile::core::puppet_master {
+  include cron
+  include r10k
+  include r10k::webhook
+  include r10k::webhook::config
+  include scl
+
+  Class['r10k::webhook::config'] -> Class['r10k::webhook']
+
+  $node_pkgs = [
+    'rh-nodejs10',
+    'rh-nodejs10-npm',
+  ]
+
+  package { $node_pkgs:
+    require => Class['scl']
+  }
+
+  exec { 'install-smee':
+    creates   => '/opt/rh/rh-nodejs10/root/usr/bin/smee',
+    command   => 'npm install --global smee-client',
+    subscribe => Package['rh-nodejs10-npm'],
+    path      => [
+      '/opt/rh/rh-nodejs10/root/usr/bin',
+      '/usr/sbin',
+      '/usr/bin',
+    ],
+  }
+
+  $service_unit = @(EOT)
+    [Unit]
+    Description=smee.io webhook daemon
+
+    [Service]
+    Type=simple
+    ExecStart=/usr/bin/scl enable rh-nodejs10 -- \
+      /opt/rh/rh-nodejs10/root/usr/bin/smee \
+      --url https://smee.io/1qBdVgdiCC9jFw3g \
+      -P /payload \
+      -p 8088
+    Restart=on-failure
+    RestartSec=10
+
+    [Install]
+    WantedBy=default.target
+    | EOT
+
+  systemd::unit_file { 'smee.service':
+    content => $service_unit,
+  }
+  ~> service { 'smee':
+    ensure    => 'running',
+    enable    => true,
+    subscribe => Exec['install-smee'],
+  }
+}

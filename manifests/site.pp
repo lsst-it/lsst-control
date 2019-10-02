24e485e1a6fe893ff665105d225198ca49ff7831
Joshua Hoblitt <josh@hoblitt.com>
2019-10-02 12:37:38 -0700

    add support for hiera packages

--- manifests/site.pp
+++ manifests/site.pp
@@ -9,3 +9,14 @@ $files = lookup(
 if ($files) {
   ensure_resources('file', $files)
 }
+
+$packages = lookup(
+  name          => 'packages',
+  value_type    => Variant[Array[String], Undef],
+  merge         => 'unique',
+  default_value => undef,
+)
+
+if ($packages) {
+  ensure_packages($packages)
+}

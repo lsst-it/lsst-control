5c5642800159891315b2e15ddebb771e853c5b45
Joshua Hoblitt <josh@hoblitt.com>
2020-02-06 18:43:03 -0300

    rm installation of packages from puppet code

--- site/profile/manifests/default.pp
+++ site/profile/manifests/default.pp
@@ -5,59 +5,6 @@ class profile::default {
   include profile::it::monitoring
   # All telegraf configuration came from Hiera
 
-  package { 'nmap':
-    ensure => installed,
-  }
-
-  package { 'vim':
-    ensure => installed,
-  }
-
-  package { 'wget':
-    ensure => installed,
-  }
-
-  # I would like to confirm if there is any particular version of gcc to be installed
-  package { 'gcc':
-    ensure => installed,
-  }
-
-  package { 'xinetd':
-    ensure => installed,
-  }
-
-  package { 'tcpdump':
-    ensure => installed,
-  }
-
-  package { 'openssl':
-    ensure => installed,
-  }
-
-  package { 'openssl-devel':
-    ensure => installed,
-  }
-
-  package { 'telnet':
-    ensure => installed,
-  }
-
-  package { 'acpid':
-    ensure => installed,
-  }
-
-  package { 'lvm2':
-    ensure => installed,
-  }
-
-  package { 'bash-completion':
-    ensure => installed,
-  }
-
-  package { 'tree':
-    ensure => installed,
-  }
-
 # Firewall and security measurements
 ################################################################################
 
@@ -100,11 +47,5 @@ class profile::default {
   }
 
 ################################################################################
-
-# Shared resources from all the teams
-
-  package { 'git':
-    ensure => present,
-  }
 
 }

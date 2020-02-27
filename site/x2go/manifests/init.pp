db8bd318fcf501b2bc533c8bb0429224c3d23d50
Adrien Thebo <adrien@lagrange-automation.io>
2020-02-27 12:02:36 -0300

    add x2go class

---
+++ site/x2go/manifests/init.pp
@@ -0,0 +1,12 @@
+# x2go
+#
+#
+class x2go {
+  notify { "x2go-header":
+    message => "This is the x2go module"
+  }
+  # install the x2go package
+
+  # configure x2go config file
+  # start the x2go service
+}

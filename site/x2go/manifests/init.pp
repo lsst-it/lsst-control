5086027de3aae6dee2f7a74f8cc21033f0e84f89
Adrien Thebo <adrien@lagrange-automation.io>
2020-02-27 12:02:36 -0300

    add x2go class

---
+++ site/x2go/manifests/init.pp
@@ -0,0 +1,29 @@
+# x2go
+#
+#
+class x2go {
+  notify { "x2go-header":
+    message => "This is the x2go module"
+  }
+
+  # install the x2go package
+  package {"x2goserver":
+    ensure => present,
+
+
+  }
+  # install the x2go package
+  package {"x2goserver-xsession":
+    ensure => present,
+
+
+  }
+  # configure x2go config file
+  # file { "/etc/x2go/not-a-real-file":
+  #   ensure  => present,
+  #   content => "hello!"
+  # }
+  # start the x2go service
+
+  # manage the x2goserver sudo rule
+}

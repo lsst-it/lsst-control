37e3c492475f167f03d42780176280ca02709363
Heinrich Reinking <hreinkinge@gmail.com>
2020-02-28 16:56:21 -0300

    Final x2go setting

--- site/x2go/manifests/init.pp
+++ site/x2go/manifests/init.pp
@@ -1,12 +1,4 @@
-# x2go
-#
-#
-
 class x2go {
-#  notify { "x2go-header":
-#    message => "This is the x2go module"
-#  }
-
   # install the x2go package
   package {"x2goserver":
     ensure => present,
@@ -15,12 +7,4 @@ class x2go {
   package {"x2goserver-xsession":
     ensure => present,
   }
-  # configure x2go config file
-  # file { "/etc/x2go/not-a-real-file":
-  #   ensure  => present,
-  #   content => "hello!"
-  # }
-  # start the x2go service
-
-  # manage the x2goserver sudo rule
 }

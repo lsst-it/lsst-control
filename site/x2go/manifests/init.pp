8ad234399563d984df7b61ddb613ed20f9fc1fdf
Heinrich Reinking <hreinkinge@gmail.com>
2020-02-28 12:47:09 -0300

    Cleanup and reorganize x2goserver module

--- site/x2go/manifests/init.pp
+++ site/x2go/manifests/init.pp
@@ -2,28 +2,13 @@
 #
 #
 class x2go {
-  notify { "x2go-header":
-    message => "This is the x2go module"
-  }
-
   # install the x2go package
-  package {"x2goserver":
+  package {'x2goserver':
     ensure => present,
-
-
   }
   # install the x2go package
-  package {"x2goserver-xsession":
+  package {'x2goserver-xsession':
     ensure => present,
-
-
   }
-  # configure x2go config file
-  # file { "/etc/x2go/not-a-real-file":
-  #   ensure  => present,
-  #   content => "hello!"
-  # }
-  # start the x2go service
-
-  # manage the x2goserver sudo rule
 }
+

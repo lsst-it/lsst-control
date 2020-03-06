af3b708b60e55240a21b41f0b37bfd0e02239b3b
Joshua Hoblitt <josh@hoblitt.com>
2020-03-06 09:22:31 -0700

    add selinux enabled check

--- site/profile/manifests/core/selinux.pp
+++ site/profile/manifests/core/selinux.pp
@@ -1,6 +1,12 @@
 class profile::core::selinux {
+  if ($facts['os']['selinux']['enabled']) {
+    notify { "os.selinux.enabled == ${facts['os']['selinux']['enabled']}":
+      notify => Reboot['selinux'],
+    }
+  }
+
   Class['selinux::config']
-  ~> reboot { 'disable selinux':
+  ~> reboot { 'selinux':
     apply   => finished,
     message => 'disable selinux',
     when    => refreshed,

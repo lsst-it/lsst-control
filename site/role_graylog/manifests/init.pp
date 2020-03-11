edb3a2628d1ecad6f4149d0fa43c36b8bbccb05d
Heinrich Reinking <hreinkinge@gmail.com>
2020-03-11 19:42:15 -0300

    one more....

--- site/role_graylog/manifests/init.pp
+++ site/role_graylog/manifests/init.pp
@@ -1,33 +1,34 @@
 class role_graylog {
   include java
   class { 'mongodb::globals':
-    manage_package_repo => true,
-    version => '4.2.3'
+    manage_package_repo
+                        => true,
+    version             => '4.2.3',
   }
   -> class { 'mongodb::server':
-    bind_ip => ['127.0.0.1'],
+    bind_ip=> ['127.0.0.1'],
   }
   class { 'elasticsearch':
-    version      => '6.6.0',
-    manage_repo  => true,
-    require => Class[
-      '::java',
-    ],
+    version     => '6.6.0',
+    manage_repo => true,
+    require     => Class[
+                    '::java',
+                    ],
   }
   -> elasticsearch::instance { 'graylog':
-    config => {
+    config          => {
       'cluster.name' => 'graylog',
       'network.host' => '127.0.0.1',
     }
   }
   class { 'graylog::repository':
-    version => '3.0'
+    version         => '3.0'
   }
   -> class { 'graylog::server':
     package_version => '3.0.0-12',
-    require => Class[
-      '::java',
-    ],
+    require         => Class[
+                        '::java',
+                        ],
   }
 }
 

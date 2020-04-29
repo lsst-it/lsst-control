2762a5412e5b74a1c9479ce2ae093d10c4cabd09
Joshua Hoblitt <josh@hoblitt.com>
2020-04-29 15:13:09 -0700

    add p::ccs::tomcat to comcam-mcm role

--- site/profile/manifests/ccs/tomcat.pp
+++ site/profile/manifests/ccs/tomcat.pp
@@ -1,11 +1,11 @@
 class profile::ccs::tomcat {
 
-  tomcat::install { '/opt/tomcat':
+  tomcat::install { '/opt/tomcat/apache-tomcat-9.0.33':
     source_url => 'https://www-eu.apache.org/dist/tomcat/tomcat-9/v9.0.33/bin/apache-tomcat-9.0.33.tar.gz',
   }
 
   tomcat::instance { 'latest':
-    catalina_home => '/opt/tomcat',
+    catalina_home => '/opt/tomcat/apache-tomcat-9.0.33',
   }
 
 }

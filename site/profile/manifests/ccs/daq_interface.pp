bfb29cb6302b69b051a43b28688c5ca0a8b2b78b
Joshua Hoblitt <josh@hoblitt.com>
2020-04-29 10:52:34 -0700

    add mode param to profile::ccs::daq_interface

--- site/profile/manifests/ccs/daq_interface.pp
+++ site/profile/manifests/ccs/daq_interface.pp
@@ -1,17 +1,32 @@
+# XXX it might be better to simply set a per host network interface setup
+# instead of resorting to conditional logic in a profile.  The main reason to
+# centralize it is to make it easier to change the rx/tx/etc. settings on all
+# hosts with a DAQ interface.
 class profile::ccs::daq_interface(
   String $hwaddr,
   String $uuid,
+  Enum['dhcp-client', 'dhcp-server'] $mode,
 ) {
+
+  $netconf = $mode ? {
+    'dhcp-client'  => {
+      bootproto => 'dhcp',
+    },
+    'dhcp-server'  => {
+      bootproto => 'none',
+      ipaddress => '192.168.100.1',
+      netmask   => '255.255.255.0',
+    },
+  }
+
   network::interface { 'lsst-daq':
-    type         => 'Ethernet',
-    bootproto    => 'none',
     defroute     => 'no',  # this was yes on comcam-fp01
+    ethtool_opts => '--set-ring ${DEVICE} rx 4096 tx 4096; --pause ${DEVICE} autoneg off rx on tx on',
     hwaddr       => $hwaddr,
     ipv6init     => 'no',
-    uuid         => $uuid,
     onboot       => true,
-    ipaddress    => '192.168.100.1',
-    netmask      => '255.255.255.0',
-    ethtool_opts => '--set-ring ${DEVICE} rx 4096 tx 4096; --pause ${DEVICE} autoneg off rx on tx on'
+    type         => 'Ethernet',
+    uuid         => $uuid,
+    *            => $netconf,
   }
 }

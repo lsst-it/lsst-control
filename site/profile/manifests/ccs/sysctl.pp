a9e30c8c6a9d3bbddc22b3b9c98b96bf24859492
Joshua Hoblitt <josh@hoblitt.com>
2020-05-01 14:03:34 -0700

    add profile::ccs::sysctl

---
+++ site/profile/manifests/ccs/sysctl.pp
@@ -0,0 +1,15 @@
+class profile::ccs::sysctl {
+  $file = '/etc/sysctl.d/99-lsst-daq-ccs.conf'
+
+  sysctl::value {
+    default:
+      target => $file,
+    ;
+    'net.core.wmem_max':
+      value => 18874368,
+    ;
+    'net.core.rmem_max':
+      value => 18874368,
+    ;
+  }
+}

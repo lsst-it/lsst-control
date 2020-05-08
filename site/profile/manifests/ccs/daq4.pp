0a733ce93429a229973a2ead8fed2ed29744e17b
Glenn Morris <gmorris@slac.stanford.edu>
2020-05-08 16:31:20 -0700

    Add a profile for DAQv4

---
+++ site/profile/manifests/ccs/daq4.pp
@@ -0,0 +1,31 @@
+## @summary
+##   Settings for DAQv4.
+
+class profile::ccs::daq4 ($instrument = 'comcam') {
+
+  $dir = '/etc/ccs'
+
+  $attributes = {
+    'owner' => 'ccsadm',
+    'group' => 'ccsadm',
+    'mode'  => '0644',
+  }
+
+  $daq4file = 'daqv4-setup'
+
+  file { "${dir}/${daq4file}":
+    ensure  => file,
+    content => @(EOF),
+      export DAQ_HOME=/srv/nfs/lsst-daq/daq-sdk/current
+      export LD_LIBRARY_PATH=$DAQ_HOME/x86/lib:$LD_LIBRARY_PATH
+      | EOF
+    *       => $attributes,
+  }
+
+  file { ["${dir}/store.app", "${dir}/${instrument}-ih.app"]:
+    ensure  => file,
+    content => "system.pre-execute=${daq4file}",
+    *       => $attributes,
+  }
+
+}

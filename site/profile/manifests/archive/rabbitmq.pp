f088594276407df430796216e62407c45f006989
Joshua Hoblitt <josh@hoblitt.com>
2020-05-13 09:44:36 -0700

    add profile::archive::rabbitmq::vhosts param

--- site/profile/manifests/archive/rabbitmq.pp
+++ site/profile/manifests/archive/rabbitmq.pp
@@ -1,10 +1,14 @@
 class profile::archive::rabbitmq(
   Hash[String, Hash] $users = {},
+  Hash[String, Hash] $vhosts = {},
 ){
   include ::rabbitmq
 
   # XXX hiera support should be upstreamed
   unless (empty($users)) {
     ensure_resources('rabbitmq_user', $users)
+  }
+  unless (empty($vhosts)) {
+    ensure_resources('rabbitmq_vhost', $vhosts)
   }
 }

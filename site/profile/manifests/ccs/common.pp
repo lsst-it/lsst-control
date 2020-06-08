b9009dc5d5470ea4370b573756fcde698004fa51
Joshua Hoblitt <josh@hoblitt.com>
2020-06-08 12:15:17 -0700

    add java_artisanal to profile::ccs::common

--- site/profile/manifests/ccs/common.pp
+++ site/profile/manifests/ccs/common.pp
@@ -1,18 +1,19 @@
-## @summary
-##   Common settings for all CCS hosts.
-
+#
+# @summary
+#   Common functionality needed by ccs nodes.
+#
 class profile::ccs::common {
-
   include profile::ccs::facts
   include profile::ccs::home
   include profile::ccs::users
   include profile::ccs::clustershell
-
-  include ccs_software
-
   include profile::ccs::profile_d
   include profile::ccs::sudo
-
   include profile::ccs::monitoring
 
+  include ccs_software
+  include java_artisanal
+
+  Class['java_artisanal']
+  -> Class['ccs_software']
 }

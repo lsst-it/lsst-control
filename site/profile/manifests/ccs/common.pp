65174b7d626eaaecbf189598889b96a63c6b8070
Joshua Hoblitt <josh@hoblitt.com>
2020-06-11 15:07:38 -0700

    add p::ccs::postfix to p::ccs::common

--- site/profile/manifests/ccs/common.pp
+++ site/profile/manifests/ccs/common.pp
@@ -3,13 +3,13 @@
 #   Common functionality needed by ccs nodes.
 #
 class profile::ccs::common {
+  include profile::ccs::clustershell
   include profile::ccs::facts
   include profile::ccs::home
-  include profile::ccs::users
-  include profile::ccs::clustershell
+  include profile::ccs::monitoring
+  include profile::ccs::postfix
   include profile::ccs::profile_d
   include profile::ccs::sudo
-  include profile::ccs::monitoring
   include profile::ccs::sysctl
 
   include ccs_software

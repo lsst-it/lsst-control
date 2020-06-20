485cbbb56d8d369e62ab4b1e8311a3671ab3eac2
Andy Loftus <aloftus@gmail.com>
2020-06-19 20:27:12 -0500

    Add profile::ncsa::lhn_target_from_bdc
    Add cluster lhntest
    
    Add level1_adm access to puppetmaster nodes in NTS
    
    Add profile::ncsa::allow_ssh_from_bdc
    
    Add role ncsa::lhntest
    
    Hieradata moved to ncsa::role::lhntest
    
    Move hieradata for profile::ncsa into org/ncsa
    
    Add default hieradata for allow_ssh_from_bdc
    
    Array merge unique for allow_ssh_from_bdc
    
    More restrictive nodelist for allow_ssh_from_bdc
    
    Remove profile::ncsa::allow_ssh_from_bdc from role lhntest

---
+++ site/profile/manifests/ncsa/lhn_target_from_bdc.pp
@@ -0,0 +1,17 @@
+class profile::ncsa::lhn_target_from_bdc {
+
+  $protocols = [ 'udp', 'tcp' ]
+  $sources = [ '139.229.140.0/22', '139.229.146.0/24', '139.229.149.0/24' ]
+
+  $protocols.each | $proto | {
+    $sources.each | $src | {
+      firewall { "550 profile::ncsa::allow_lhn_from_bdc - ${src} + ${proto}" :
+        action => 'accept',
+        dport  => '5000-5050',
+        source => $src,
+        proto  => $proto,
+      }
+    }
+  }
+
+}

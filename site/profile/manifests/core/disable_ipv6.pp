6cb1bd477cb7dbc4c499144601fadfd24462f0ec
Joshua Hoblitt <josh@hoblitt.com>
2020-07-01 14:48:39 -0700

    add p::core::disable_ipv6

---
+++ site/profile/manifests/core/disable_ipv6.pp
@@ -0,0 +1,22 @@
+class profile::core::disable_ipv6(
+  Boolean $disable = true,
+) {
+  $file = '/etc/sysctl.d/50-ipv6.conf'
+
+  $v = $disable ? {
+    true    => 1,
+    default => 0
+  }
+
+  sysctl::value {
+    default:
+      target => $file,
+    ;
+    'net.ipv6.conf.all.disable_ipv6':
+      value => $v,
+    ;
+    'net.ipv6.conf.default.disable_ipv6':
+      value => $v,
+    ;
+  }
+}

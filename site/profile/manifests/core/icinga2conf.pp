a172a870e64d468152d7406404f09d5179f5b16f
Heinrich Reinking <hreinkinge@gmail.com>
2020-07-10 13:56:03 -0400

    move variables to hiera

--- site/profile/manifests/core/icinga2conf.pp
+++ site/profile/manifests/core/icinga2conf.pp
@@ -1,7 +1,20 @@
 # @summary
 #   Ensure icinga2.conf file
 
-class profile::core::icinga2conf
+class profile::core::icinga2conf (
+  $icinga_user,
+  $icinga_pwd,
+  $icinga_db ,
+  $icinga_hostname,
+  $ldap_server,
+  $ldap_root,
+  $ldap_user,
+  $ldap_pwd,
+  $ldap_resource,
+  $ldap_user_filter,
+  $ldap_group_filter,
+  $ldap_group_base,
+)
 {
   include '::apache'
   include '::apache::mod::proxy'
@@ -15,100 +28,86 @@ class profile::core::icinga2conf
   ::apache::namevirtualhost { '*:443': }
   ::apache::listen { '443': }
 
-$icinga_user = 'icinga2'
-$icinga_pwd  = 'supersecret'
-$icinga_db   = 'icinga2'
-$icinga_hostname = 'it-icinga.ls.lsst.org'
 
-$ldap_server = '139.229.135.6'
-$ldap_root = 'cn=accounts,dc=lsst,dc=cloud'
-$ldap_user = 'uid=svc_icinga,cn=users,cn=accounts,dc=lsst,dc=cloud'
-$ldap_pwd  = 'um0l7BmP;$WU'
-$ldap_resource = 'rubinobs'
-$ldap_user_filter = '(memberof=cn=icinga-admins,cn=groups,cn=accounts,dc=lsst,dc=cloud)'
-$ldap_group_filter = 'cn=icinga-*'
-$ldap_group_base = 'cn=groups,cn=accounts,dc=lsst,dc=cloud'
-
-mysql::db { $icinga_db:
-  user     => $icinga_user,
-  password => $icinga_pwd,
-  host     => 'localhost',
-  grant    => [ 'ALL' ],
-}
-
-class { '::icinga2::feature::idomysql':
-      user          => $icinga_user,
-      password      => $icinga_pwd,
-      database      => $icinga_db,
-      import_schema => true,
-}
-
-class { '::icinga2::feature::api':
-  pki             => 'none',
-  accept_commands => true,
-  endpoints       => {
-    $icinga_hostname => {},
-  },
-  zones           => {
-    'master' => {
-      'endpoints' => [$icinga_hostname],
-    },
+  mysql::db { $icinga_db:
+    user     => $icinga_user,
+    password => $icinga_pwd,
+    host     => 'localhost',
+    grant    => [ 'ALL' ],
   }
-}
 
-icinga2::object::zone { 'global-templates':
-  global => true,
-}
+  class { '::icinga2::feature::idomysql':
+        user          => $icinga_user,
+        password      => $icinga_pwd,
+        database      => $icinga_db,
+        import_schema => true,
+  }
 
-icingaweb2::config::resource{ $ldap_resource:
-  type         => 'ldap',
-  host         => $ldap_server,
-  port         => 389,
-  ldap_root_dn => $ldap_root,
-  ldap_bind_dn => $ldap_user,
-  ldap_bind_pw => $ldap_pwd,
-}
-
-icingaweb2::config::authmethod { 'ldap-auth':
-  backend                  => 'ldap',
-  resource                 => $ldap_resource,
-  ldap_user_class          => 'inetOrgPerson',
-  ldap_filter              => $ldap_user_filter,
-  ldap_user_name_attribute => 'uid',
-  order                    => '05',
-}
-
-icingaweb2::config::groupbackend { 'ldap-groups':
-  backend                   => 'ldap',
-  resource                  => $ldap_resource,
-  ldap_group_class          => 'groupOfNames',
-  ldap_group_name_attribute => 'cn',
-  ldap_group_filter         => $ldap_group_filter,
-  ldap_base_dn              => $ldap_group_base,
-}
-
-icingaweb2::config::role { 'Admin User':
-  groups      => 'icinga-admins',
-  permissions => '*',
-}
-
-class {'icingaweb2::module::monitoring':
-  ido_host          => 'localhost',
-  ido_db_name       => $icinga_db,
-  ido_db_username   => $icinga_user,
-  ido_db_password   => $icinga_pwd,
-  commandtransports => {
-    icinga2 => {
-      transport => 'api',
-      username  => 'icingaweb2',
-      password  => 'supersecret',
+  class { '::icinga2::feature::api':
+    pki             => 'none',
+    accept_commands => true,
+    endpoints       => {
+      $icinga_hostname => {},
+    },
+    zones           => {
+      'master' => {
+        'endpoints' => [$icinga_hostname],
+      },
     }
   }
-}
-  file { '/etc/httpd/conf.d/icingaweb2.conf':
-    ensure => file,
-    source => 'puppet:///modules/icingaweb2/examples/apache2/for-mod_proxy_fcgi.conf',
-    notify => Service['httpd'],
+
+  icinga2::object::zone { 'global-templates':
+    global => true,
   }
 
+  icingaweb2::config::resource{ $ldap_resource:
+    type         => 'ldap',
+    host         => $ldap_server,
+    port         => 389,
+    ldap_root_dn => $ldap_root,
+    ldap_bind_dn => $ldap_user,
+    ldap_bind_pw => $ldap_pwd,
+  }
+
+  icingaweb2::config::authmethod { 'ldap-auth':
+    backend                  => 'ldap',
+    resource                 => $ldap_resource,
+    ldap_user_class          => 'inetOrgPerson',
+    ldap_filter              => $ldap_user_filter,
+    ldap_user_name_attribute => 'uid',
+    order                    => '05',
+  }
+
+  icingaweb2::config::groupbackend { 'ldap-groups':
+    backend                   => 'ldap',
+    resource                  => $ldap_resource,
+    ldap_group_class          => 'groupOfNames',
+    ldap_group_name_attribute => 'cn',
+    ldap_group_filter         => $ldap_group_filter,
+    ldap_base_dn              => $ldap_group_base,
+  }
+
+  icingaweb2::config::role { 'Admin User':
+    groups      => 'icinga-admins',
+    permissions => '*',
+  }
+
+  class {'icingaweb2::module::monitoring':
+    ido_host          => 'localhost',
+    ido_db_name       => $icinga_db,
+    ido_db_username   => $icinga_user,
+    ido_db_password   => $icinga_pwd,
+    commandtransports => {
+      icinga2 => {
+        transport => 'api',
+        username  => 'icingaweb2',
+        password  => 'supersecret',
+      }
+    }
+  }
+    file { '/etc/httpd/conf.d/icingaweb2.conf':
+      ensure => file,
+      source => 'puppet:///modules/icingaweb2/examples/apache2/for-mod_proxy_fcgi.conf',
+      notify => Service['httpd'],
+    }
 }

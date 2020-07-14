ab4b6beb24cccd01c89b65ca7e5e86b3aee77513
Heinrich Reinking <hreinkinge@gmail.com>
2020-07-14 11:01:52 -0400

    add satellite

--- site/profile/manifests/it/icinga_satellite.pp
+++ site/profile/manifests/it/icinga_satellite.pp
@@ -1,7 +1,33 @@
 # @summary
 #   Same as common but excludes icinga agent
 
-class profile::it::icinga_satellite {
+class profile::it::icinga_satellite (
+  $icinga_master,
+){
   include profile::core::uncommon
   include profile::core::remi
+  include ::icinga2
+  include ::icinga2::repo
+
+  class { '::icinga2::feature::api':
+    pki             => 'none',
+    accept_config   => true,
+    accept_commands => true,
+    endpoints       => {
+      $icinga_master => {},
+      $facts['fqdn'] => {},
+    },
+    zones           => {
+      'master'    => {
+        'endpoints'  => [$icinga_master],
+      },
+      'satellite' => {
+        'endpoints' => [$facts['fqdn']],
+        'parent'    => 'master',
+      },
+    },
+  }
+  icinga2::object::zone { 'global-templates':
+    global => true,
+  }
 }

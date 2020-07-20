60a1fe11f8c9a777e294c70217ad4c48fc238c10
shahramsobhani <44172862+shahramsobhani@users.noreply.github.com>
2020-07-20 09:40:48 -0700

    added monog ver

--- site/profile/manifests/it/graylog.pp
+++ site/profile/manifests/it/graylog.pp
@@ -4,7 +4,7 @@ class profile::it::graylog {
     }
 class {'mongodb::globals':
   manage_package_repo => true,
-#  manage_package      => true,
+  version             => '3.6',
   }
 class { 'mongodb::server':
   bind_ip => ['127.0.0.1'],

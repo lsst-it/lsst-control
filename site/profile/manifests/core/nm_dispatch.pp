3e0f9a1755696d8eb8dd7d3e1dd25776ed6abe33
Joshua Hoblitt <josh@hoblitt.com>
2020-07-21 11:10:31 -0700

    add profile::core::nm_dispatch

---
+++ site/profile/manifests/core/nm_dispatch.pp
@@ -0,0 +1,24 @@
+class profile::core::nm_dispatch (
+  Optional[Hash[String, Array[String]]] $interfaces = undef,
+) {
+
+  if ($interfaces) {
+    $interfaces.each |String $dev, Array $cmds| {
+      # if restart is configured to be only per interface... needs some experimentation
+      #$network_notify = "Exec[network_restart_${dev}]"
+      $network_notify = 'Class[network]'
+
+      $data = {
+        dev  => $dev,
+        cmds => $cmds,
+      }
+
+      file { "/etc/NetworkManager/dispatcher.d/50-${dev}":
+        ensure  => file,
+        content => epp("${module_name}/core/nm_interface/50-dev.epp", $data),
+        mode    => '0755',
+        notify  => $network_notify,
+      }
+    }
+  }
+}

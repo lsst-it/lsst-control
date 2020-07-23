fe3e3c3d1e94cff5b486b10b17fbb32a9c0c338d
Andy Loftus <aloftus@gmail.com>
2020-07-23 15:15:18 -0500

    Allow scp from BDC nodelist, add ampath to nodelist

--- site/profile/manifests/ncsa/lhn_target_from_bdc.pp
+++ site/profile/manifests/ncsa/lhn_target_from_bdc.pp
@@ -1,7 +1,12 @@
 class profile::ncsa::lhn_target_from_bdc {
 
   $protocols = [ 'udp', 'tcp' ]
-  $sources = [ '139.229.140.0/22', '139.229.146.0/24', '139.229.149.0/24' ]
+  $sources = [
+    '139.229.140.0/22',
+    '139.229.146.0/24',
+    '139.229.149.0/24',
+    '198.32.252.217/32',
+  ]
 
   $protocols.each | $proto | {
     $sources.each | $src | {

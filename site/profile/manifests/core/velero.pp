fa886a33dfffda0c2ddd5374bd8ebb2aa89c1b25
Heinrich Reinking <hreinkinge@gmail.com>
2020-08-14 14:02:03 -0400

    redesign module

--- site/profile/manifests/core/velero.pp
+++ site/profile/manifests/core/velero.pp
@@ -1,32 +1,20 @@
 # @summary
 #   Velero cli instalation and autocompletion
 
-class profile::core::velero
-{
-  $runif1  = 'test ! -f /usr/bin/velero'
-  $runif2  = 'test ! -f /etc/bash_completion.d/velero'
-  $runif3  = 'test ! -d /var/tmp/velero-v1.4.0-linux-amd64'
+class profile::core::velero {
+  $runif  = 'test ! -f /etc/bash_completion.d/velero'
+  $command = 'velero completion bash > /etc/bash_completion.d/velero'
 
-  $command1 = 'wget -qO- https://github.com/vmware-tanzu/velero/releases/download/v1.4.0/velero-v1.4.0-linux-amd64.tar.gz | tar xz; mv velero-v1.4.0-linux-amd64/velero /usr/bin/velero'
-  $command2 = 'velero completion bash > /etc/bash_completion.d/velero'
-  $command3 = 'rm -rf /var/tmp/velero-v1.4.0-linux-amd64'
-
-  exec { $command1:
-    cwd      => '/var/tmp',
-    path     => ['/sbin', '/usr/sbin', '/bin'],
-    provider => shell,
-    onlyif   => $runif1,
+  vcsrepo { '/usr/share/velero':
+    ensure   => present,
+    provider => git,
+    source   => 'git://github.com/vmware-tanzu/velero.git',
+    revision => 'v1.4.2',
   }
-  -> exec { $command2:
+  exec { $command:
     cwd      => '/var/tmp',
     path     => ['/sbin', '/usr/sbin', '/bin'],
     provider => shell,
-    onlyif   => $runif2,
-  }
-  -> exec { $command3:
-    cwd      => '/var/tmp',
-    path     => ['/sbin', '/usr/sbin', '/bin'],
-    provider => shell,
-    onlyif   => $runif3,
+    onlyif   => $runif,
   }
 }

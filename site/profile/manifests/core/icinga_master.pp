24dae900bfef15777cd58fc82adfb89cd31594d3
Heinrich Reinking <hreinkinge@gmail.com>
2020-08-25 16:39:05 -0400

    remove force deploy exec

--- site/profile/manifests/core/icinga_master.pp
+++ site/profile/manifests/core/icinga_master.pp
@@ -36,14 +36,6 @@ class profile::core::icinga_master (
   #Letsencrypt cert path
   $le_root = "/etc/letsencrypt/live/${master_fqdn}"
 
-  #IcingaDirector force Deploy
-  $url         = "https://${master_fqdn}/director"
-  $credentials = "Authorization:Basic ${credentials_hash}"
-  $format      = 'Accept: application/json'
-  $curl        = 'curl -s -k -H'
-  $icinga_path = '/opt/icinga'
-  $deploy_cmd  = "${curl} '${credentials}' -H '${format}' -X POST '${url}/config/deploy'"
-
   #pnp4nagios webpage integration
   $pnp4nagios_conf = @(PNPNAGIOS/L)
     location /pnp4nagios {
@@ -389,14 +381,6 @@ class profile::core::icinga_master (
   service { 'npcd':
     ensure  => running,
     require => Package[$packages],
-  }
-  #Force Deploy every puppet run
-  exec { $deploy_cmd:
-    cwd      => $icinga_path,
-    path     => ['/sbin', '/usr/sbin', '/bin'],
-    provider => shell,
-    require  => Nginx::Resource::Location['icingaweb2_index'],
-    loglevel => debug,
   }
   #<-----------END Clases definition----------------->
 }

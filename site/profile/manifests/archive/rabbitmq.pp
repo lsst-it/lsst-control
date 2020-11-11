60ad25e4605316b49e0178fc26da1667839b7973
Joshua Hoblitt <josh@hoblitt.com>
2020-11-11 16:06:50 -0700

    revise rabbitmq queues to support both auxtel & comcam

--- site/profile/manifests/archive/rabbitmq.pp
+++ site/profile/manifests/archive/rabbitmq.pp
@@ -1,13 +1,13 @@
 class profile::archive::rabbitmq(
-  Hash[String, Hash] $users            = {},
-  Hash[String, Hash] $vhosts           = {},
-  Hash[String, Hash] $exchanges        = {},
-  Hash[String, Hash] $queues           = {},
-  Hash[String, Hash] $bindings         = {},
-  Hash[String, Hash] $user_permissions = {},
-  Array              $arc_queues       = [],
-  Optional[String]   $queue_user       = undef,
-  Optional[String]   $queue_password   = undef,
+  Hash[String, Hash] $users               = {},
+  Hash[String, Hash] $vhosts              = {},
+  Hash[String, Hash] $exchanges           = {},
+  Hash[String, Hash] $queues              = {},
+  Hash[String, Hash] $bindings            = {},
+  Hash[String, Hash] $user_permissions    = {},
+  Hash[String, Array[String]] $arc_queues = [],
+  Optional[String]   $queue_user          = undef,
+  Optional[String]   $queue_password      = undef,
 ){
   include ::rabbitmq
 
@@ -38,20 +38,22 @@ class profile::archive::rabbitmq(
   }
 
   unless (empty($arc_queues)) {
-    $arc_queues.each |String $q| {
-      rabbitmq_queue { "${q}@/test_cc":
-        ensure   => present,
-        user     => $queue_user,
-        password => $queue_password,
-        durable  => true,
-      }
-      rabbitmq_binding { "message@${q}@/test_cc":
-        ensure           => present,
-        user             => $queue_user,
-        password         => $queue_password,
-        destination_type => 'queue',
-        routing_key      => $q,
-      }
-    }
+    $arc_queues.each |String $vhost, Array $queues| {
+      $queues.each |String $q| {
+        rabbitmq_queue { "${q}@${vhost}":
+          ensure   => present,
+          user     => $queue_user,
+          password => $queue_password,
+          durable  => true,
+        }
+        rabbitmq_binding { "message@${q}@${vhost}":
+          ensure           => present,
+          user             => $queue_user,
+          password         => $queue_password,
+          destination_type => 'queue',
+          routing_key      => $q,
+        }
+      } # $queues
+    } # $arc_queues
   }
 }

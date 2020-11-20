ff5d30dd49133fc2d096507b7359a2418018d31c
Heinrich Reinking <hreinkinge@gmail.com>
2020-11-20 10:41:46 -0300

    m2-control node definition for VIPM dependancies

---
+++ site/profile/manifests/core/ni_packages.pp
@@ -0,0 +1,23 @@
+# @summary
+#  NI LabView 2018 requirement packages
+
+class profile::core::ni_packages {
+  $packages = [
+    'mlocate',
+    'wget',
+    'openssl-devel',
+    'make',
+    'gcc-c++',
+    'bzip2-devel',
+    'libffi-devel',
+    'libXinerama',
+    'mesa-libGL',
+    'libstdc++',
+    'libstdc++.i686',
+    'libXft',
+  ]
+
+  package { $packages:
+    ensure => 'present',
+  }
+}

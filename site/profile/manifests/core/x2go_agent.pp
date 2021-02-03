c46de2500ce3fa4b852f94eae42937aa4c33952a
Heinrich Reinking <hreinkinge@gmail.com>
2021-02-03 13:38:49 -0300

    add x2go agent packages

---
+++ site/profile/manifests/core/x2go_agent.pp
@@ -0,0 +1,17 @@
+# @summary
+#   Adds additional packages for x2go agent
+
+class profile::core::x2go_agent {
+
+  $packages = [
+    'x2goserver',
+    'x2goclient',
+    'x2goserver-common',
+    'x2goserver-xsession',
+    'x2goagent',
+  ]
+
+  package { $packages:
+    ensure => 'present',
+  }
+}

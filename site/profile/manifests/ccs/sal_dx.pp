a4ab27e50219f8834ba6729ad20459e90398f27a
Heinrich Reinking <hreinkinge@gmail.com>
2021-02-19 13:16:24 -0300

    back to single repo config

--- site/profile/manifests/ccs/sal_dx.pp
+++ site/profile/manifests/ccs/sal_dx.pp
@@ -3,9 +3,10 @@
 #   Provides base packages for SAL/DDS diagnostics
 #
 
-class profile::ccs::sal_dx {
-  include profile::ccs::ts_repo
-
+class profile::ccs::sal_dx (
+  String $username,
+  String $password,
+){
   $directory = '/opt/lsst-ts'
   $packages = [
     'vim',
@@ -20,14 +21,25 @@ class profile::ccs::sal_dx {
     'nethogs',
     'etherape',
     'iptraf-ng',
-    'OpenSpliceDDS-6.10.4-2',
   ]
+
+  yumrepo { 'ts_yum_private':
+    ensure   => 'present',
+    enabled  => true,
+    gpgcheck => false,
+    descr    => 'LSST Telescope and Site Private Packages',
+    baseurl  => "https://${username}:${password}@repo-nexus.lsst.org/nexus/repository/ts_yum_private",
+    target   => '/etc/yum.repos.d/ts_yum_private.repo',
+  }
+  ->package {'OpenSpliceDDS-6.10.4-2':
+    ensure => present,
+  }
 
   yum::group { 'Development Tools':
     ensure => present,
   }
   ->package { $packages:
-    ensure => 'present'
+    ensure => present,
   }
 
   file { $directory:

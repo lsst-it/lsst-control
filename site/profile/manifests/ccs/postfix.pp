019ca993010e5aef92e8dfaf4d3a282f7ad9e3cd
Heinrich Reinking <hreinkinge@gmail.com>
2021-03-03 10:59:23 -0300

    remove ::1 from /etc/hosts
    (cherry picked from commit 99f2c5b4ab466184090add020cd907a40d4faa03)

--- site/profile/manifests/ccs/postfix.pp
+++ site/profile/manifests/ccs/postfix.pp
@@ -8,6 +8,12 @@ class profile::ccs::postfix(
     ensure  => 'present',
     content => $auth,
   }
+  exec { 'sed -i \'/^::1/d\' /etc/hosts':
+    cwd      => '/tmp',
+    path     => ['/sbin', '/usr/sbin', '/bin'],
+    provider => shell,
+    onlyif   => 'grep \'::1         localhost\' /etc/hosts',
+  }
 
   ensure_packages($packages)
   Package[$packages] -> Class[postfix]

304c200c67798e0ff0566a009cfebfdf7766e51d
Heinrich Reinking <hreinkinge@gmail.com>
2021-05-05 12:56:07 -0400

    initial commit tang profile

---
+++ site/profile/manifests/core/tang.pp
@@ -0,0 +1,44 @@
+# @summary
+#   Tang Server Encryption Module
+
+class profile::core::tang() {
+  #Variables
+  $packages = [
+    'vim',
+    'tang',
+  ]
+
+  #Add require packages
+  package { $packages:
+    ensure => 'present',
+  }
+
+  #Ensure tang service is running
+  service { 'tangd.socket':
+    ensure  => 'running',
+    require => Package[$packages],
+  }
+
+  #Create tangd.socket override directory
+  file { 'tangd.socket.d':
+    ensure => 'directory',
+    path   => '/etc/systemd/system',
+    mode   => '0755',
+  }
+  #Create override config file
+  file { 'override.conf':
+    ensure  => 'present',
+    path    => '/etc/systemd/system/tangd.socket.d',
+    mode    => '0644',
+    require => File['tangd.socket.d'],
+    content => @(OVERRIDE/L)
+      [Socket]
+      ListenStream=
+      ListenStream=7500
+      | OVERRIDE
+  }
+  ->exec { '/usr/bin/systemctl daemon-reload':
+    refreshonly => true,
+    notify      => Service['tangd.socket']
+  }
+}

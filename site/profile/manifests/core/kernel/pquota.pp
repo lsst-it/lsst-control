e005cb3c15b32347829fa006268c13b76b080700
Joshua Hoblitt <josh@hoblitt.com>
2021-07-09 10:51:37 -0700

    refactor p::core::kernel::pquote to use p::util::kernel_param

--- site/profile/manifests/core/kernel/pquota.pp
+++ site/profile/manifests/core/kernel/pquota.pp
@@ -1,10 +1,3 @@
 class profile::core::kernel::pquota {
-  kernel_parameter { 'rootflags=pquota':
-    ensure => present,
-  }
-  ~> reboot { 'rootfs xfs pquota':
-    apply   => finished,
-    message => 'enable rootfs xfs pquota',
-    when    => refreshed,
-  }
+  profile::util::kernel_param { 'rootflags=pquota': }
 }

983906c666bd3050acabdf22c1c3d083d67509f2
Joshua Hoblitt <josh@hoblitt.com>
2021-07-27 10:05:00 -0700

    (p::core::systemd profile) include systemd class

--- site/profile/manifests/core/systemd.pp
+++ site/profile/manifests/core/systemd.pp
@@ -11,6 +11,7 @@ class profile::core::systemd(
   Optional[Hash[String, Hash]] $dropin_file = undef,
   Optional[Hash[String, Hash]] $tmpfile = undef,
 ) {
+  include systemd
 
   if $dropin_file {
     ensure_resources('systemd::dropin_file', $dropin_file)

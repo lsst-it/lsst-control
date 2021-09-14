ff5213617f59c3ef2e50cbf26548626b25ac905f
Joshua Hoblitt <josh@hoblitt.com>
2021-09-14 15:49:06 -0700

    (daq-mgt role) enable nfs

--- spec/hosts/roles/daq_mgt_spec.rb
+++ spec/hosts/roles/daq_mgt_spec.rb
@@ -2,6 +2,28 @@
 
 require 'spec_helper'
 
+shared_examples 'generic daq manager' do
+  it { is_expected.to compile.with_all_deps }
+
+  it { is_expected.to contain_class('profile::core::common') }
+  it { is_expected.to contain_class('profile::ccs::daq_interface') }
+  it { is_expected.to contain_class('hosts') }
+  it { is_expected.to contain_class('nfs') }
+
+  it do
+    is_expected.to contain_class('dhcp').with(
+      interfaces: ['lsst-daq'],
+    )
+  end
+
+  it do
+    is_expected.to contain_class('chrony').with(
+      port: 123,
+      queryhosts: ['192.168/16'],
+    )
+  end
+end
+
 describe 'test1.dev.lsst.org', :site do
   describe 'daq-mgt role' do
     lsst_sites.each do |site|
@@ -15,34 +37,8 @@ describe 'test1.dev.lsst.org', :site do
           }
         end
 
-        it { is_expected.to compile.with_all_deps }
+        include_examples 'generic daq manager'
 
-        it { is_expected.to contain_class('profile::core::common') }
-
-        it do
-          is_expected.to contain_class('dhcp').with(
-            interfaces: ['lsst-daq'],
-          )
-        end
-
-        it do
-          is_expected.to contain_class('chrony').with(
-            port: 123,
-            queryhosts: ['192.168/16'],
-          )
-        end
-
-        it { is_expected.to contain_class('profile::ccs::daq_interface') }
-
-        #it do
-        #  is_expected.to contain_network__interface('lsst-daq').with(
-        #    bootproto: 'none',
-        #    ipaddress: '192.168.100.1',
-        #    netmask: '255.255.255.0',
-        #  )
-        #end
-
-        it { is_expected.to contain_class('hosts') }
       end
     end  # site
   end  # role

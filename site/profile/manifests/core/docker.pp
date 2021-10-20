0456b27f5a226a362bf5471dd3e139145924ef3f
Joshua Hoblitt <josh@hoblitt.com>
2021-10-20 12:55:57 -0700

    (p::core::docker) add versionlock param

--- site/profile/manifests/core/docker.pp
+++ site/profile/manifests/core/docker.pp
@@ -7,21 +7,17 @@
 # @param socket_group
 #   gid for docker named pipe
 #
-# @param docker_ce_package
-#   complete package string for versionlock
-#
-# @param docker_ce_cli_package
-#   complete package string for versionlock
-#
 # @param storage_driver
 #   name of docker storage driver to use
 #
+# @param versionlock
+#   Array of yum::versionlock resources to create
+#
 class profile::core::docker (
-  Optional[String] $version     = '19.03.15',
-  String $socket_group          = '70014',
-  String $docker_ce_package     = 'docker-ce-19.03.15-3.el7.x86_64',
-  String $docker_ce_cli_package = 'docker-ce-cli-19.03.15-3.el7.x86_64',
-  String $storage_driver        = 'overlay2',
+  Optional[String] $version,
+  String $socket_group                 = '70014',
+  String $storage_driver               = 'overlay2',
+  Optional[Array[String]] $versionlock = undef,
 ) {
   class { 'docker':
     overlay2_override_kernel_check => true,  # needed on el7
@@ -31,10 +27,11 @@ class profile::core::docker (
     version                        => $version,
   }
 
-  yum::versionlock { [
-      "3:${docker_ce_package}",
-      "1:${docker_ce_cli_package}",
-    ]:
+  if $versionlock {
+    include yum::plugin::versionlock
+
+    yum::versionlock { [$versionlock]:
       ensure => present,
+    }
   }
 }

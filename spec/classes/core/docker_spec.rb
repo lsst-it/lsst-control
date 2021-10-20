bd8aa72c869ab22e7a05210fce2a480ba904899e
Joshua Hoblitt <josh@hoblitt.com>
2021-10-20 12:56:46 -0700

    (site tu) update docker to 20.10.9

---
+++ spec/classes/core/docker_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'profile::core::docker' do
+  let(:node_params) { { org: 'lsst' } }
+
+  it { is_expected.to compile.with_all_deps }
+
+  it do
+    is_expected.to contain_class('docker').with(
+      overlay2_override_kernel_check: true,
+      socket_group: 70_014,
+      socket_override: true,
+      storage_driver: 'overlay2',
+      version: '19.03.15',
+    )
+  end
+
+  it { is_expected.to contain_class('yum::plugin::versionlock') }
+  it { is_expected.to have_yum__versionlock_resource_count(2) }
+
+  context 'when site tu' do
+    let(:node_params) do
+      super().merge(
+        site: 'tu',
+      )
+    end
+
+    it { is_expected.to contain_class('docker').with(version: '20.10.9') }
+  end
+end

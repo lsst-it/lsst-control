d699672db828359f1e4f92b33bcfdac21a970676
Heinrich Reinking <hreinkinge@gmail.com>
2021-11-05 16:48:41 -0300

    define duo role

--- site/profile/manifests/core/duo.pp
+++ site/profile/manifests/core/duo.pp
@@ -2,8 +2,25 @@
 #   Install and manage Duo 2fa proxy
 
 class profile::core::duo (
-  String $ikey = NULL,
-  String $skey = NULL,
-  String $api = NULL,
+  String $ikey,
+  String $skey,
+  String $api,
 ) {
+  class { 'duo_unix':
+    ensure            => 'present',
+    manage_ssh        => false,
+    usage             => 'login',
+    ikey              => $ikey,
+    skey              => $skey,
+    host              => $api,
+    motd              => 'no',
+    manage_pam        => false,
+    manage_repo       => true,
+    fallback_local_ip => 'no',
+    failmode          => 'safe',
+    pushinfo          => 'yes',
+    autopush          => 'yes',
+    prompts           => 3,
+    accept_env_factor => 'no'
+  }
 }

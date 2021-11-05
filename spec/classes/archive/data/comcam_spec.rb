c36ec7c7e1d7bb21c44f045cac5f7e4b75330ba4
Joshua Hoblitt <josh@hoblitt.com>
2021-11-04 18:45:16 -0700

    (p::archive::data::auxtel) add /data/repo/LATISS/u

--- spec/classes/archive/data/comcam_spec.rb
+++ spec/classes/archive/data/comcam_spec.rb
@@ -8,5 +8,6 @@ describe 'profile::archive::data::comcam' do
   it { is_expected.to compile.with_all_deps }
 
   it { is_expected.not_to contain_file('/data/repo/LATISS') }
-  it { is_expected.to contain_file('/data/repo/LSSTComCam') }
+  it { is_expected.to contain_file('/data/repo/LSSTComCam').with_mode('0777') }
+  it { is_expected.to contain_file('/data/repo/LSSTComCam/u').with_mode('1777') }
 end

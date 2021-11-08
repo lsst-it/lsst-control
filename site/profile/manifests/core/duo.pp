fe7a7cb6b7e444f7319ca3d5e508f99445ae9527
Heinrich Reinking <hreinkinge@gmail.com>
2021-11-08 08:52:18 -0300

    fetch and untar duo package

--- site/profile/manifests/core/duo.pp
+++ site/profile/manifests/core/duo.pp
@@ -6,21 +6,39 @@ class profile::core::duo (
   String $skey,
   String $api,
 ) {
-  class { 'duo_unix':
-    ensure            => 'present',
-    manage_ssh        => false,
-    usage             => 'login',
-    ikey              => $ikey,
-    skey              => $skey,
-    host              => $api,
-    motd              => 'no',
-    manage_pam        => false,
-    manage_repo       => true,
-    fallback_local_ip => 'no',
-    failmode          => 'safe',
-    pushinfo          => 'yes',
-    autopush          => 'yes',
-    prompts           => 3,
-    accept_env_factor => 'no'
+  #  Duo Archive Variables
+  $source_name    = 'duoauthproxy'
+  $install_path   = "/opt/${source_name}"
+  $package_ensure = '5.5.0-src'
+  $repository_url = 'https://dl.duosecurity.com'
+  $package_name   = "${source_name}-${package_ensure}.tgz"
+  $package_source = "${repository_url}/${package_name}"
+
+  #  Yum list packages
+  $yum_packages = [
+    'gcc',
+    'make',
+    'libffi-devel',
+    'perl',
+    'zlib-devel',
+    'diffutils',
+  ]
+  #  Install Duo packages requirement
+  package { $yum_packages:
+    ensure => 'present'
+  }
+  #  Create Duo Directory
+  file { $install_path:
+    ensure => directory,
+  }
+  #  Fetch and untar duosecurity
+  -> archive { $package_name:
+    path            => "/tmp/${package_name}",
+    source          => $package_source,
+    extract         => true,
+    extract_path    => '/opt',
+    extract_command => "tar zxf %s -C ${install_path} --strip-components=1",
+    cleanup         => true,
+    require         => Package[$yum_packages],
   }
 }

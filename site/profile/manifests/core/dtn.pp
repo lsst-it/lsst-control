3a27fffcd4a62ecb1439407522612d3c48d369c5
Joshua Hoblitt <josh@hoblitt.com>
2022-01-27 12:41:54 -0700

    (profile::core::dtn) rm tql_tunning service; replace with NM dispatch
    The `tql_tunning` service is being invoked on every puppet run.  This is
    undesirable as it makes it appear if the puppet state on the node is
    not converged.  Running interface tuning commands as a one-off is also
    undesirable as parameters may be lost when the interface goes up and
    down.  The NM dispatch script hooks are a more reliable option.
    Additionally, interface name specific configuration generally should not
    be baked into a profile class as it is inherently tied to a hosts
    hardware.

--- site/profile/manifests/core/dtn.pp
+++ site/profile/manifests/core/dtn.pp
@@ -23,33 +23,4 @@ class profile::core::dtn (
   service { 'irqbalance':
     ensure => 'stopped'
   }
-
-  # Transmit Queue Lenght at boot
-  file { '/usr/local/bin/tql_tunning':
-    ensure  => 'present',
-    mode    => '0755',
-    content => @(TQL/L)
-      #!/bin/bash
-      ip link set enp6s0 txqueuelen 20000
-      ethtool -K enp6s0 lro on
-      ethtool -G enp6s0 rx 8192 tx 8192
-      | TQL
-  }
-  -> systemd::unit_file { 'tql_tunning.service':
-    enable  => true,
-    active  => true,
-    content => @(SERVICE/L)
-      [Unit]
-      Description=Network Interfaces Tunning
-      After=network.target
-
-      [Service]
-      Type=simple
-      ExecStart=/usr/local/bin/tql_tunning
-      TimeoutStartSec=0
-
-      [Install]
-      WantedBy=default.target
-      | SERVICE
-  }
 }

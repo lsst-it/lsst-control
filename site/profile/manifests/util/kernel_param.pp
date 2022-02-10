5ed317cdbedfc1b178cba8ec3a19354fdc2e2c5a
Joshua Hoblitt <josh@hoblitt.com>
2022-02-10 09:38:45 -0700

    (profile::util::kernel_param) add reboot param

--- site/profile/manifests/util/kernel_param.pp
+++ site/profile/manifests/util/kernel_param.pp
@@ -4,15 +4,25 @@
 # @param name
 #   Kernel parameter string. E.g.: `systemd.unified_cgroup_hierarchy=0`
 #
-define profile::util::kernel_param {
+# @param reboot
+#   Whether or not to force a reboot to ensure kernel parameter is set on the running kernel.
+#   Defaults to `true`.
+#
+define profile::util::kernel_param (
+  Boolean $reboot = true,
+) {
   $message = "set kernel parameter: ${name}"
 
   kernel_parameter { $name:
     ensure => present,
   }
-  ~> reboot { $name:
-    apply   => finished,
-    message => $message,
-    when    => refreshed,
+
+  if ($reboot) {
+    reboot { $name:
+      apply     => finished,
+      message   => $message,
+      when      => refreshed,
+      subscribe => Kernel_parameter[$name],
+    }
   }
 }
---
+++ spec/defines/util/kernel_param_spec.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'profile::util::kernel_param' do
+  let(:title) { 'foo' }
+
+  it { is_expected.to compile.with_all_deps }
+
+  context 'with no params' do
+    it do
+      is_expected.to contain_kernel_parameter(title).with(
+        ensure: 'present',
+      )
+    end
+
+    it do
+      is_expected.to contain_reboot(title).with(
+        apply: 'finished',
+        message: "set kernel parameter: #{title}",
+        when: 'refreshed',
+      )
+    end
+
+    it do
+      is_expected.to contain_reboot(title).that_subscribes_to("Kernel_parameter[#{title}]")
+    end
+  end
+
+  context 'with reboot => true' do
+    let(:params) { { reboot: true } }
+
+    it do
+      is_expected.to contain_kernel_parameter(title).with(
+        ensure: 'present',
+      )
+    end
+
+    it do
+      is_expected.to contain_reboot(title).with(
+        apply: 'finished',
+        message: "set kernel parameter: #{title}",
+        when: 'refreshed',
+      )
+    end
+
+    it do
+      is_expected.to contain_reboot(title).that_subscribes_to("Kernel_parameter[#{title}]")
+    end
+  end
+
+  context 'with reboot => false' do
+    let(:params) { { reboot: false } }
+
+    it do
+      is_expected.to contain_kernel_parameter(title).with(
+        ensure: 'present',
+      )
+    end
+
+    it { is_expected.not_to contain_reboot(title) }
+  end
+end

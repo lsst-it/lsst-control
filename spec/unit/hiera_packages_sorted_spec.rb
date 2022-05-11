5cd748ed361c1ae9c33c16193111e27bffa3112f
Joshua Hoblitt <josh@hoblitt.com>
2022-05-11 11:16:46 -0700

    sort hiera packages key

---
+++ spec/unit/hiera_packages_sorted_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+hieradata_pathname = Pathname.new(control_hieradata_path)
+
+describe 'hiera' do
+  hiera_all_files.sort.each do |f|
+    f_relpath = Pathname.new(f).relative_path_from(hieradata_pathname)
+    y = YAML.load_file(f)
+    next unless y.key?('packages')
+
+    describe f_relpath do
+      it 'packages key is sorted' do
+        k = y['packages']
+        expect(k).to eq k.sort
+      end
+    end
+  end
+end

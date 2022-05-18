00e71daaeed61ddd017095825dc30f3b1e8d371c
Joshua Hoblitt <josh@hoblitt.com>
2022-05-18 09:22:07 -0700

    (profile::core::puppet_master) fix cron /dev/null redirects
    A typo (missing >) is causing error emails to pile up in
    /var/spool/mail/root.

--- spec/classes/core/puppet_master_spec.rb
+++ spec/classes/core/puppet_master_spec.rb
@@ -6,4 +6,12 @@ describe 'profile::core::puppet_master' do
   it { is_expected.to compile.with_all_deps }
   it { is_expected.to contain_cron('webhook') }
   it { is_expected.to contain_cron('smee') }
+
+  it do
+    is_expected.to contain_cron('webhook').with_command('/usr/bin/systemctl restart webhook > /dev/null 2>&1')
+  end
+
+  it do
+    is_expected.to contain_cron('smee').with_command('/usr/bin/systemctl restart smee > /dev/null 2>&1')
+  end
 end

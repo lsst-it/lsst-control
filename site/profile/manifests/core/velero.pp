71fff5196356baaa07516ea29efbc840a4c86544
Heinrich Reinking <hreinkinge@gmail.com>
2022-05-30 12:39:22 -0400

    add symlink for velero

--- site/profile/manifests/core/velero.pp
+++ site/profile/manifests/core/velero.pp
@@ -4,12 +4,19 @@
 class profile::core::velero {
   $runif  = 'test ! -f /etc/bash_completion.d/velero'
   $command = 'velero completion bash > /etc/bash_completion.d/velero'
+  $binary_path = 'velero-v1.8.1-linux-amd64'
 
-  vcsrepo { '/usr/share/velero':
-    ensure   => present,
-    provider => git,
-    source   => 'https://github.com/vmware-tanzu/velero.git',
-    revision => 'v1.8.1',
+  archive { '/tmp/velero.tar.gz':
+    ensure       => present,
+    extract      => true,
+    extract_path => '/opt',
+    source       => 'https://github.com/vmware-tanzu/velero/releases/download/v1.8.1/velero-v1.8.1-linux-amd64.tar.gz',
+    creates      => "/opt/${$binary_path}",
+    cleanup      => true,
+  }
+  -> file { '/bin/velero':
+    ensure => 'link',
+    target => "/opt/${$binary_path}/velero",
   }
   -> exec { $command:
     cwd      => '/var/tmp',

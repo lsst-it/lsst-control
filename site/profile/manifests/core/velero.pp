3234e683cd88e181a3e1e993c156af554d2f5d65
Heinrich Reinking <hreinkinge@gmail.com>
2022-05-30 11:57:46 -0400

    create velero module

---
+++ site/profile/manifests/core/velero.pp
@@ -0,0 +1,20 @@
+# @summary
+#   Velero cli instalation and autocompletion
+
+class profile::core::velero {
+  $runif  = 'test ! -f /etc/bash_completion.d/velero'
+  $command = 'velero completion bash > /etc/bash_completion.d/velero'
+
+  vcsrepo { '/usr/share/velero':
+    ensure   => present,
+    provider => git,
+    source   => 'git://github.com/vmware-tanzu/velero.git',
+    revision => 'v1.8.1',
+  }
+  exec { $command:
+    cwd      => '/var/tmp',
+    path     => ['/sbin', '/usr/sbin', '/bin'],
+    provider => shell,
+    onlyif   => $runif,
+  }
+}

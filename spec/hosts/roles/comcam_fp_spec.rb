5dc4977546c004475a0d39add9049f8bc41ac7c7
Joshua Hoblitt <josh@hoblitt.com>
2022-06-07 10:42:13 -0700

    (comcam-fp role) rm DAQ network DHCP server

--- spec/hosts/roles/comcam_fp_spec.rb
+++ spec/hosts/roles/comcam_fp_spec.rb
@@ -2,10 +2,10 @@
 
 require 'spec_helper'
 
-describe 'test1.dev.lsst.org', :site do
-  describe 'comcam-fp role' do
-    lsst_sites.each do |site|
-      context "with site #{site}" do
+describe 'comcam-fp role' do
+  %w[tu cp].each do |site|
+    context "with site #{site}", :site do
+      describe "comcam-fp01.#{site}.lsst.org" do
         let(:node_params) do
           {
             site: site,
@@ -15,10 +15,20 @@ describe 'test1.dev.lsst.org', :site do
           }
         end
 
+        let(:facts) {{ fqdn: self.class.description }}
+
         it { is_expected.to compile.with_all_deps }
 
         include_examples 'lhn sysctls'
-      end
+
+        it { is_expected.not_to contain_class('dhcp') }
+
+        it do
+          is_expected.to contain_class('profile::ccs::daq_interface').with(
+            mode: 'dhcp-client',
+          )
+        end
+      end # host
     end # site
-  end # role
-end
+  end
+end # role

befd0da461781b6bf35626c09ee58c910d53f126
Heinrich Reinking <hreinkinge@gmail.com>
2022-07-04 16:12:01 -0400

    install plugins

--- site/profile/manifests/core/it_ansible.pp
+++ site/profile/manifests/core/it_ansible.pp
@@ -6,7 +6,12 @@ class profile::core::it_ansible (
 ) {
   $ansible_path = '/opt/ansible'
   $ansible_repo = "${ansible_path}/ansible_network"
-
+  $ansible_plugins = [
+    'cisco,nxos',
+    'arista,eos',
+    'cisco,ios',
+    'community,network',
+  ]
   $known_hosts = @("KNOWN")
     sudo -u ansible_net ssh-keyscan -t rsa github.com > ${ansible_path}/.ssh/known_hosts
     chown ansible_net:ansible_net ${ansible_path}/.ssh/known_hosts
@@ -49,5 +54,13 @@ class profile::core::it_ansible (
     ensure => file,
     mode   => '0644',
     source => "file:///${ansible_repo}/playbooks/ansible.cfg",
+  }
+  $ansible_plugins.each |$plugins| {
+    $value = split($plugins,',')
+    exec { "sudo -u ansible_net ansible-galaxy collection install ${value[0]}.${value[1]}":
+      cwd    => '/var/tmp/',
+      path   => ['/sbin', '/usr/sbin', '/bin'],
+      onlyif => ["test ! -d ${ansible_path}/.ansible/collections/ansible_collections/${value[0]}/${value[1]}"],
+    }
   }
 }

7fa06d9f3acf9a3802ed4a0c1a755bdacd9eece2
Heinrich Reinking <hreinkinge@gmail.com>
2022-07-11 14:07:24 -0400

    change sed delimiter

--- site/profile/manifests/core/cmms.pp
+++ site/profile/manifests/core/cmms.pp
@@ -9,14 +9,14 @@ class profile::core::cmms (
   $fqdn  = $facts[fqdn]
 
   #  Letsencrypt cert path
-  $le_root = "\/etc\/letsencrypt\/live\/${fqdn}"
-  $old_cert = '\/etc\/apache2\/ssl\/appliance\/appliance.openmaint.org'
+  $le_root = "/etc/letsencrypt/live/${fqdn}"
+  $old_cert = '/etc/apache2/ssl/appliance/appliance.openmaint.org'
   $new_certs = '/opt/new_certs.sh'
   $cert_default_path = '/etc/apache2/sites-available'
   $replace_certs = @("CERTS")
     #!/usr/bin/bash
-    sed -i 's/${old_cert}.crt/${le_root}\/fullchain.pem/g' ${cert_default_path}/openmaint_default.conf
-    sed -i 's/${old_cert}.key/${le_root}\/privkey.pem/g' ${cert_default_path}/openmaint_default.conf
+    sed -i 's,${old_cert}.crt,${le_root}/fullchain.pem,g' ${cert_default_path}/openmaint_default.conf
+    sed -i 's,${old_cert}.key,${le_root}/privkey.pem,g' ${cert_default_path}/openmaint_default.conf
     |CERTS
 
   letsencrypt::certonly { $fqdn:

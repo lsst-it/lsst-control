049c5615fe0a1cd9466ea041623b1f92160b2138
Joshua Hoblitt <josh@hoblitt.com>
2022-07-27 10:51:08 -0700

    (profile::core::puppet_master) rm duplicate cron resource spec

--- spec/classes/core/puppet_master_spec.rb
+++ spec/classes/core/puppet_master_spec.rb
@@ -4,8 +4,6 @@ require 'spec_helper'
 
 describe 'profile::core::puppet_master' do
   it { is_expected.to compile.with_all_deps }
-  it { is_expected.to contain_cron('webhook') }
-  it { is_expected.to contain_cron('smee') }
 
   it do
     is_expected.to contain_cron('webhook').with_command('/usr/bin/systemctl restart webhook > /dev/null 2>&1')

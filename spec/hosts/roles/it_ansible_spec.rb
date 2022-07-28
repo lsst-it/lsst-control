8e66e89ce5821d3d04da271fe7ad6bd46230ded2
Joshua Hoblitt <josh@hoblitt.com>
2022-07-28 12:55:32 -0700

    (it-ansible role) add basic spec

---
+++ spec/hosts/roles/it_ansible_spec.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+role = 'it-ansible'
+
+describe "#{role} role" do
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) do
+        facts.merge(
+          fqdn: self.class.description,
+        )
+      end
+
+      let(:node_params) do
+        {
+          role: role,
+          site: site,
+        }
+      end
+
+      let(:pre_condition) do
+        <<~PP
+          file { '/opt/ansible/.ssh/id_rsa': }
+        PP
+      end
+
+      lsst_sites.each do |site|
+        describe "#{role}.#{site}.lsst.org", :site, :common do
+          let(:site) { site }
+
+          it { is_expected.to compile.with_all_deps }
+        end # host
+      end # lsst_sites
+    end # on os
+  end # on_supported_os
+end # role

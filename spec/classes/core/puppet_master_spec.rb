a29c95181772637650322ee434bdb37694117ffd
Joshua Hoblitt <josh@hoblitt.com>
2022-08-12 11:12:28 -0700

    (profile::core::puppet_master) tidy spec

--- spec/classes/core/puppet_master_spec.rb
+++ spec/classes/core/puppet_master_spec.rb
@@ -9,23 +9,7 @@ describe 'profile::core::puppet_master' do
 
       it { is_expected.to compile.with_all_deps }
 
-      it do
-        is_expected.to contain_cron('webhook').with_command('/usr/bin/systemctl restart webhook > /dev/null 2>&1')
-      end
-
-      it do
-        is_expected.to contain_cron('smee').with_command('/usr/bin/systemctl restart smee > /dev/null 2>&1')
-      end
-
-      it { is_expected.to contain_foreman__plugin('puppet') }
-      it { is_expected.to contain_foreman__cli__plugin('foreman_puppet') }
-      it { is_expected.to contain_foreman__plugin('tasks') }
-      it { is_expected.to contain_foreman__cli__plugin('foreman_tasks') }
-      it { is_expected.to contain_foreman__plugin('remote_execution') }
-      it { is_expected.to contain_foreman__cli__plugin('foreman_remote_execution') }
-      it { is_expected.to contain_foreman__plugin('templates') }
-      it { is_expected.to contain_foreman__cli__plugin('foreman_templates') }
-      it { is_expected.to contain_foreman__plugin('column_view') }
+      include_examples 'puppet_master'
 
       context 'with foreman_hostgroup param' do
         let(:params) do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -272,4 +272,24 @@ shared_examples 'debugutils' do
   it { is_expected.to contain_package('jq') }
 end
 
+shared_examples 'puppet_master' do
+  it do
+    is_expected.to contain_cron('webhook').with_command('/usr/bin/systemctl restart webhook > /dev/null 2>&1')
+  end
+
+  it do
+    is_expected.to contain_cron('smee').with_command('/usr/bin/systemctl restart smee > /dev/null 2>&1')
+  end
+
+  it { is_expected.to contain_foreman__plugin('puppet') }
+  it { is_expected.to contain_foreman__cli__plugin('foreman_puppet') }
+  it { is_expected.to contain_foreman__plugin('tasks') }
+  it { is_expected.to contain_foreman__cli__plugin('foreman_tasks') }
+  it { is_expected.to contain_foreman__plugin('remote_execution') }
+  it { is_expected.to contain_foreman__cli__plugin('foreman_remote_execution') }
+  it { is_expected.to contain_foreman__plugin('templates') }
+  it { is_expected.to contain_foreman__cli__plugin('foreman_templates') }
+  it { is_expected.to contain_foreman__plugin('column_view') }
+end
+
 # 'spec_overrides' from sync.yml will appear below this line

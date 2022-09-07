85a57a314faab4f43da8e3afa24e794da095c2d1
Joshua Hoblitt <josh@hoblitt.com>
2022-09-07 08:47:20 -0700

    (tel-lt1.tu node) allow login as saluser from saluser@tel-hw1.tu.lsst.org

--- spec/hosts/nodes/tel-lt1.tu.lsst.org_spec.rb
+++ spec/hosts/nodes/tel-lt1.tu.lsst.org_spec.rb
@@ -20,6 +20,13 @@ describe 'tel-lt1.tu.lsst.org', :site do
 
       it { is_expected.to compile.with_all_deps }
       it { is_expected.not_to contain_class('resolv_conf') }
+
+      it do
+        is_expected.to contain_k5login('/home/saluser/.k5login').with(
+          ensure: 'present',
+          principals: ['saluser/tel-hw1.tu.lsst.org@LSST.CLOUD'],
+        )
+      end
     end # on os
   end # on_supported_os
 end # role

f072699de416ac868b9a3e3985058a39bbead7c6
Heinrich Reinking <hreinkinge@gmail.com>
2022-09-22 11:47:09 -0300

    add client class

---
+++ site/profile/manifests/bacula/client.pp
@@ -0,0 +1,38 @@
+# @summary
+#   Installs Bacula File Daemon in clients
+#
+# @param id
+#   Bacula customer Identification string
+#
+
+class profile::bacula::client (
+  String $id = 'null',
+) {
+  include yum
+
+  $bacula_version = '14.0.4'
+
+  #  Import Licenced GPG Bacula Key
+  file { '/etc/pki/rpm-gpg/RPM-GPG-KEY-BACULA':
+    ensure => file,
+    source => "https://www.baculasystems.com/dl/${id}/BaculaSystems-Public-Signature-08-2017.asc",
+  }
+
+  #  Bacula Enterprise Repository
+  yumrepo { 'bacula':
+    ensure   => 'present',
+    baseurl  => "https://www.baculasystems.com/dl/${id}/rpms/bin/${bacula_version}/rhel7-64/",
+    descr    => 'Bacula Enterprise Repository',
+    enabled  => true,
+    gpgcheck => '1',
+    gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-BACULA',
+    require  => File['/etc/pki/rpm-gpg/RPM-GPG-KEY-BACULA'],
+  }
+
+  #  Fetch BIM - Bacula Installation Manager
+  archive { '/opt/bee_installation_manager':
+    ensure => present,
+    source => 'https://www.baculasystems.com/ml/bee_installation_manager',
+    mode   => '0755',
+  }
+}

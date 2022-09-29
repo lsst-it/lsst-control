a3ff0406b2d22ea44edbe9d2f13e7794dc9ce140
Heinrich Reinking <hreinkinge@gmail.com>
2022-09-29 15:47:49 -0300

    add client management

--- site/profile/manifests/bacula/client.pp
+++ site/profile/manifests/bacula/client.pp
@@ -4,18 +4,45 @@
 # @param id
 #   Bacula customer Identification string
 #
+# @param version
+#   Bacula's FD Version'
+#
+# @param bacula_dir
+#   Bacula Director
+#
+# @param bacula_pwd
+#   Bacula Enrollment Pasword
+#
 
 class profile::bacula::client (
   String $id = 'null',
   String $version = 'null',
+  String $bacula_dir = 'null',
+  String $bacula_pwd = 'null'
 ) {
   include yum
 
-  $bacula_packages = [
-    "bacula-enterprise-libs-${version}-22060319.el7.x86_64",
-    "bacula-enterprise-client-${version}-22060319.el7.x86_64",
-  ]
+  $fqdn = $facts[fqdn]
+  $bacula_root = '/opt/bacula'
+  $bacula_fd_conf = @("BACULACONF")
+    FileDaemon {
+      Name = ${fqdn}-fd
+      FDPort = 9102
+      WorkingDirectory = "${bacula_root}/working"
+      PIDDirectory = "${bacula_root}/working"
+      PluginDirectory = "${bacula_root}/plugins"
+      MaximumConcurrentJobs = 20
+    }
+    Director {
+      Name = ${bacula_dir}-dir
+      Password = "${bacula_pwd}"
+    }
+    Messages {
+      Name = Default
+      Director = ${fqdn}-dir = all, !skipped, !restored, !verified, !saved
+    }
 
+    |BACULACONF
   #  Import Licenced GPG Bacula Key
   file { '/etc/pki/rpm-gpg/RPM-GPG-KEY-BACULA':
     ensure => file,
@@ -32,8 +59,14 @@ class profile::bacula::client (
     gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-BACULA',
     require  => File['/etc/pki/rpm-gpg/RPM-GPG-KEY-BACULA'],
   }
-
-  package { $bacula_packages:
-    ensure => 'present',
+  #  Fetch BIM - Bacula Installation Manager
+  archive { '/opt/bee_installation_manager':
+    ensure => present,
+    source => 'https://www.baculasystems.com/ml/bee_installation_manager',
+  }
+  exec { "bash /opt/bee_installation_manager --director it-bacula-dir --download_area ${id} --version ${version} --silent":
+    cwd    => '/var/tmp',
+    path   => ['/sbin', '/usr/sbin', '/bin'],
+    unless => "test -d ${bacula_root}",
   }
 }

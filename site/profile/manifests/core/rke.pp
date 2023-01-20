1e799ae049817e9a2495b514cc3f3b0685ab0640
Joshua Hoblitt <josh@hoblitt.com>
2023-01-20 15:37:39 -0700

    (manke cluster) bump rke to 1.4.1

--- site/profile/manifests/core/rke.pp
+++ site/profile/manifests/core/rke.pp
@@ -48,6 +48,7 @@ class profile::core::rke (
     '1.3.3'  => '61088847d80292f305e233b7dff4ac8e47fefdd726e5245052450bf05da844aa',
     '1.3.6'  => 'c02a8dd7405e3729e004bb1d551fda4c1437f5e0e8279ea67efba8056c0d4898',
     '1.3.12' => '579da2206aec09cadccd8d6f4818861e78a256b6ae550a229335e500a472bd50',
+    '1.4.1'  => '0db4be307e64549fe2e72b955562df52335b9895cc7ec838927991403f86c7c2',
     default  => undef,
   }
   unless ($rke_checksum) {

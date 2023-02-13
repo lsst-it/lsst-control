2e07844467251e8bdb86c3afb9c41577c0436d42
Joshua Hoblitt <josh@hoblitt.com>
2023-02-13 15:52:31 -0700

    (cluster/manke) fwv

---
+++ spec/hosts/nodes/manke01.ls.lsst.org_spec.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'manke01.ls.lsst.org', :site do
+  alma8 = FacterDB.get_facts({ operatingsystem: 'AlmaLinux', operatingsystemmajrelease: '8' }).first
+  # rubocop:disable Naming/VariableNumber
+  { 'almalinux-8-x86_64': alma8 }.each do |os, facts|
+    # rubocop:enable Naming/VariableNumber
+    context "on #{os}" do
+      let(:facts) { override_facts(facts, fqdn: 'manke01.ls.lsst.org') }
+
+      let(:node_params) do
+        {
+          role: 'rke',
+          site: 'ls',
+          cluster: 'manke',
+        }
+      end
+      let(:vlan_id) { 2505 }
+      let(:rt_id) { vlan_id }
+
+      it { is_expected.to compile.with_all_deps }
+
+      it do
+        is_expected.to contain_class('clustershell').with(
+          groupmembers: {
+            'manke' => {
+              'group' => 'manke',
+              'member' => 'manke[01-10]',
+            },
+          },
+        )
+      end
+
+      it do
+        is_expected.to contain_class('profile::core::rke').with(
+          enable_dhcp: true,
+          version: '1.3.12',
+        )
+      end
+
+      it do
+        is_expected.to contain_class('cni::plugins').with(
+          version: '1.2.0',
+          checksum: 'f3a841324845ca6bf0d4091b4fc7f97e18a623172158b72fc3fdcdb9d42d2d37',
+          enable: ['macvlan'],
+        )
+      end
+    end # on os
+  end # on_supported_os
+end

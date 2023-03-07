6f0fce4c8146ddebdb6dd303a27394e047746f9d
Heinrich Reinking <hreinkinge@gmail.com>
2023-03-07 10:59:18 -0300

    (profile::archive::common) handle python per OS

--- spec/classes/archive/commmon_spec.rb
+++ spec/classes/archive/commmon_spec.rb
@@ -19,13 +19,6 @@ describe 'profile::archive::common' do
         it { is_expected.to contain_package(p) }
       end
 
-      %w[
-        docker-compose
-        cryptography
-      ].each do |p|
-        it { is_expected.to contain_python__pip(p) }
-      end
-
       it { is_expected.to contain_accounts__user('arc').with_uid('61000') }
       it { is_expected.to contain_accounts__user('atadbot').with_uid('61002') }
       it { is_expected.to contain_group('docker-foo').with_gid('70014') }

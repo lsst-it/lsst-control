ec42e1fb8f8690679cfabd52f1ae4f2efaedca45
Heinrich Reinking <hreinkinge@gmail.com>
2023-03-08 08:45:06 -0300

    (profile::archive::common) remove python install

--- site/profile/manifests/archive/common.pp
+++ site/profile/manifests/archive/common.pp
@@ -32,29 +32,6 @@ class profile::archive::common (
     ensure_packages($packages)
   }
 
-  if fact('os.architecture') == 'x86_64' {
-    if fact('os.release.major') == '7' {
-      class { 'python':
-        version     => 'python36',
-        pip         => 'present',
-        dev         => 'present',
-        venv        => 'present',
-        python_pips => $python_pips,
-      }
-    }
-    if fact('os.release.major') >= '8' {
-      package { 'python3': }
-      -> alternatives { 'python':
-        path => '/usr/bin/python3',
-      }
-    }
-    if fact('os.release.major') >= '9' {
-      alternative_entry { '/usr/bin/python3':
-        ensure  => present,
-        altlink => '/usr/bin/python',
-      }
-    }
-  }
   if $user_list {
     ensure_resources('accounts::user', $user_list)
   }

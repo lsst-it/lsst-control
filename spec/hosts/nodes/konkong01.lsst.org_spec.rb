6ff89dfef70ef38d0f510b4469a149e496f50c35
Carlos Barria <cbarria@lsst.org>
2023-03-10 10:58:21 -0300

    (konkong) initial network config and test

---
+++ spec/hosts/nodes/konkong01.lsst.org_spec.rb
@@ -0,0 +1,108 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'konkong01.ls.lsst.org', :site do
+  alma8 = FacterDB.get_facts({ operatingsystem: 'AlmaLinux', operatingsystemmajrelease: '8' }).first
+  # rubocop:disable Naming/VariableNumber
+  { 'almalinux-8-x86_64': alma8 }.each do |os, facts|
+    # rubocop:enable Naming/VariableNumber
+    context "on #{os}" do
+      let(:facts) { override_facts(facts, fqdn: 'konkong01.ls.lsst.org') }
+      let(:node_params) do
+        {
+          role: 'rke',
+          site: 'ls',
+          cluster: 'konkong',
+        }
+      end
+      let(:vlan_id) { 2505 }
+      let(:rt_id) { vlan_id }
+
+      include_context 'with nm interface'
+
+      it { is_expected.to compile.with_all_deps }
+
+      it do
+        is_expected.to contain_class('profile::core::sysctl::rp_filter').with_enable(false)
+      end
+
+      it do
+        is_expected.to contain_class('clustershell').with(
+          groupmembers: {
+            'konkong' => {
+              'group' => 'konkong',
+              'member' => 'konkong[01-03]',
+            },
+          },
+        )
+      end
+
+      it do
+        is_expected.to contain_class('profile::core::rke').with(
+          enable_dhcp: true,
+          version: '1.3.12',
+        )
+      end
+
+      it do
+        is_expected.to contain_class('cni::plugins').with(
+          version: '1.2.0',
+          checksum: 'f3a841324845ca6bf0d4091b4fc7f97e18a623172158b72fc3fdcdb9d42d2d37',
+          enable: ['macvlan'],
+        )
+      end
+
+      it { is_expected.to have_network__interface_resource_count(0) }
+      it { is_expected.to have_profile__nm__connection_resource_count(7) }
+
+      %w[
+        eno1np0
+        eno2np1
+        enp4s0f3u2u2c2
+        enp129s0f1
+      ].each do |i|
+        context "with #{name}" do
+          let(:interface) { i }
+
+          it_behaves_like 'nm disabled interface'
+        end
+      end
+
+      context 'with enp129s0f0' do
+        let(:interface) { 'enp129s0f0' }
+
+        it_behaves_like 'nm named interface'
+        it_behaves_like 'nm dhcp interface'
+        it { expect(nm_keyfile['connection']['type']).to eq('ethernet') }
+        it { expect(nm_keyfile['connection']['autoconnect']).to be_nil }
+      end
+
+      context 'with enp129s0f1.2505' do
+        let(:interface) { 'enp129s0f1.2505' }
+
+        it_behaves_like 'nm named interface'
+        it { expect(nm_keyfile['connection']['type']).to eq('vlan') }
+        it { expect(nm_keyfile['connection']['autoconnect']).to be_nil }
+        it { expect(nm_keyfile['connection']['master']).to eq('br2505') }
+        it { expect(nm_keyfile['connection']['slave-type']).to eq('bridge') }
+      end
+
+      context 'with br2505' do
+        let(:interface) { 'br2505' }
+
+        it_behaves_like 'nm named interface'
+        it { expect(nm_keyfile['connection']['type']).to eq('bridge') }
+        it { expect(nm_keyfile['connection']['autoconnect']).to be_nil }
+        it { expect(nm_keyfile['bridge']['stp']).to be false }
+        it { expect(nm_keyfile['ipv4']['method']).to eq('disabled') }
+        it { expect(nm_keyfile['ipv4']['route1']).to eq('139.229.153.0/24') }
+        it { expect(nm_keyfile['ipv4']['route1_options']).to eq('table=2505') }
+        it { expect(nm_keyfile['ipv4']['route2']).to eq('0.0.0.0/0,139.229.153.254') }
+        it { expect(nm_keyfile['ipv4']['route2_options']).to eq('table=2505') }
+        it { expect(nm_keyfile['ipv4']['routing-rule1']).to eq('priority 100 from 139.229.153.64/26 table 2505') }
+        it { expect(nm_keyfile['ipv6']['method']).to eq('disabled') }
+      end
+    end # on os
+  end # on_supported_os
+end

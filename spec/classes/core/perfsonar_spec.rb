b248e95ff74e9980473992028b5cdf4cddea199c
Joshua Hoblitt <josh@hoblitt.com>
2023-04-18 11:44:55 -0700

    (profile::core::perfsonar) add trivial spec

---
+++ spec/classes/core/perfsonar_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'profile::core::perfsonar' do
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) { facts }
+
+      it { is_expected.to compile.with_all_deps }
+    end
+  end
+end

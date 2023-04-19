3b60ebfa27b417fe58a24cf1c9d4560c5a99aeae
Joshua Hoblitt <josh@hoblitt.com>
2023-04-19 16:52:05 -0700

    (node/lsstcam-daq-mgt.ls.lsst.org) add mgt shelf interface

--- spec/hosts/nodes/lsstcam-daq-mgt.ls.lsst.org_spec.rb
+++ spec/hosts/nodes/lsstcam-daq-mgt.ls.lsst.org_spec.rb
@@ -28,7 +28,6 @@ describe 'lsstcam-daq-mgt.ls.lsst.org', :site do
         eno1np0
         eno2np1
         enp4s0f3u2u2c2
-        enp129s0f1
         enp197s0f1
       ].each do |i|
         context "with #{i}" do
@@ -36,6 +35,17 @@ describe 'lsstcam-daq-mgt.ls.lsst.org', :site do
 
           it_behaves_like 'nm disabled interface'
         end
+      end
+
+      context 'with enp129s0f1' do
+        let(:interface) { 'enp129s0f1' }
+
+        it_behaves_like 'nm enabled interface'
+        it_behaves_like 'nm ethernet interface'
+        it { expect(nm_keyfile['ipv4']['address1']).to eq('192.168.100.1/24') }
+        it { expect(nm_keyfile['ipv4']['ignore-auto-dns']).to be true }
+        it { expect(nm_keyfile['ipv4']['method']).to eq('manual') }
+        it { expect(nm_keyfile['ipv6']['method']).to eq('disabled') }
       end
 
       context 'with enp129s0f0' do
@@ -63,6 +73,7 @@ describe 'lsstcam-daq-mgt.ls.lsst.org', :site do
         it { expect(nm_keyfile['ipv4']['address1']).to eq('192.168.100.1/24') }
         it { expect(nm_keyfile['ipv4']['ignore-auto-dns']).to be true }
         it { expect(nm_keyfile['ipv4']['method']).to eq('manual') }
+        it { expect(nm_keyfile['ipv6']['method']).to eq('disabled') }
       end
 
       it { is_expected.to contain_class('nfs::server').with_nfs_v4(false) }

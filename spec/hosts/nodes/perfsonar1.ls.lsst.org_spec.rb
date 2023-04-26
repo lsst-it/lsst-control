a08b51d5479068015ce1f08cab7339dc4c38d398
Joshua Hoblitt <josh@hoblitt.com>
2023-04-26 10:56:58 -0700

    (node/perfsonar1.ls.lsst.org) add node spec

---
+++ spec/hosts/nodes/perfsonar1.ls.lsst.org_spec.rb
@@ -0,0 +1,180 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'perfsonar1.cp.lsst.org', :site do
+  on_supported_os.each do |os, facts|
+    # XXX networking needs to be updated to support EL8+
+    next unless os =~ %r{centos-7-x86_64}
+
+    context "on #{os}" do
+      let(:facts) { override_facts(facts, fqdn: 'perfsonar1.ls.lsst.org') }
+      let(:node_params) do
+        {
+          role: 'perfsonar',
+          site: 'ls',
+        }
+      end
+
+      it { is_expected.to contain_file('/etc/NetworkManager/dispatcher.d/50-em1').with_content(%r{rx 2047 tx 511}) }
+      it { is_expected.to contain_file('/etc/NetworkManager/dispatcher.d/50-p2p1').with_content(%r{.*rx 4096 tx 4096.*}) }
+      it { is_expected.to contain_file('/etc/NetworkManager/dispatcher.d/50-p2p2').with_content(%r{.*rx 4096 tx 4096.*}) }
+
+      it { is_expected.to compile.with_all_deps }
+
+      %w[p2p1 p2p2].each do |i|
+        it do
+          is_expected.to contain_network__interface(i).with(
+            bootproto: 'none',
+            onboot: 'yes',
+            type: 'Ethernet',
+            mtu: '9000',
+          )
+        end
+      end
+
+      it do
+        is_expected.to contain_network__interface('p2p1.360').with(
+          bootproto: 'none',
+          onboot: 'yes',
+          vlan: 'yes',
+          type: 'vlan',
+          ipaddress: '139.229.140.135',
+          netmask: '255.255.255.254',
+          nozeroconf: 'yes',
+          mtu: '9000',
+        )
+      end
+
+      it do
+        is_expected.to contain_network__interface('p2p1.726').with(
+          bootproto: 'none',
+          onboot: 'yes',
+          vlan: 'yes',
+          type: 'vlan',
+          ipaddress: '10.7.26.2',
+          netmask: '255.255.255.0',
+          nozeroconf: 'yes',
+          mtu: '9000',
+        )
+      end
+
+      it do
+        is_expected.to contain_network__interface('p2p1.728').with(
+          bootproto: 'none',
+          onboot: 'yes',
+          vlan: 'yes',
+          type: 'vlan',
+          ipaddress: '10.7.28.1',
+          netmask: '255.255.255.0',
+          nozeroconf: 'yes',
+          mtu: '9000',
+        )
+      end
+
+      it do
+        is_expected.to contain_network__interface('p2p2.370').with(
+          bootproto: 'none',
+          onboot: 'yes',
+          vlan: 'yes',
+          type: 'vlan',
+          ipaddress: '139.229.140.137',
+          netmask: '255.255.255.254',
+          nozeroconf: 'yes',
+          mtu: '9000',
+        )
+      end
+
+      it do
+        is_expected.to contain_network__interface('p2p2.727').with(
+          bootproto: 'none',
+          onboot: 'yes',
+          vlan: 'yes',
+          type: 'vlan',
+          ipaddress: '10.7.27.2',
+          netmask: '255.255.255.0',
+          nozeroconf: 'yes',
+          mtu: '9000',
+        )
+      end
+
+      it do
+        is_expected.to contain_network__interface('p2p2.729').with(
+          bootproto: 'none',
+          onboot: 'yes',
+          vlan: 'yes',
+          type: 'vlan',
+          ipaddress: '10.7.29.1',
+          netmask: '255.255.255.0',
+          nozeroconf: 'yes',
+          mtu: '9000',
+        )
+      end
+
+      context 'with p2p1.360 routes' do
+        let(:via) { '139.229.140.134' }
+
+        it do
+          is_expected.to contain_network__mroute('p2p1.360').with(
+            routes: [
+              '139.229.140.134/31' => 'p2p1.360',
+              '139.229.140.0/22' => via,
+              '139.229.144.248/32' => via,
+              '141.142.129.86/31' => via,
+              '141.142.141.64/26' => via,
+              '141.142.176.0/24' => via,
+              '141.142.180.0/24' => via,
+              '141.142.181.0/24' => via,
+              '141.142.191.1/32' => via,
+              '141.142.237.0/24' => via,
+              '141.142.238.0/23' => via,
+              '10.128.0.0/20' => via,
+              '10.125.0.0/20' => via,
+              '198.17.196.0/24' => via,
+              '198.32.252.39/32' => via,
+              '198.32.252.192/31' => via,
+              '198.32.252.208/31' => via,
+              '198.32.252.210/31' => via,
+              '198.32.252.216/31' => via,
+              '198.32.252.218/31' => via,
+              '198.32.252.232/31' => via,
+              '198.32.252.234/31' => via,
+              '199.36.153.8/30' => via,
+              '134.79.235.226/32' => via,
+              '134.79.235.242/32' => via,
+            ],
+          )
+        end
+      end
+
+      context 'with p2p2.370 routes' do
+        let(:via) { '139.229.140.136' }
+
+        it do
+          is_expected.to contain_network__mroute('p2p2.370').with(
+            routes: [
+              '139.229.140.136/31' => 'p2p2.370',
+              '139.229.140.0/22' => via,
+              '139.229.144.248/32' => via,
+              '141.142.129.84/31' => via,
+              '141.142.141.64/26' => via,
+              '141.142.176.0/24' => via,
+              '141.142.180.0/24' => via,
+              '141.142.181.0/24' => via,
+              '141.142.191.2/32' => via,
+              '141.142.237.0/24' => via,
+              '141.142.238.0/23' => via,
+              '10.128.0.0/20' => via,
+              '10.125.0.0/20' => via,
+              '198.17.196.0/24' => via,
+              '198.32.252.194/31' => via,
+              '199.36.153.8/30' => via,
+              '134.79.235.226/32' => via,
+              '134.79.235.242/32' => via,
+            ],
+          )
+        end
+      end
+    end # on os
+  end # on_supported_os
+end

088484c20597fcc279a2c21d6f6ba26e1f15e92d
Joshua Hoblitt <josh@hoblitt.com>
2023-05-04 11:52:57 -0700

    (role/net-dx) add basic spec

---
+++ spec/hosts/roles/net_dx_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+role = 'net-dx'
+
+describe "#{role} role" do
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) do
+        facts.merge(
+          fqdn: self.class.description,
+        )
+      end
+
+      let(:node_params) do
+        {
+          role: role,
+          site: site,
+        }
+      end
+
+      lsst_sites.each do |site|
+        describe "#{role}.#{site}.lsst.org", :site do
+          let(:site) { site }
+
+          it { is_expected.to compile.with_all_deps }
+
+          include_examples 'common', facts: facts
+          include_examples 'x2go packages'
+        end # host
+      end # lsst_sites
+    end # on os
+  end # on_supported_os
+end # role

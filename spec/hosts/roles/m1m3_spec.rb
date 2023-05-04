4c9d72ae8b20cf7ffa2b1d8a0e4e7e6f275cfbe3
Joshua Hoblitt <josh@hoblitt.com>
2023-05-04 12:32:57 -0700

    (role/m1m3) add basic spec

---
+++ spec/hosts/roles/m1m3_spec.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+role = 'm1m3'
+
+describe "#{role} role" do
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) do
+        facts.merge(
+          fqdn: self.class.description,
+        )
+      end
+
+      let(:node_params) do
+        {
+          role: role,
+          site: site,
+        }
+      end
+
+      lsst_sites.each do |site|
+        describe "#{role}.#{site}.lsst.org", :site do
+          let(:site) { site }
+
+          it { is_expected.to compile.with_all_deps }
+
+          include_examples 'common', facts: facts
+          include_examples 'x2go packages'
+          it { is_expected.to contain_class('mate') }
+
+          %w[
+            catch-devel
+            yaml-cpp-devel
+          ].each do |pkg|
+            it do
+              is_expected.to contain_package(pkg)
+            end
+          end
+        end # host
+      end # lsst_sites
+    end # on os
+  end # on_supported_os
+end # role

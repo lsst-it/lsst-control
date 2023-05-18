89e67b440d7f9e23bd6a0bdb93b2037c67b9337a
Joshua Hoblitt <josh@hoblitt.com>
2023-05-18 09:17:27 -0700

    (profile::core::docker) bump docker-ce to 23.0.6

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -449,7 +449,7 @@ shared_examples 'puppet_master' do
 end
 
 shared_examples 'docker' do
-  docker_version = '20.10.21'
+  docker_version = '23.0.6'
 
   it do
     is_expected.to contain_class('docker').with(
@@ -474,13 +474,13 @@ shared_examples 'docker' do
 
   it do
     is_expected.to contain_yum__versionlock('containerd.io').with(
-      version: '1.6.10',
+      version: '1.6.21',
     )
   end
 
   it do
     is_expected.to contain_yum__versionlock('docker-scan-plugin').with(
-      version: '0.21.0',
+      version: '0.23.0',
     )
   end
 end

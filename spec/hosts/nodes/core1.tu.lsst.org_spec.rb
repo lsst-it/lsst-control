177c9d3c988050dd0ca37cd849e32d15ac8e4b33
Joshua Hoblitt <josh@hoblitt.com>
2023-06-27 15:52:44 -0700

    (node/core1.tu.lsst.org) set bmc static ip to 140.252.145.138

---
+++ spec/hosts/nodes/core1.tu.lsst.org_spec.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'core1.tu.lsst.org', :sitepp do
+  on_supported_os.each do |os, facts|
+    # XXX networking needs to be updated to support EL8+
+    next unless os =~ %r{centos-7-x86_64}
+
+    context "on #{os}" do
+      let(:facts) do
+        override_facts(facts,
+                       fqdn: 'core1.tu.lsst.org',
+                       is_virtual: false,
+                       dmi: {
+                         'product' => {
+                           'name' => 'PowerEdge R440',
+                         },
+                       })
+      end
+      let(:node_params) do
+        {
+          role: 'hypervisor',
+          site: 'tu',
+        }
+      end
+
+      it { is_expected.to compile.with_all_deps }
+
+      include_examples('baremetal',
+                       bmc: {
+                         lan1: {
+                           ip: '140.252.146.138',
+                           netmask: '255.255.255.192',
+                           gateway: '140.252.146.129',
+                           type: 'static',
+                         },
+                       })
+
+      it do
+        is_expected.to contain_network__interface('em1').with(
+          ipaddress: '140.252.146.68',
+        )
+      end
+    end # on os
+  end # on_supported_os
+end # role

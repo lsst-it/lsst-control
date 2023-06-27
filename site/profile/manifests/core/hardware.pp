f2b3a56ea9a1f1cd3c5aabe61f1bb33656be753e
Joshua Hoblitt <josh@hoblitt.com>
2023-06-27 12:03:27 -0700

    (profile::core::hardware) use fact() function for consistency

--- site/profile/manifests/core/hardware.pp
+++ site/profile/manifests/core/hardware.pp
@@ -2,7 +2,7 @@
 #   Manage packages and services needed on different hardware platforms.
 class profile::core::hardware {
   # lint:ignore:case_without_default
-  case $facts.dig('dmi', 'product', 'name') {
+  case fact('dmi.product.name') {
     # XXX add a fact to check /sys/class/ipmi/ instead of white listing specific models
     /PowerEdge/: {
       include ipmi

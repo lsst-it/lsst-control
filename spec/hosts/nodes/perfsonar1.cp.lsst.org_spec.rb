2204b0aa3314fa9e37360f4ae47fd1f34457b441
Carlos Barria <cbarria@lsst.org>
2023-07-12 16:29:47 -0400

    (perfsonar.cp) Add network config for EL9

--- spec/hosts/nodes/perfsonar1.cp.lsst.org_spec.rb
+++ spec/hosts/nodes/perfsonar1.cp.lsst.org_spec.rb
@@ -4,8 +4,7 @@ require 'spec_helper'
 
 describe 'perfsonar1.cp.lsst.org', :sitepp do
   on_supported_os.each do |os, facts|
-    # XXX networking needs to be updated to support EL8+
-    next unless os =~ %r{centos-7-x86_64}
+    next if os =~ %r{centos-7-x86_64}
 
     context "on #{os}" do
       let(:facts) do
@@ -24,66 +23,91 @@ describe 'perfsonar1.cp.lsst.org', :sitepp do
           site: 'cp',
         }
       end
-      let(:lhn_vlan_id) { 1120 }
 
       it { is_expected.to compile.with_all_deps }
 
       include_examples 'baremetal'
+      include_context 'with nm interface'
 
       it { is_expected.to contain_class('Profile::Core::Nm_dispatch') }
       it { is_expected.to contain_file('/etc/NetworkManager/dispatcher.d/50-eno1').with_content(%r{.*rx 2047 tx 2047.*}) }
       it { is_expected.to contain_file('/etc/NetworkManager/dispatcher.d/50-enp1s0f0').with_content(%r{.*rx 4096 tx 4096.*}) }
       it { is_expected.to contain_file('/etc/NetworkManager/dispatcher.d/50-enp1s0f1').with_content(%r{.*rx 4096 tx 4096.*}) }
 
-      it do
-        is_expected.to contain_network__interface('enp1s0f0').with(
-          bootproto: 'none',
-          onboot: 'yes',
-          type: 'Ethernet',
-          mtu: '9000',
-        )
+      it { is_expected.to have_network__interface_resource_count(0) }
+      it { is_expected.to have_profile__nm__connection_resource_count(9) }
+
+      %w[
+        enp5s0f3u2u2c2
+        eno2np1
+      ].each do |i|
+        context "with #{i}" do
+          let(:interface) { i }
+
+          it_behaves_like 'nm disabled interface'
+        end
       end
 
-      it do
-        is_expected.to contain_network__interface('enp1s0f1').with(
-          bootproto: 'none',
-          onboot: 'yes',
-          type: 'Ethernet',
-          mtu: '9000',
-        )
+      context 'with eno1np0' do
+        let(:interface) { 'eno1np0' }
+
+        it_behaves_like 'nm named interface'
+        it { expect(nm_keyfile['connection']['autoconnect']).to be true }
+
+        it_behaves_like 'nm dhcp interface'
+        it_behaves_like 'nm ethernet interface'
       end
 
-      it do
-        is_expected.to contain_network__interface("enp1s0f0.#{lhn_vlan_id}").with(
-          bootproto: 'none',
-          onboot: 'yes',
-          type: 'vlan',
-          vlan: 'yes',
-          ipaddress: '139.229.164.220',
-          netmask: '255.255.255.254',
-          nozeroconf: 'yes',
-          mtu: '9000',
-        )
+      context 'with enp1s0f0' do
+        let(:interface) { 'enp1s0f0' }
+
+        it_behaves_like 'nm named interface'
+        it { expect(nm_keyfile['connection']['autoconnect']).to be false }
+
+        it_behaves_like 'nm no-ip interface'
+        it_behaves_like 'nm ethernet interface'
       end
 
-      it do
-        is_expected.to contain_network__mroute("enp1s0f0.#{lhn_vlan_id}").with(
-          routes: [
-            '139.229.164.0/24' => "enp1s0f0.#{lhn_vlan_id}",
-            '139.229.140.135/32' => '139.229.164.254',
-            '139.229.140.137/32' => '139.229.164.254',
-            '198.32.252.39/32' => '139.229.164.254',
-            '198.32.252.192/31' => '139.229.164.254',
-            '198.32.252.208/31' => '139.229.164.254',
-            '198.32.252.210/31' => '139.229.164.254',
-            '198.32.252.216/31' => '139.229.164.254',
-            '198.32.252.218/31' => '139.229.164.254',
-            '198.32.252.232/31' => '139.229.164.254',
-            '198.32.252.234/31' => '139.229.164.254',
-            '134.79.235.226/32' => '139.229.164.254',
-            '134.79.235.242/32' => '139.229.164.254',
-          ],
-        )
+      context 'with enp1s0f0.1120' do
+        let(:interface) { 'enp1s0f0.1120' }
+
+        it_behaves_like 'nm enabled interface'
+        it_behaves_like 'nm vlan interface', id: 1120, parent: 'enp1s0f0'
+        it_behaves_like 'nm bridge slave interface', master: 'bra1120'
+      end
+
+      context 'with bra1120' do
+        let(:interface) { 'bra1120' }
+
+        it_behaves_like 'nm enabled interface'
+        it_behaves_like 'nm dhcp interface'
+        it_behaves_like 'nm bridge interface'
+      end
+
+      context 'with enp1s0f1' do
+        let(:interface) { 'enp1s0f1' }
+
+        it_behaves_like 'nm named interface'
+        it { expect(nm_keyfile['connection']['autoconnect']).to be false }
+
+        it_behaves_like 'nm no-ip interface'
+        it_behaves_like 'nm ethernet interface'
+      end
+
+      context 'with enp1s0f1.1120' do
+        let(:interface) { 'enp1s0f1.1120' }
+
+        it_behaves_like 'nm enabled interface'
+        it_behaves_like 'nm vlan interface', id: 1120, parent: 'enp1s0f1'
+        it_behaves_like 'nm bridge slave interface', master: 'brb1120'
+      end
+
+      context 'with brb1120' do
+        let(:interface) { 'brb1120' }
+
+        it_behaves_like 'nm enabled interface'
+        it_behaves_like 'nm dhcp interface'
+        it_behaves_like 'nm bridge interface'
       end
     end # on os
   end # on_supported_os

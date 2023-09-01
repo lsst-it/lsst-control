3b2a94d5f1a106c7a8e1d0d3becddcd7afac310b
Glenn Morris <gmorris@slac.stanford.edu>
2023-09-01 14:21:50 -0700

    (profile::ccs::el9) new, for CCS requirements specific to EL9

---
+++ site/profile/manifests/ccs/el9.pp
@@ -0,0 +1,40 @@
+# @summary
+#   EL9-specific requirements.
+#
+# @param rpms
+#   Hash of package/rpm pairs to install.
+# @param pkgurl
+#   String specifying url to fetch binaries from
+# @param pkgurl_user
+#   String specifying username for pkgurl
+# @param pkgurl_pass
+#   String specifying password for pkgurl
+#
+class profile::ccs::el9 (
+  Hash[String,String] $rpms = {
+    'compat-bin' => 'compat-bin-1.0.0-1.el9.noarch.rpm',
+  },
+  String $pkgurl = $profile::ccs::common::pkgurl,
+  String $pkgurl_user = $profile::ccs::common::pkgurl_user,
+  String $pkgurl_pass = $profile::ccs::common::pkgurl_pass,
+) {
+  $rpm_opts = {
+    ensure   => 'latest',
+    provider => 'rpm',
+  }
+
+  $rpms.each |$package, $rpm| {
+    $file = "/var/tmp/${rpm}"
+
+    archive { $file:
+      ensure   => present,
+      source   => "${pkgurl}/${rpm}",
+      username => $pkgurl_user,
+      password => $pkgurl_pass,
+    }
+    package { $package:
+      source => $file,
+      *      => $rpm_opts,
+    }
+  }
+}

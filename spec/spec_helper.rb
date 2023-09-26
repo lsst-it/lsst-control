62cfca73003e04b2a8ec032e5e03fe6d141031e3
Joshua Hoblitt <josh@hoblitt.com>
2023-09-26 13:12:52 -0700

    (role/foreman) disable updating interfaces from puppet facts
    Updating the ip / mac address for interfaces from puppet fact has caused
    strange problems such as when the host grabs an incorrect IP from dhcp
    and then updates the host entry... but the dhcp reservation is not
    changed.  Foreman should be the source of truth and not the running
    state of the host.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -810,6 +810,7 @@ shared_examples 'generic foreman' do
     bmc_credentials_accessible: false,
     default_pxe_item_global: 'discovery',
     host_details_ui: false,
+    ignore_puppet_facts_for_provisioning: true,
     template_sync_associate: 'always',
     template_sync_commit_msg: 'Templates export made by a Foreman user',
     template_sync_dirname: '/',

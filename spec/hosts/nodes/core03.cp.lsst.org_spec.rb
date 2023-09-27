a1ff053f1ad5e9fc21b2e6e77c9927ba7ae33d4d
Joshua Hoblitt <josh@hoblitt.com>
2023-09-27 16:39:15 -0700

    (node/core0[123].cp.lsst.org) add 1114s based servers
    As replacement for the core[123].cp.lsst.org r440s.

---
+++ spec/hosts/nodes/core03.cp.lsst.org_spec.rb
@@ -0,0 +1,96 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'core03.cp.lsst.org', :sitepp do
+  alma9 = FacterDB.get_facts({ operatingsystem: 'AlmaLinux', operatingsystemmajrelease: '9' }).first
+  # rubocop:disable Naming/VariableNumber
+  { 'almalinux-9-x86_64': alma9 }.each do |os, facts|
+    # rubocop:enable Naming/VariableNumber
+    context "on #{os}" do
+      let(:facts) do
+        override_facts(facts,
+                       fqdn: 'core03.cp.lsst.org',
+                       is_virtual: false,
+                       virtual: 'physical',
+                       dmi: {
+                         'product' => {
+                           'name' => 'AS -1114S-WN10RT',
+                         },
+                       })
+      end
+      let(:node_params) do
+        {
+          role: 'hypervisor',
+          site: 'cp',
+        }
+      end
+
+      it { is_expected.to compile.with_all_deps }
+
+      include_examples('baremetal',
+                       bmc: {
+                         lan1: {
+                           ip: '139.229.162.3',
+                           netmask: '255.255.255.0',
+                           gateway: '139.229.162.254',
+                           type: 'static',
+                         },
+                       })
+      include_context 'with nm interface'
+
+      it { is_expected.to have_nm__connection_resource_count(7) }
+
+      %w[
+        eno1np0
+        eno2np1
+        enp5s0f3u2u2c2
+      ].each do |i|
+        context "with #{i}" do
+          let(:interface) { i }
+
+          it_behaves_like 'nm disabled interface'
+        end
+      end
+
+      context 'with enp1s0f0' do
+        let(:interface) { 'enp1s0f0' }
+
+        it_behaves_like 'nm enabled interface'
+        it_behaves_like 'nm ethernet interface'
+        it { expect(nm_keyfile['ipv4']['address1']).to eq('139.229.161.35/28,139.229.161.46') }
+        it { expect(nm_keyfile['ipv4']['dns']).to eq('139.229.160.53;139.229.160.54;139.229.160.55;') }
+        it { expect(nm_keyfile['ipv4']['dns-search']).to eq('cp.lsst.org;') }
+        it { expect(nm_keyfile['ipv4']['method']).to eq('manual') }
+      end
+
+      context 'with enp1s0f1' do
+        let(:interface) { 'enp1s0f1' }
+
+        it_behaves_like 'nm enabled interface'
+        it_behaves_like 'nm ethernet interface'
+        it { expect(nm_keyfile['ipv4']['method']).to eq('disabled') }
+        it { expect(nm_keyfile['ipv6']['method']).to eq('disabled') }
+      end
+
+      context 'with enp1s0f1.1101' do
+        let(:interface) { 'enp1s0f1.1101' }
+
+        it_behaves_like 'nm enabled interface'
+        it { expect(nm_keyfile['vlan']['id']).to eq(1101) }
+        it { expect(nm_keyfile['vlan']['parent']).to eq('enp1s0f1') }
+        it { expect(nm_keyfile['connection']['master']).to eq('br1101') }
+        it { expect(nm_keyfile['connection']['slave-type']).to eq('bridge') }
+      end
+
+      context 'with br1101' do
+        let(:interface) { 'br1101' }
+
+        it_behaves_like 'nm enabled interface'
+        it { expect(nm_keyfile['ipv4']['method']).to eq('disabled') }
+        it { expect(nm_keyfile['ipv6']['method']).to eq('disabled') }
+        it { expect(nm_keyfile['bridge']['stp']).to be(false) }
+      end
+    end # on os
+  end # on_supported_os
+end # role

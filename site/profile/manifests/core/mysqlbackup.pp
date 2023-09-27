e4cc1f0e58a7d1779a2ce26e4b4c14ab95e415e5
Cristian Silva <cristiansilva@gmail.com>
2023-09-27 17:14:41 -0300

    (mysql) add class fix

--- site/profile/manifests/core/mysqlbackup.pp
+++ site/profile/manifests/core/mysqlbackup.pp
@@ -1,17 +1,42 @@
 # @summary
 #   mysqldump class
 #
-# @param packages
-#  An array of packages to install
+# @param databases
+#   A string or an array containing the names of the database(s) to back up. 
+#   Defaults the resource $title. Use 'all' to back up all databases.
 #
+# @param mysql_user
+#   MySQL user with rights to dump the specified databases. Defaults to 'root'.
+#
+# @param mysql_passwd
+#   Password for the above user.
+#
+# @param mysqldump_extra_params
+#   Extra parameters to pass to mysqldump. Defaults to '--lock-tables', which 
+#   works for MyISAM tables. For InnoDB tables '--single-transaction' is more 
+#   appropriate.
+#
+# @param hour
+#   Hour(s) when mysqldump gets run. String or Integer or an array of them.
+#   Defaults to '01'.
+#
+# @param minute
+#   Minute(s) when mysqldump gets run. Defaults to '10'.
+*
+# @param weekday
+#   Weekday(s) when mysqldump gets run. Defaults to '*' (all weekdays).
+#
+# @param email
+#   Email address where notifications are sent. Defaults to top-scope variable
+#   $::servermonitor.
 class profile::core::mysqlbackup (
-  $mysql_user,
-  $mysql_passwd,
-  $databases,
+  Optional[String] $mysql_user,
+  Optional[String] $mysql_passwd,
+  Variant[String, Array]  $databases,
   Optional[String] $mysqldump_extra_params,
-  $hour,
-  $minute,
-  $weekday,
+  Variant[Array[String], Array[Integer[0-23]], String, Integer[0-23]] $hour,
+  Variant[Array[String], Array[Integer[0-59]], String, Integer[0-59]] $minute,
+  Variant[Array[String], Array[Integer[0-7]],  String, Integer[0-7]] $weekday,
   Optional[String] $email
 ) {
 

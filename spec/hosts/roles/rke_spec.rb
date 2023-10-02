d97e2181e36f35884f65e864fac14cc1723e7176
Carlos Barria <cbarria@yahoo.com>
2023-10-02 16:23:07 -0300

    (site/dev) set rke client version 1.4.6

--- spec/hosts/roles/rke_spec.rb
+++ spec/hosts/roles/rke_spec.rb
@@ -22,13 +22,6 @@ shared_examples 'generic rke' do
       .with_content(%r{^alias k='kubectl'$})
       .with_content(%r{^complete -o default -F __start_kubectl k$})
   end
-
-  it do
-    is_expected.to contain_class('rke').with(
-      version: '1.3.12',
-      checksum: '579da2206aec09cadccd8d6f4818861e78a256b6ae550a229335e500a472bd50',
-    )
-  end
 end
 
 role = 'rke'
@@ -59,6 +52,13 @@ describe "#{role} role" do
 
         include_examples 'common', facts: facts
         include_examples 'generic rke'
+
+        it do
+          is_expected.to contain_class('rke').with(
+            version: '1.3.12',
+            checksum: '579da2206aec09cadccd8d6f4818861e78a256b6ae550a229335e500a472bd50',
+          )
+        end
       end # host
 
       fqdn = "#{role}.ls.lsst.org"
@@ -72,6 +72,13 @@ describe "#{role} role" do
 
         include_examples 'common', facts: facts
         include_examples 'generic rke'
+
+        it do
+          is_expected.to contain_class('rke').with(
+            version: '1.3.12',
+            checksum: '579da2206aec09cadccd8d6f4818861e78a256b6ae550a229335e500a472bd50',
+          )
+        end
       end # host
 
       fqdn = "#{role}.cp.lsst.org"
@@ -85,6 +92,13 @@ describe "#{role} role" do
 
         include_examples 'common', facts: facts
         include_examples 'generic rke'
+
+        it do
+          is_expected.to contain_class('rke').with(
+            version: '1.3.12',
+            checksum: '579da2206aec09cadccd8d6f4818861e78a256b6ae550a229335e500a472bd50',
+          )
+        end
       end # host
 
       fqdn = "#{role}.dev.lsst.org"
@@ -98,6 +112,13 @@ describe "#{role} role" do
 
         include_examples 'common', facts: facts
         include_examples 'generic rke'
+
+        it do
+          is_expected.to contain_class('rke').with(
+            version: '1.4.6',
+            checksum: '12d8fee6f759eac64b3981ef2822353993328f2f839ac88b3739bfec0b9d818c',
+          )
+        end
       end # host
     end # on os
   end # on_supported_os

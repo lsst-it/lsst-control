c6a1ec7b1049b11a27b07e8885813625f98f5d51
Joshua Hoblitt <josh@hoblitt.com>
2023-11-14 12:36:03 -0700

    (spec) add example of rke role restic specific config

--- spec/hosts/roles/rke_spec.rb
+++ spec/hosts/roles/rke_spec.rb
@@ -19,6 +19,21 @@ shared_examples 'generic rke' do |facts:|
       .with_content(%r{^alias k='kubectl'$})
       .with_content(%r{^complete -o default -F __start_kubectl k$})
   end
+
+  it do
+    is_expected.to contain_restic__repository('awsrepo').with(
+      backup_path: %w[
+        /home/rke
+        /var/lib/rook
+        /etc/kubernetes
+      ],
+      backup_flags: '--exclude=/var/lib/rook/rook-ceph/log',
+      backup_timer: '*-*-* 09:00:00',
+      enable_forget: true,
+      forget_timer: 'Mon..Sun 23:00:00',
+      forget_flags: '--keep-last 20',
+    )
+  end
 end
 
 role = 'rke'

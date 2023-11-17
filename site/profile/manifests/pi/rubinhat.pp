5f5f35f0e455204595be0cd71400b147ce4a5358
Joshua Hoblitt <josh@hoblitt.com>
2023-11-17 14:33:34 -0700

    (profile::pi::rubinhat) fwv

---
+++ site/profile/manifests/pi/rubinhat.pp
@@ -0,0 +1,35 @@
+# @summary
+#   Configuration to enable a Rubin Observatory Raspberry Pi hat.
+#
+class profile::pi::rubinhat {
+  # https://confluence.lsstcorp.org/pages/viewpage.action?spaceKey=LTS&title=Rubin+Raspberry+Pi+Pin+Configuration+Issue
+  profile::pi::config::fragment { 'rubinhat':
+    # lint:ignore:strict_indent
+    content => @("CONTENT"),
+      enable_uart=1
+      dtoverlay=disable-bt
+      dtoverlay=uart1
+      dtoverlay=uart2
+      dtoverlay=uart3
+      dtoverlay=uart4
+      dtoverlay=uart5
+      gpio=18=ip
+      gpio=11,17,23=op,dh
+      gpio=3,7,24=op
+      | CONTENT
+    # lint:endignore
+  }
+
+  if defined(Class['profile::core::docker']) {
+    $group = Class['profile::core::docker']['socket_group']
+  } else {
+    $group = '70014'
+  }
+
+  systemd::udev::rule { 'rubinhat.rules':
+    rules => [
+      "KERNEL==\"ttyS[0-9]*\", GROUP=\"${group}\", MODE=\"0660\"",
+      "KERNEL==\"ttyAMA[0-9]*\", GROUP=\"${group}\", MODE=\"0660\"",
+    ],
+  }
+}
---
+++ spec/support/spec/rubinhat.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+shared_examples 'rubinhat' do
+  it do
+    is_expected.to contain_profile__pi__config__fragment('rubinhat').with(
+      content: <<~CONTENT,
+      enable_uart=1
+      dtoverlay=disable-bt
+      dtoverlay=uart1
+      dtoverlay=uart2
+      dtoverlay=uart3
+      dtoverlay=uart4
+      dtoverlay=uart5
+      gpio=18=ip
+      gpio=11,17,23=op,dh
+      gpio=3,7,24=op
+      CONTENT
+    )
+  end
+
+  it do
+    is_expected.to contain_systemd__udev__rule('rubinhat.rules').with(
+      rules: [
+        'KERNEL=="ttyS[0-9]*", GROUP="70014", MODE="0660"',
+        'KERNEL=="ttyAMA[0-9]*", GROUP="70014", MODE="0660"',
+      ],
+    )
+  end
+end

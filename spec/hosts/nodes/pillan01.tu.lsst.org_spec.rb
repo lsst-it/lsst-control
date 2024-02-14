60c2bfac6695f6d1009e9f4a4995d9cc848ea389
Joshua Hoblitt <josh@hoblitt.com>
2024-02-14 13:19:18 -0700

    (cluster/pillan) bump rke to 1.4.6 (to support k8s v1.25.9-rancher2-2)

--- spec/hosts/nodes/pillan01.tu.lsst.org_spec.rb
+++ spec/hosts/nodes/pillan01.tu.lsst.org_spec.rb
@@ -39,7 +39,7 @@ describe 'pillan01.tu.lsst.org', :sitepp do
       it do
         is_expected.to contain_class('profile::core::rke').with(
           enable_dhcp: true,
-          version: '1.3.12',
+          version: '1.4.6',
         )
       end
 

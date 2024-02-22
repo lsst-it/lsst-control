7bcc8a1fd6abfe6389a1e2bae99dc6a90ed90e9e
Joshua Hoblitt <josh@hoblitt.com>
2024-02-22 08:40:47 -0700

    (role/auxtel-vent-gate) fwv

---
+++ spec/hosts/roles/auxtel_vent_gate_spec.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+role = 'auxtel-vent-gate'
+
+describe "#{role} role" do
+  on_supported_os.each do |os, os_facts|
+    next unless os =~ %r{almalinux-9-x86_64}
+
+    context "on #{os}" do
+      os_facts = override_facts(os_facts,
+                                cpuinfo: {
+                                  'processor' => {
+                                    'Model' => 'Raspberry Pi 4 Model B Rev 1.2',
+                                  },
+                                },
+                                os: {
+                                  'architecture' => 'aarch64',
+                                })
+      let(:facts) { os_facts }
+      let(:node_params) do
+        {
+          role: role,
+          site: site,
+        }
+      end
+
+      lsst_sites.each do |site|
+        fqdn = "#{role}.#{site}.lsst.org"
+        os_facts = override_facts(os_facts, fqdn: fqdn, networking: { fqdn => fqdn })
+
+        describe fqdn, :sitepp do
+          let(:site) { site }
+
+          it { is_expected.to compile.with_all_deps }
+
+          include_examples 'common', os_facts: os_facts
+          include_examples 'docker'
+          include_examples 'gpio', os_facts: os_facts
+          include_examples 'i2c', os_facts: os_facts
+          include_examples 'ftdi'
+          include_examples 'pigpio'
+        end # host
+      end # lsst_sites
+    end # on os
+  end # on_supported_os
+end # role

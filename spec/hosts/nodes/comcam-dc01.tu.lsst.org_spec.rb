625ed1b1e3aa72633ee28324fb152e29a275153b
Joshua Hoblitt <josh@hoblitt.com>
2024-03-11 09:55:47 -0700

    (node/comcam-dc01.tu.lsst.org) add lsst-daq interface

--- spec/hosts/nodes/comcam-dc01.tu.lsst.org_spec.rb
+++ spec/hosts/nodes/comcam-dc01.tu.lsst.org_spec.rb
@@ -30,7 +30,7 @@ describe 'comcam-dc01.tu.lsst.org', :sitepp do
       include_examples 'baremetal'
       include_context 'with nm interface'
 
-      it { is_expected.to have_nm__connection_resource_count(1) }
+      it { is_expected.to have_nm__connection_resource_count(3) }
 
       context 'with ens2f0' do
         let(:interface) { 'ens2f0' }
@@ -38,6 +38,25 @@ describe 'comcam-dc01.tu.lsst.org', :sitepp do
         it_behaves_like 'nm enabled interface'
         it_behaves_like 'nm dhcp interface'
         it_behaves_like 'nm ethernet interface'
+      end
+
+      context 'with ens2f1' do
+        let(:interface) { 'ens2f1' }
+
+        it_behaves_like 'nm enabled interface'
+        it_behaves_like 'nm ethernet interface'
+        it_behaves_like 'nm bridge slave interface', master: 'lsst-daq'
+        it { expect(nm_keyfile['ethtool']['ring-rx']).to eq(4096) }
+        it { expect(nm_keyfile['ethtool']['ring-tx']).to eq(4096) }
+      end
+
+      context 'with lsst-daq' do
+        let(:interface) { 'lsst-daq' }
+
+        it_behaves_like 'nm enabled interface'
+        it_behaves_like 'nm dhcp interface'
+        it_behaves_like 'nm bridge interface'
+        it { expect(nm_keyfile['ipv4']['ignore-auto-dns']).to be true }
       end
     end # on os
   end # on_supported_os

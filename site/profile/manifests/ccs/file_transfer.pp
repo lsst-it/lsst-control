ebf5b3d970a06475fba22817a2ed4a2d48ad816e
Glenn Morris <gmorris@slac.stanford.edu>
2024-03-21 15:09:06 -0700

    (profile::ccs::file_transfer) add option to install s3daemon
    New class parameters:
    (s3daemon) boolean, true to install
    (s3daemon_repo_url, s3daemon_repo_rev, s3daemon_repo_directory)
    strings for repo URL and revision, and install location
    (s3daemon_env_file, s3daemon_env_url, s3daemon_env_access, s3daemon_env_secret)
    strings for environment file location, and contents
    
    When true: install required yum and pip packages; clone repository;
    configure and start s3daemon system service.
    
    (templates/ccs/service.epp) new file
    (templates/ccs/file_transfer/s3daemon_envfile.epp) new file

--- site/profile/manifests/ccs/file_transfer.pp
+++ site/profile/manifests/ccs/file_transfer.pp
@@ -23,8 +23,32 @@
 #   String specifying username for pkgurl
 # @param pkgurl_pass
 #   String specifying password for pkgurl
+# @param s3daemon
+#   Boolean, true to install s3daemon
+# @param s3daemon_repo_url
+#   URL of the s3daemon repo
+# @param s3daemon_repo_rev
+#   Git repository revision of s3daemon to install
+# @param s3daemon_repo_directory
+#   String specifying s3daemon installation directory
+# @param s3daemon_env_file
+#   String specifying s3daemon environment file
+# @param s3daemon_env_url
+#   String giving s3daemon endpoint URL
+# @param s3daemon_env_access
+#   String giving s3daemon access key
+# @param s3daemon_env_secret
+#  String giving s3daemon secret key
 #
 class profile::ccs::file_transfer (
+  String[1] $s3daemon_env_access,
+  String[1] $s3daemon_env_secret,
+  Boolean $s3daemon = false,
+  Stdlib::HTTPUrl $s3daemon_repo_url = 'https://github.com/lsst-dm/s3daemon',
+  Optional[String[1]] $s3daemon_repo_rev = undef,
+  String $s3daemon_repo_directory = '/home/ccs-ipa/s3daemon/git',
+  String $s3daemon_env_file = '/home/ccs-ipa/s3daemon/env',
+  String $s3daemon_env_url = 'https://s3dfrgw.slac.stanford.edu',
   String $directory = '/home/ccs-ipa/bin',
   String $user  = 'ccs-ipa',
   String $group = 'ccs-ipa',
@@ -37,6 +61,124 @@ class profile::ccs::file_transfer (
   String $pkgurl_user = $profile::ccs::common::pkgurl_user,
   String $pkgurl_pass = $profile::ccs::common::pkgurl_pass,
 ) {
+  ## We expect that s3daemon will become the default (and only) method
+  ## some time soonish.
+  if $s3daemon {
+    case fact('os.release.major') {
+      '7': {
+        $pip_extra_packages = [
+          'aiosignal',
+          'async-timeout',
+          'attrs',
+          'charset-normalizer',
+          'multidict',
+          'typing_extensions',
+          'yarl',
+        ]
+        $yum_extra_packages = []
+      }
+      '8': {
+        $pip_extra_packages = [
+          'aiosignal',
+        ]
+        $yum_extra_packages = [
+          'python3-async-timeout',
+          'python3-attrs',
+          'python3-charset-normalizer',
+          'python3-multidict',
+          'python3-typing-extensions',
+          'python3-yarl',
+        ]
+      }
+      default: {
+        $pip_extra_packages = []
+        $yum_extra_packages = [
+          'python3-aiosignal',
+          'python3-async-timeout',
+          'python3-attrs',
+          'python3-charset-normalizer',
+          'python3-multidict',
+          'python3-typing-extensions',
+          'python3-yarl',
+        ]
+      }
+    }
+
+    $yum_packages = $yum_extra_packages + [
+      'python3',
+      'python3-pip',
+      'python3-devel',
+    ]
+
+    $pip_packages = $pip_extra_packages + [
+      'idna_ssl',
+      'aiobotocore',
+    ]
+
+    package { $yum_packages:
+      ensure => 'present',
+    }
+
+    package { $pip_packages:
+      ensure   => 'present',
+      provider => 'pip3',
+    }
+
+    $repo_parent = "${dirname($s3daemon_repo_directory)}"
+    $env_parent = "${dirname($s3daemon_env_file)}"
+
+    $parents = [$repo_parent, $env_parent]
+
+    $parents.unique.each | String $direc | {
+      file { $direc:
+        ensure => directory,
+        mode   => '0755',
+        owner  => $user,
+        group  => $group,
+      }
+    }
+
+    $envfile_epp_vars = {
+      url    => $s3daemon_env_url,
+      access => $s3daemon_env_access,
+      secret => $s3daemon_env_secret,
+    }
+
+    file { $s3daemon_env_file:
+      content => epp("${module_name}/ccs/file_transfer/s3daemon_envfile.epp", $envfile_epp_vars),
+      owner   => $user,
+      group   => $group,
+      mode    => '0600',
+      require => File[$env_parent],
+    }
+
+    vcsrepo { $s3daemon_repo_directory:
+      ensure   => latest,
+      provider => git,
+      source   => $s3daemon_repo_url,
+      revision => $s3daemon_repo_rev,
+      user     => $user,
+      require  => File[$repo_parent],
+    }
+
+    $epp_vars = {
+      desc    => 's3daemon file transfer service',
+      user    => $user,
+      group   => $group,
+      cmd     => "/usr/bin/python3 ${s3daemon_repo_directory}/python/s3daemon/s3daemon.py",
+      workdir => "/home/${user}",
+      envfile => $s3daemon_env_file,
+    }
+
+    systemd::unit_file { 's3daemon.service':
+      content => epp("${module_name}/ccs/service.epp", $epp_vars),
+    }
+    ~> service { 's3daemon':
+      ensure => 'running',
+      enable => true,
+    }
+  }                           # s3daemon
+
   file { $directory:
     ensure => directory,
     owner  => $user,

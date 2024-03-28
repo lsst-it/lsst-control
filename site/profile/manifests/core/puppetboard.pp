8c10c5f1095ae43ccd4c25453c36febc34956184
Joshua Hoblitt <josh@hoblitt.com>
2024-03-28 11:14:28 -0700

    (profile::core::puppetboard) fwv

---
+++ site/profile/manifests/core/puppetboard.pp
@@ -0,0 +1,22 @@
+# @summary
+#   Install puppetboard https://github.com/voxpupuli/puppetboard
+#
+# @param secret_key
+#  The secret key to use for the puppetboard
+#
+class profile::core::puppetboard (
+  Sensitive $secret_key,
+) {
+  docker::image { 'ghcr.io/voxpupuli/puppetboard': }
+
+  docker::run { 'puppetboard':
+    image => 'ghcr.io/voxpupuli/puppetboard',
+    env   => [
+      'PUPPETDB_HOST=127.0.0.1',
+      'PUPPETDB_PORT=8080',
+      'PUPPETBOARD_PORT=8088',
+      "SECRET_KEY=${secret_key.unwrap}",
+    ],
+    net   => 'host',
+  }
+}

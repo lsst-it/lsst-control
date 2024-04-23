b85de3b2f430c53657bca2a79a6e92927e19a96f
Diego Tapia <64498901+dtapia-IT@users.noreply.github.com>
2024-04-23 15:46:56 -0400

    (spec) add role tests

--- spec/hosts/roles/laserrpi_spec.rb
+++ spec/hosts/roles/laserrpi_spec.rb
@@ -37,6 +37,7 @@ describe "#{role} role" do
           include_examples 'docker'
           include_examples 'gpio', os_facts: os_facts
           include_examples 'pigpio'
+          include_examples 'ftdi'
         end # host
       end # lsst_sites
     end # on os

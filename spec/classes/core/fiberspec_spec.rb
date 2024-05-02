8b10bef7d7736cd04efea5093bf54612deed5524
Diego Tapia <64498901+dtapia-IT@users.noreply.github.com>
2024-05-02 13:53:57 -0400

    (spec/fiberspec.rb) add test el9

--- spec/classes/core/fiberspec_spec.rb
+++ spec/classes/core/fiberspec_spec.rb
@@ -4,6 +4,8 @@ require 'spec_helper'
 
 describe 'profile::core::fiberspec' do
   on_supported_os.each do |os, os_facts|
+    next unless os =~ %r{almalinux-9-x86_64}
+
     context "on #{os}" do
       let(:facts) { os_facts }
 

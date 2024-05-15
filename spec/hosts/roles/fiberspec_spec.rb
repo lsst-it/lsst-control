78cdaa0b86be1be6218a31c1bd610af3e4eb0078
Diego Tapia <64498901+dtapia-IT@users.noreply.github.com>
2024-05-15 16:19:04 -0400

    (roles/fiberspec_spec.rb) add role tests

---
+++ spec/hosts/roles/fiberspec_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+role = 'fiberspec'
+
+describe "#{role} role" do
+  on_supported_os.each do |os, os_facts|
+    next unless os =~ %r{almalinux-9-x86_64}
+
+    context "on #{os}" do
+      let(:facts) { os_facts }
+      let(:node_params) do
+        {
+          role: role,
+          site: site,
+        }
+      end
+
+      lsst_sites.each do |site|
+        fqdn = "#{role}.#{site}.lsst.org"
+        override_facts(os_facts, fqdn: fqdn, networking: { fqdn => fqdn })
+
+        describe fqdn, :sitepp do
+          let(:site) { site }
+
+          it { is_expected.to compile.with_all_deps }
+
+          include_examples 'dco'
+          include_examples 'fiberspec'
+        end # host
+      end # lsst_sites
+    end # on os
+  end # on_supported_os
+end # role

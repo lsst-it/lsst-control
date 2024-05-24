8682eff7ca323e014942bf89b89a9926f3d89fd3
Baden Erb <berb@NB400480-1-A.local>
2024-05-24 11:26:24 -0400

    (node/lsstcam-vw05.ls.lsst.org) provision host as EL9 & add test
    (spec) add tests
    
    (node/lsstcam-vw05.ls.lsst.org) provision host as EL9 & add test

---
+++ spec/hosts/nodes/lsstcam-vw05.ls.lsst.org_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'lsstcan-vw05.ls.lsst.org', :sitepp do
+  on_supported_os.each do |os, os_facts|
+    next unless os =~ %r{almalinux-9-x86_64}
+
+    context "on #{os}" do
+      let(:facts) do
+        override_facts(os_facts,
+                       fqdn: 'lsstcam-vw05.ls.lsst.org',
+                       is_virtual: false,
+                       virtual: 'physical',
+                       dmi: {
+                         'product' => {
+                           'name' => 'MS-7E25',
+                         },
+                       })
+      end
+      let(:node_params) do
+        {
+          role: 'ccs-desktop',
+          site: 'ls',
+        }
+      end
+
+      it { is_expected.to compile.with_all_deps }
+
+      include_examples 'baremetal no bmc'
+      include_context 'with nm interface'
+      it { is_expected.to have_nm__connection_resource_count(1) }
+
+      context 'with enp3s0' do
+        let(:interface) { 'enp3s0' }
+
+        it_behaves_like 'nm enabled interface'
+        it_behaves_like 'nm ethernet interface'
+        it_behaves_like 'nm dhcp interface'
+      end
+    end # on os
+  end # on_supported_os
+end

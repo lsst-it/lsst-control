a45cbeb28ddff0875e3515227d80229714165622
Baden Erb <berb@NB400480-1-A.local>
2024-05-27 15:13:53 -0400

    (spec) add tests

---
+++ spec/hosts/nodes/lsstcam-pcs01.cp.lsst.org_spec.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'lsstcam-vac.cp.lsst.org', :sitepp do
+  on_supported_os.each do |os, os_facts|
+    next unless os =~ %r{almalinux-9-x86_64}
+
+    context "on #{os}" do
+      let(:facts) do
+        override_facts(os_facts,
+                       fqdn: 'lsstcam-pcs01.cp.lsst.org',
+                       is_virtual: false,
+                       virtual: 'physical',
+                       dmi: {
+                         'product' => {
+                           'name' => 'UNO-148-B',
+                         },
+                       })
+      end
+      let(:node_params) do
+        {
+          role: 'lsstcam-ccs',
+          site: 'cp',
+        }
+      end
+
+      it { is_expected.to compile.with_all_deps }
+
+      include_examples 'baremetal no bmc'
+      include_context 'with nm interface'
+      it { is_expected.to have_nm__connection_resource_count(2) }
+
+      context 'with enp1s0' do
+        let(:interface) { 'enp1s0' }
+
+        it_behaves_like 'nm enabled interface'
+        it_behaves_like 'nm ethernet interface'
+        it_behaves_like 'nm manual interface'
+        it { expect(nm_keyfile['ipv4']['address1']).to eq('139.229.175.94/26,139.229.175.126') }
+        it { expect(nm_keyfile['ipv4']['dns']).to eq('139.229.160.53;139.229.160.54;139.229.160.55;') }
+        it { expect(nm_keyfile['ipv4']['dns-search']).to eq('cp.lsst.org;') }
+      end
+
+      context 'with enp2s0' do
+        let(:interface) { 'enp2s0' }
+
+        it_behaves_like 'nm enabled interface'
+        it_behaves_like 'nm ethernet interface'
+        it_behaves_like 'nm manual interface'
+        it { expect(nm_keyfile['ipv4']['address1']).to eq('192.168.1.1/24') }
+      end
+    end # on os
+  end # on_supported_os
+end

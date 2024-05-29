a29469da750e668fcb230ee5dada1e2dce2484d7
Diego Tapia <64498901+dtapia-IT@users.noreply.github.com>
2024-05-29 14:17:20 -0400

    (spec) add lsstcam-dc*.cp node tests

--- spec/hosts/nodes/lsstcam-dc02.cp.lsst.org_spec.rb
+++ spec/hosts/nodes/lsstcam-dc02.cp.lsst.org_spec.rb
@@ -23,6 +23,7 @@ describe 'lsstcam-dc02.cp.lsst.org', :sitepp do
           role: 'ccs-dc',
           cluster: 'lsstcam-ccs',
           site: 'cp',
+          variant: 'r6515',
         }
       end
 
@@ -30,7 +31,52 @@ describe 'lsstcam-dc02.cp.lsst.org', :sitepp do
 
       include_examples 'baremetal'
 
+      include_context 'with nm interface'
+
       it { is_expected.not_to contain_service('image-handling') }
+      it { is_expected.to have_nm__connection_resource_count(7) }
+
+      %w[
+        ens1f1np1
+        eno1
+        eno2
+        ens3f1np1
+      ].each do |i|
+        context "with #{i}" do
+          let(:interface) { i }
+
+          it_behaves_like 'nm disabled interface'
+        end
+      end
+
+      context 'with ens1f0np0' do
+        let(:interface) { 'ens1f0np0' }
+
+        it_behaves_like 'nm enabled interface'
+        it_behaves_like 'nm ethernet interface'
+        it { expect(nm_keyfile['ipv4']['address1']).to eq('139.229.175.66/26,139.229.175.126') }
+        it { expect(nm_keyfile['ipv4']['dns']).to eq('139.229.160.53;139.229.160.54;139.229.160.55;') }
+        it { expect(nm_keyfile['ipv4']['dns-search']).to eq('cp.lsst.org;') }
+        it { expect(nm_keyfile['ipv4']['method']).to eq('manual') }
+      end
+
+      context 'with ens3f0np0' do
+        let(:interface) { 'ens3f0np0' }
+
+        it_behaves_like 'nm enabled interface'
+        it_behaves_like 'nm ethernet interface'
+        it_behaves_like 'nm bridge slave interface', master: 'lsst-daq'
+        it { expect(nm_keyfile['ethtool']['ring-rx']).to eq(4096) }
+        it { expect(nm_keyfile['ethtool']['ring-tx']).to eq(4096) }
+      end
+
+      context 'with lsst-daq' do
+        let(:interface) { 'lsst-daq' }
+
+        it_behaves_like 'nm enabled interface'
+        it_behaves_like 'nm dhcp interface'
+        it_behaves_like 'nm bridge interface'
+      end
     end # on os
   end # on_supported_os
 end

0e7b8766a96ebdae1838bae80ac1c276bd5fa1ae
Diego Tapia <64498901+dtapia-IT@users.noreply.github.com>
2024-05-30 11:41:08 -0400

    (spec) add network tests to support EL9 for comcam-lion02

---
+++ spec/hosts/nodes/comcam-lion02.cp.lsst.org_spec.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'comcam-lion02.cp.lsst.org', :sitepp do
+  on_supported_os.each do |os, os_facts|
+    next unless os =~ %r{almalinux-9-x86_64}
+
+    context "on #{os}" do
+      let(:facts) do
+        override_facts(os_facts,
+                       fqdn: 'comcam-lion02.cp.lsst.org',
+                       is_virtual: false,
+                       virtual: 'physical',
+                       dmi: {
+                         'product' => {
+                           'name' => 'Versalogic Corporation',
+                         },
+                       })
+      end
+      let(:node_params) do
+        {
+          cluster: 'comcam-ccs',
+          role: 'ccs-hcu',
+          site: 'cp',
+        }
+      end
+
+      it { is_expected.to compile.with_all_deps }
+
+      include_examples 'baremetal no bmc'
+      include_context 'with nm interface'
+      it { is_expected.to have_nm__connection_resource_count(2) }
+
+      context 'with enp0s31f6' do
+        let(:interface) { 'enp0s31f6' }
+
+        it_behaves_like 'nm enabled interface'
+        it_behaves_like 'nm dhcp interface'
+        it_behaves_like 'nm ethernet interface'
+        it { expect(nm_keyfile['ipv6']['method']).to eq('disabled') }
+      end
+
+      context 'with enp3s0' do
+        let(:interface) { 'enp3s0' }
+
+        it_behaves_like 'nm enabled interface'
+        it_behaves_like 'nm ethernet interface'
+        it { expect(nm_keyfile['ipv4']['address1']).to eq('192.168.1.123/24') }
+        it { expect(nm_keyfile['ipv4']['method']).to eq('manual') }
+        it { expect(nm_keyfile['ipv6']['method']).to eq('disabled') }
+      end
+    end # on os
+  end # on_supported_os
+end

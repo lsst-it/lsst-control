20aafe6c31839daabadfa01d386e0bf9916a7c90
Joshua Hoblitt <josh@hoblitt.com>
2024-06-21 13:52:23 -0700

    (role/rke) bump helm to 3.10.3

--- site/profile/manifests/core/helm.pp
+++ site/profile/manifests/core/helm.pp
@@ -13,6 +13,7 @@ class profile::core::helm (
     '3.7.2' => '4ae30e48966aba5f807a4e140dad6736ee1a392940101e4d79ffb4ee86200a9e',
     '3.6.3' => '07c100849925623dc1913209cd1a30f0a9b80a5b4d6ff2153c609d11b043e262',
     '3.5.4' => 'a8ddb4e30435b5fd45308ecce5eaad676d64a5de9c89660b56face3fe990b318',
+    '3.10.3' => '950439759ece902157cf915b209b8d694e6f675eaab5099fb7894f30eeaee9a2',
     default => undef,
   }
   unless ($rke_checksum) {

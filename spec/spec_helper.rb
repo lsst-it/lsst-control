43f2c5936e96928b61f4a34fb6f522ba2131ef23
Joshua Hoblitt <josh@hoblitt.com>
2024-06-26 11:18:34 -0700

    (spec) mv 'rke profile' shared example to support file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -622,37 +622,6 @@ shared_examples 'foreman' do
   end
 end
 
-shared_examples 'rke profile' do
-  it { is_expected.to contain_vcsrepo('/home/rke/k8s-cookbook') }
-
-  it { is_expected.to contain_kmod__load('br_netfilter') }
-
-  it do
-    is_expected.to contain_sysctl__value('net.bridge.bridge-nf-call-iptables').with(
-      value: 1,
-      target: '/etc/sysctl.d/80-rke.conf',
-    ).that_requires('Kmod::Load[br_netfilter]').that_comes_before('Class[docker]')
-  end
-
-  it do
-    is_expected.to contain_sysctl__value('fs.inotify.max_user_instances').with(
-      value: 104_857,
-      target: '/etc/sysctl.d/80-rke.conf',
-    )
-  end
-
-  it do
-    is_expected.to contain_sysctl__value('fs.inotify.max_user_watches').with(
-      value: 1_048_576,
-      target: '/etc/sysctl.d/80-rke.conf',
-    )
-  end
-
-  it do
-    is_expected.to contain_vcsrepo('/home/rke/k8s-cookbook').that_requires('Class[ipa]')
-  end
-end
-
 shared_examples 'generic foreman' do
   include_examples 'debugutils'
   include_examples 'docker'

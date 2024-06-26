888e8ac37275f5fe46130ac0583092f58b363c8f
Diego Tapia <64498901+dtapia-IT@users.noreply.github.com>
2024-06-26 14:12:32 -0400

    (profile::core::rucio ) add puppet configuration

--- site/profile/manifests/core/rucio.pp
+++ site/profile/manifests/core/rucio.pp
@@ -1,75 +1,35 @@
-# @summary
-#   Install required rucio packages
-#
-class profile::core::rucio () {
-  include profile::core::letsencrypt
-
-  #  Host FQDN
-  $fqdn = $facts[fqdn]
-
-  #  Define XRootD Path
-  $xrootd_path = '/opt/xrootd'
-
-  #  Define Yum Packages
-  $yum_packages = [
-    'gcc-c++',
-    'cmake3',
-    'krb5-devel',
-    'libuuid-devel',
-    'libxml2-devel',
-    'openssl-devel',
-    'systemd-devel',
-    'zlib-devel',
-    'devtoolset-7',
-    'xrootd',
-    'voms',
-  ]
-
-  #  Define PIP Packages
-  $pip_packages = [
-    'wheel',
-    'cryptography',
-    'rucio',
-  ]
-
-  #  Signed Certificate Location
-  $le_root = "/etc/letsencrypt/live/${fqdn}"
-
-  #  Generate and sign certificate
-  letsencrypt::certonly { $fqdn:
-    plugin      => 'dns-route53',
-    manage_cron => true,
+class profile::core::rucio {
+  yumrepo { 'xrootd-stable':
+    descr               => 'XRootD Stable Repository',
+    baseurl             => 'https://xrootd.web.cern.ch/repo/stable/el$releasever/$basearch',
+    skip_if_unavailable => 'true',
+    gpgcheck            => '1',
+    gpgkey              => 'https://xrootd.web.cern.ch/repo/RPM-GPG-KEY.txt',
+    enabled             => '1',
+    target              => '/etc/yum.repo.d/xrootd.repo',
+  }
+  -> package { 'xrootd':
+    ensure => 'installed',
+  }
+  file { [
+    '/lib/systemd/system/xrootd@.service',
+    '/lib/systemd/system/cmsd@.service',
+  ]:
+    ensure => file,
+    mode   => '0644',
+    owner  => 'saluser',
+    group  => 'saluser',
   }
 
-  #  Copy the certificates into /etc/grid-security
-  -> cron::monthly { 'update_cert':
-    command => "/bin/rsync  -a --copy-links  --chown=xrootd:xrootd ${le_root}/cert.pem ${le_root}/chain.pem ${le_root}/fullchain.pem ${le_root}/privkey.pem /etc/grid-security/ /dev/null 2>&1",
-    user    => 'root',
-    hour    => 0,
-    minute  => 0,
-    date    => 1,
-  }
-
-  #  Install Pip3 Packages
-  package { $pip_packages:
-    ensure   => 'present',
-    provider => 'pip3',
-  }
-
-  #  Install Yum Packages
-  package { $yum_packages:
-    ensure   => 'present',
-  }
-  firewalld_port { 'Enable 1094 TCP access to xrootd':
-    ensure   => present,
-    zone     => 'dmz',
-    port     => 1094,
-    protocol => 'tcp',
-  }
-  firewalld_port { 'Enable 1094 UDP access to xrootd':
-    ensure   => present,
-    zone     => 'dmz',
-    port     => 1094,
-    protocol => 'udp',
+  file { [
+    '/etc/xrootd',
+    '/var/log/xrootd',
+    '/var/run/xrootd',
+    '/var/spool/xrootd',
+  ]:
+    ensure => directory,
+    mode   => '0644',
+    owner  => 'saluser',
+    group  => 'saluser',
   }
 }

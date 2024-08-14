157df9cf15a0b580ca7b1e3074a0f5bc7ee71a03
Glenn Morris <gmorris@slac.stanford.edu>
2024-08-14 13:12:23 -0700

    (profile::ccs::monitoring) add option to install mrtg

--- site/profile/manifests/ccs/monitoring.pp
+++ site/profile/manifests/ccs/monitoring.pp
@@ -1,4 +1,16 @@
-class profile::ccs::monitoring {
+# @summary
+#   Monitoring for CCS hosts.
+#
+# @param mrtg
+#   Boolean - if true, install mrtg.
+#
+class profile::ccs::monitoring (
+  Boolean $mrtg = false,
+) {
   ## These will be replaced by some other alerting/monitoring system.
   include ccs_monit
+
+  if ($mrtg) {
+    include ccs_mrtg
+  }
 }

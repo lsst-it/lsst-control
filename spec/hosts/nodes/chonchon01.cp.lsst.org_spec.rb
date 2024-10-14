5d65a1342ea690b879e47e389b6ae5f3f2e24ae0
Carlos Barria <cbarria@yahoo.com>
2024-10-14 12:46:14 -0300

    (role/rke) bump CP client to v1.6.2

--- spec/hosts/nodes/chonchon01.cp.lsst.org_spec.rb
+++ spec/hosts/nodes/chonchon01.cp.lsst.org_spec.rb
@@ -50,8 +50,8 @@ describe 'chonchon01.cp.lsst.org', :sitepp do
 
       it do
         is_expected.to contain_class('rke').with(
-          version: '1.5.12',
-          checksum: 'f0d1f6981edbb4c93f525ee51bc2a8ad729ba33c04f21a95f5fc86af4a7af586'
+          version: '1.6.2',
+          checksum: '68608a97432b4472d3e8f850a7bde0119582ea80fbb9ead923cd831ca97db1d7'
         )
       end
 

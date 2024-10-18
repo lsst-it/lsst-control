2bd7a6b1cca4ec8345a5c7bd730a429f60499791
Glenn Morris <gmorris@slac.stanford.edu>
2024-10-17 17:13:14 -0700

    (spec/ccs_hcu_fb_spec) add basic spec

---
+++ spec/hosts/roles/ccs_hcu_fb_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+role = 'ccs-hcu-fb'
+
+describe "#{role} role" do
+  on_supported_os.each do |os, os_facts|
+    context "on #{os}" do
+      lsst_sites.each do |site|
+        describe "#{role}.#{site}.lsst.org", :sitepp do
+          let(:node_params) do
+            {
+              role:,
+              site:,
+            }
+          end
+          let(:facts) { lsst_override_facts(os_facts) }
+
+          it { is_expected.to compile.with_all_deps }
+
+          include_examples('common', os_facts:, site:)
+          include_examples('ccs common', os_facts:)
+          include_examples('x2go packages', os_facts:)
+          it { is_expected.to contain_class('ccs_hcu') }
+        end # host
+      end # lsst_sites
+    end # on os
+  end # on_supported_os
+end # role

fb9d65db10705c15dc16c24674dc2e83522892fd
Joshua Hoblitt <josh@hoblitt.com>
2025-02-28 11:32:50 -0700

    (node/nfs3.cp) disable remote nfs exports

--- spec/hosts/nodes/nfs3.cp.lsst.org_spec.rb
+++ spec/hosts/nodes/nfs3.cp.lsst.org_spec.rb
@@ -30,40 +30,6 @@ describe 'nfs3.cp.lsst.org', :sitepp do
       include_context 'with nm interface'
 
       it do
-        expect(catalogue.resource('class', 'nfs')[:nfs_v4_export_root_clients]).to include(
-          '139.229.160.0/24(rw,fsid=root,insecure,no_subtree_check,async,root_squash)',
-          '139.229.165.0/24(rw,fsid=root,insecure,no_subtree_check,async,root_squash)',
-          '139.229.180.0/24(rw,fsid=root,insecure,no_subtree_check,async,root_squash)',
-          'azar03.cp.lsst.org(rw,fsid=root,insecure,no_subtree_check,async,root_squash)'
-        )
-      end
-
-      it { is_expected.to contain_class('nfs::server').with_nfs_v4(true) }
-
-      it do
-        expect(catalogue.resource('nfs::server::export', '/comcam')[:clients])
-          .to include(
-            '139.229.160.0/24(rw,nohide,insecure,no_subtree_check,async,root_squash)',
-            '139.229.165.0/24(rw,nohide,insecure,no_subtree_check,async,root_squash)',
-            '139.229.180.0/24(rw,nohide,insecure,no_subtree_check,async,root_squash)',
-            'azar03.cp.lsst.org(rw,nohide,insecure,no_subtree_check,async,root_squash)',
-            'comcam-archiver.cp.lsst.org(rw,nohide,insecure,no_subtree_check,async,no_root_squash)',
-            'comcam-dc01.cp.lsst.org(rw,nohide,insecure,no_subtree_check,async,no_root_squash)',
-            'comcam-vs01.cp.lsst.org(rw,nohide,insecure,no_subtree_check,async,no_root_squash)'
-          )
-      end
-
-      it do
-        expect(catalogue.resource('nfs::server::export', '/lsstcam')[:clients])
-          .to include(
-            '139.229.160.0/24(rw,nohide,insecure,no_subtree_check,async,root_squash)',
-            '139.229.165.0/24(rw,nohide,insecure,no_subtree_check,async,root_squash)',
-            '139.229.180.0/24(rw,nohide,insecure,no_subtree_check,async,root_squash)',
-            'azar03.cp.lsst.org(rw,nohide,insecure,no_subtree_check,async,root_squash)'
-          )
-      end
-
-      it do
         is_expected.to contain_nfs__client__mount('/net/self/comcam').with(
           share: 'comcam',
           server: facts[:networking]['fqdn'],

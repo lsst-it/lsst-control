5196557f09c78f6e1d5010a33a65a6ffadda2c8d
Joshua Hoblitt <josh@hoblitt.com>
2025-03-03 11:49:04 -0700

    (node/bastion1.cp) add missing nfs project & scratch mounts

---
+++ spec/hosts/nodes/bastion1.cp.lsst.org_spec.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'bastion1.cp.lsst.org', :sitepp do
+  on_supported_os.each do |os, os_facts|
+    next if os =~ %r{centos-7-x86_64}
+
+    context "on #{os}" do
+      let(:facts) do
+        lsst_override_facts(os_facts,
+                            is_virtual: true,
+                            virtual: 'kvm',
+                            dmi: {
+                              'product' => {
+                                'name' => 'KVM',
+                              },
+                            })
+      end
+      let(:node_params) do
+        {
+          role: 'bastion',
+          site: 'cp',
+        }
+      end
+
+      it { is_expected.to compile.with_all_deps }
+
+      include_examples 'vm'
+
+      it { is_expected.to contain_class('nfs').with_server_enabled(false) }
+      it { is_expected.to contain_class('nfs').with_client_enabled(true) }
+
+      it do
+        is_expected.to contain_nfs__client__mount('/project').with(
+          share: 'project',
+          server: 'nfs-project.cp.lsst.org',
+          atboot: true
+        )
+      end
+
+      it do
+        is_expected.to contain_nfs__client__mount('/scratch').with(
+          share: 'scratch',
+          server: 'nfs-scratch.cp.lsst.org',
+          atboot: true
+        )
+      end
+    end # on os
+  end # on_supported_os
+end

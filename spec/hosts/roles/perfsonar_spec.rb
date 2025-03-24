4b47c44cacba49a23b89f1291eeb4423ad5fb13d
Julio Constanzo <julio.constanzo@gmail.com>
2025-03-24 17:15:22 -0300

    (role/perfsonar) update toolkit config

--- spec/hosts/roles/perfsonar_spec.rb
+++ spec/hosts/roles/perfsonar_spec.rb
@@ -6,7 +6,7 @@ role = 'perfsonar'
 
 describe "#{role} role" do
   on_supported_os.each do |os, os_facts|
-    next if os =~ %r{almalinux-8-x86_64}
+    next unless os =~ %r{almalinux-9-x86_64}
 
     context "on #{os}" do
       lsst_sites.each do |site|
@@ -20,7 +20,7 @@ describe "#{role} role" do
           let(:facts) { lsst_override_facts(os_facts) }
 
           let(:le_root) { "/etc/letsencrypt/live/#{facts[:networking]['fqdn']}" }
-          let(:perfsonar_version) { '5.0.8' }
+          let(:perfsonar_version) { '5.1.4' }
 
           it { is_expected.to compile.with_all_deps }
 
@@ -33,6 +33,22 @@ describe "#{role} role" do
           it do
             is_expected.to contain_yum__versionlock('perfsonar-toolkit').with(
               version: perfsonar_version
+            )
+          end
+
+          it do
+            is_expected.to contain_file('/etc/perfsonar/psconfig/pscheduler-agent.json').with(
+              ensure: 'file',
+              content: <<~JSON
+                {
+                  "remotes": [
+                    {
+                      "url": "https://ps-cma.amlight.net/psconfig/lhn-psconfig.json",
+                      "configure-archives": true
+                    }
+                  ]
+                }
+              JSON
             )
           end
 

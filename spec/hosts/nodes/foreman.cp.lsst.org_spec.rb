7277e593e76d2421a13eb13245a8fb849e31207f
Joshua Hoblitt <josh@hoblitt.com>
2025-04-28 10:13:08 -0700

    (foreman.cp) change ELQUI|139.229.181.0/27|vlan1801 mask to /26

--- spec/hosts/nodes/foreman.cp.lsst.org_spec.rb
+++ spec/hosts/nodes/foreman.cp.lsst.org_spec.rb
@@ -264,8 +264,8 @@ describe 'foreman.cp.lsst.org', :sitepp do
       it do
         is_expected.to contain_dhcp__pool('ELQUI').with(
           network: '139.229.181.0',
-          mask: '255.255.255.224',
-          range: ['139.229.181.27 139.229.181.27'],
+          mask: '255.255.255.192',
+          range: ['139.229.181.50 139.229.181.59'],
           gateway: '139.229.181.30'
         )
       end

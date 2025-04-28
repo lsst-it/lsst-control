e1161ff2b8568c4224e98e6cdfff3c2f5d4d4abf
Cristian Silva <cristiansilva@gmail.com>
2025-04-28 16:00:42 -0400

    (pukem) fix rke version

--- spec/hosts/nodes/pukem04.dev.lsst.org_spec.rb
+++ spec/hosts/nodes/pukem04.dev.lsst.org_spec.rb
@@ -55,7 +55,7 @@ describe 'pukem04.dev.lsst.org', :sitepp do
         is_expected.to contain_class('rke2').with(
           node_type: 'agent',
           release_series: '1.31',
-          version: '1.31.6~rke2r1'
+          version: '1.31.7~rke2r1'
         )
       end
 

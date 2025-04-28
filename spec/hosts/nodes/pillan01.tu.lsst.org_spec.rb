729b5c6d7cb5cd07f8ca6dd639ca7ddfa24c3f68
dtapiacl <dtapiagonzalez.cl@gmail.com>
2025-04-28 12:13:26 -0400

    (role/rke) bump LS client to v1.7.6

--- spec/hosts/nodes/pillan01.tu.lsst.org_spec.rb
+++ spec/hosts/nodes/pillan01.tu.lsst.org_spec.rb
@@ -55,8 +55,8 @@ describe 'pillan01.tu.lsst.org', :sitepp do
       it do
         is_expected.to contain_class('rke2').with(
           node_type: 'server',
-          release_series: '1.30',
-          version: '1.30.7~rke2r1'
+          release_series: '1.31',
+          version: '1.31.7~rke2r1'
         )
       end
 

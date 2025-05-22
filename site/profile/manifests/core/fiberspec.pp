bc41d6bcd04b13c8029973d1d0c2c209441352eb
dtapiacl <dtapiagonzalez.cl@gmail.com>
2025-05-22 14:58:35 -0400

    (role/fiberspec) add udev rule symlink

--- site/profile/manifests/core/fiberspec.pp
+++ site/profile/manifests/core/fiberspec.pp
@@ -4,7 +4,7 @@
 class profile::core::fiberspec {
   systemd::udev::rule { 'fiberspec.rules':
     rules => [
-      "SUBSYSTEM==\"usb\", ATTR{idVendor}==\"1992\", ATTR{idProduct}==\"0667\", ACTION==\"add\", GROUP=\"saluser\", MODE=\"0664\"",
+      "SUBSYSTEM==\"usb\", ATTR{idVendor}==\"1992\", ATTR{idProduct}==\"0667\", ACTION==\"add\", GROUP=\"saluser\", MODE=\"0664\", SYMLINK+=\"fiberspec\"",
     ],
   }
 }

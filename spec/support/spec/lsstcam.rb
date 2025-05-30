f28ee7989b09dc24671d6c85f8f905ea903ac311
Joshua Hoblitt <josh@hoblitt.com>
2025-05-30 13:35:02 -0700

    (site/cp/cluster/lsstcam-ccs) add s3daemon|s3nd sdfembs3-lsstcam-test*

--- spec/support/spec/lsstcam.rb
+++ spec/support/spec/lsstcam.rb
@@ -70,4 +70,40 @@ shared_examples 'lsstcam-dc.cp' do
       }
     )
   end
+
+  it do
+    is_expected.to contain_s3daemon__instance('sdfembs3-lsstcam-test').with(
+      image: 'ghcr.io/lsst-dm/s3daemon:sha-57e1aa9',
+      volumes: [
+        '/data:/data',
+        '/home:/home',
+      ],
+      env: {
+        'S3_ENDPOINT_URL' => 'https://sdfembs3.sdf.slac.stanford.edu',
+        'S3DAEMON_PORT' => 15_590,
+        'S3DAEMON_HOST' => '0.0.0.0',
+      }
+    )
+  end
+
+  it do
+    is_expected.to contain_s3daemon__instance('sdfembs3-lsstcam-test-s3nd').with(
+      image: 'ghcr.io/lsst-dm/s3nd:1.0.1',
+      volumes: [
+        '/data:/data',
+        '/home:/home',
+      ],
+      env: {
+        'S3ND_ENDPOINT_URL' => 'https://sdfembs3.sdf.slac.stanford.edu',
+        'S3ND_PORT' => 15_591,
+        'S3ND_HOST' => '',
+        'S3ND_UPLOAD_TIMEOUT' => '10s',
+        'S3ND_QUEUE_TIMEOUT' => '30s',
+        'S3ND_UPLOAD_MAX_PARALLEL' => '25',
+        'S3ND_UPLOAD_TRIES' => '5',
+        'S3ND_UPLOAD_PARTSIZE' => '50Mi',  # larger than test file size
+        'S3ND_UPLOAD_BWLIMIT' => '160Mi',
+      }
+    )
+  end
 end

73b3b4f79d7a67f2d332e37146a8f645d2c21703
dtapiacl <dtapiagonzalez.cl@gmail.com>
2025-06-02 10:15:16 -0400

    (role/rke/rke2) bump CP rke client v1.7.7 and rke2 k8s v1.31.8

--- spec/hosts/nodes/elqui06.cp.lsst.org_spec.rb
+++ spec/hosts/nodes/elqui06.cp.lsst.org_spec.rb
@@ -69,7 +69,7 @@ describe 'elqui06.cp.lsst.org', :sitepp do
         is_expected.to contain_class('rke2').with(
           node_type: 'agent',
           release_series: '1.31',
-          version: '1.31.7~rke2r1'
+          version: '1.31.8~rke2r1'
         )
       end
 
--- spec/hosts/nodes/rancher01.cp.lsst.org_spec.rb
+++ spec/hosts/nodes/rancher01.cp.lsst.org_spec.rb
@@ -31,7 +31,7 @@ describe 'rancher01.cp.lsst.org', :sitepp do
         is_expected.to contain_class('rke2').with(
           node_type: 'server',
           release_series: '1.31',
-          version: '1.31.7~rke2r1'
+          version: '1.31.8~rke2r1'
         )
       end
 

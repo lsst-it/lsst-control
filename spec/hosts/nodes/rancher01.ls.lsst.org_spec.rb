2d2b9fa202de73bc11dd84c66e28e638352c0a00
Carlos Barria <cbarria@yahoo.com>
2025-06-06 11:13:39 -0400

    (cluster/rancher) migrate rancher.ls RKE2

--- spec/hosts/nodes/rancher01.ls.lsst.org_spec.rb
+++ spec/hosts/nodes/rancher01.ls.lsst.org_spec.rb
@@ -19,7 +19,7 @@ describe 'rancher01.ls.lsst.org', :sitepp do
       end
       let(:node_params) do
         {
-          role: 'rke',
+          role: 'rke2server',
           site: 'ls',
           cluster: 'rancher',
         }
@@ -27,38 +27,16 @@ describe 'rancher01.ls.lsst.org', :sitepp do
 
       it { is_expected.to compile.with_all_deps }
 
-      include_examples 'docker', docker_version: '24.0.9'
-
       it do
-        is_expected.to contain_class('profile::core::rke').with(
-          version: '1.7.7'
+        is_expected.to contain_class('rke2').with(
+          node_type: 'server',
+          release_series: '1.31',
+          version: '1.31.8~rke2r1'
         )
       end
 
       include_examples 'vm'
-      include_context 'with nm interface'
-      it do
-        is_expected.to contain_class('profile::core::sysctl::rp_filter').with_enable(false)
-      end
-
-      it { is_expected.to have_nm__connection_resource_count(2) }
-
-      context 'with enp1s0' do
-        let(:interface) { 'enp1s0' }
-
-        it_behaves_like 'nm enabled interface'
-        it_behaves_like 'nm ethernet interface'
-        it_behaves_like 'nm dhcp interface'
-      end
-
-      context 'with enp2s0' do
-        let(:interface) { 'enp2s0' }
-
-        it_behaves_like 'nm enabled interface'
-        it_behaves_like 'nm ethernet interface'
-        it { expect(nm_keyfile['ipv4']['method']).to eq('disabled') }
-        it { expect(nm_keyfile['ipv6']['method']).to eq('disabled') }
-      end
+      it { is_expected.to have_nm__connection_resource_count(0) }
     end # on os
   end # on_supported_os
 end # role

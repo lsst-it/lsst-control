6065d1e214b90905d954e3b0cc4ea7580d858261
Baden Erb <baden.erb@gmail.com>
2025-06-10 15:24:14 -0400

    (role/hexrot) Update hexrot role with bump

--- spec/hosts/roles/hexrot_spec.rb
+++ spec/hosts/roles/hexrot_spec.rb
@@ -91,11 +91,11 @@ describe "#{role} role" do
             },
             'ts-mtdomecom' => {
               'channel' => 'lsstts',
-              'version' => '0.2.11',
+              'version' => '0.2.12',
             },
             'ts-mtdomegui' => {
               'channel' => 'lsstts',
-              'version' => '0.4.11',
+              'version' => '0.4.12',
             },
             'ts-rotgui' => {
               'channel' => 'lsstts',

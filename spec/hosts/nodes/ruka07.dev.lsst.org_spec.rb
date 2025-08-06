c8791bea26845e6a4bcf5457c17fd116941aa8e0
dtapiacl <dtapiagonzalez.cl@gmail.com>
2025-08-06 16:45:16 -0400

    (rke2) bump dev rke2 k8s 1.31.9~rke2r1

--- spec/hosts/nodes/ruka07.dev.lsst.org_spec.rb
+++ spec/hosts/nodes/ruka07.dev.lsst.org_spec.rb
@@ -72,7 +72,7 @@ describe 'ruka07.dev.lsst.org', :sitepp do
         is_expected.to contain_class('rke2').with(
           node_type: 'agent',
           release_series: '1.31',
-          version: '1.31.8~rke2r1'
+          version: '1.31.9~rke2r1'
         )
       end
 

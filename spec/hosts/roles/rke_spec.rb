512a6f9d652844e274abb6ea960ec3a6aad76eff
dtapiacl <dtapiagonzalez.cl@gmail.com>
2025-09-24 15:13:37 -0300

    (role/rke/rke2) bump LS client to v1.8.0 / rke2 k8s 1.32.2~rke2r1

--- spec/hosts/roles/rke_spec.rb
+++ spec/hosts/roles/rke_spec.rb
@@ -43,11 +43,21 @@ shared_examples 'generic rke' do |os_facts:, site:|
     )
   end
 
-  it do
-    is_expected.to contain_class('rke').with(
-      version: '1.7.8',
-      checksum: '9494448f684ab0f3f79c62aa9736cd718743ad94e78a291f49eafec8bc71abde'
-    )
+  case site
+  when 'ls'
+    it do
+      is_expected.to contain_class('rke').with(
+        version: '1.8.0',
+        checksum: '8815da0452ae14a45566b534c48a2af6286ee73f800208ba6ec59188cb9a8d25'
+      )
+    end
+  else
+    it do
+      is_expected.to contain_class('rke').with(
+        version: '1.7.8',
+        checksum: '9494448f684ab0f3f79c62aa9736cd718743ad94e78a291f49eafec8bc71abde'
+      )
+    end
   end
 
   it do

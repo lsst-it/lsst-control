024efce4366f92e937c633d975669bbb22f92c08
Joshua Hoblitt <josh@hoblitt.com>
2025-10-15 09:11:22 -0700

    (role/foreman) bump lsst/smee to 3.0.0

--- site/profile/manifests/core/foreman.pp
+++ site/profile/manifests/core/foreman.pp
@@ -84,8 +84,6 @@ class profile::core::foreman (
     }
   }
 
-  # smee has some startup problems due to too new (?) nodejs on EL8
-  # https://gist.github.com/bastelfreak/6d0d3bc324633f04c5b1fc432e871192
   if $manage_smee {
     assert_type(Stdlib::HTTPSUrl, $smee_url)
     class { 'smee':
@@ -96,7 +94,7 @@ class profile::core::foreman (
     cron { 'smee':
       command => '/usr/bin/systemctl restart smee > /dev/null 2>&1',
       user    => 'root',
-      hour    => 4,
+      hour    => '*',
       minute  => 42,
     }
   }

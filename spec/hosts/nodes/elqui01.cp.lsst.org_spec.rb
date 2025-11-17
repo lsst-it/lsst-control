14e32af403f921eacc1653152d64a66e505e2678
Baden Erb <baden.erb@gmail.com>
2025-11-17 15:52:39 -0300

    (rke2) bump CP rke2 k8s 1.32.9~rke2r1

--- spec/hosts/nodes/elqui01.cp.lsst.org_spec.rb
+++ spec/hosts/nodes/elqui01.cp.lsst.org_spec.rb
@@ -57,7 +57,7 @@ describe 'elqui01.cp.lsst.org', :sitepp do
         is_expected.to contain_class('rke2').with(
           node_type: 'server',
           release_series: '1.32',
-          version: '1.32.2~rke2r1'
+          version: '1.32.9~rke2r1'
         )
       end
 

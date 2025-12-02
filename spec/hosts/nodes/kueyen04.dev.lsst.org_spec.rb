b9767d048081081fe3fbe12cc0737aa5389bd1fe
Gonzalo Seriche Vega <gseriche@users.noreply.github.com>
2025-12-02 16:16:12 -0300

    (kueyen) Add RKE2 roles to kueyen

--- spec/hosts/nodes/kueyen04.dev.lsst.org_spec.rb
+++ spec/hosts/nodes/kueyen04.dev.lsst.org_spec.rb
@@ -58,7 +58,7 @@ describe 'kueyen04.dev.lsst.org', :sitepp do
 
       it do
         is_expected.to contain_class('rke2').with(
-          node_type: 'server',
+          node_type: 'agent',
           release_series: '1.32',
           version: '1.32.10~rke2r1'
         )

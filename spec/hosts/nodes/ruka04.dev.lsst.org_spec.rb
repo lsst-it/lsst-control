f4492c7277756b891fa747e20f1121ef750d45ca
Baden Erb <baden.erb@gmail.com>
2026-01-12 20:13:37 -0300

    (rke2) bump dev rke2 k8s 1.32.11~rke2r1

--- spec/hosts/nodes/ruka04.dev.lsst.org_spec.rb
+++ spec/hosts/nodes/ruka04.dev.lsst.org_spec.rb
@@ -64,7 +64,7 @@ describe 'ruka04.dev.lsst.org', :sitepp do
         is_expected.to contain_class('rke2').with(
           node_type: 'server',
           release_series: '1.32',
-          version: '1.32.10~rke2r1'
+          version: '1.32.11~rke2r1'
         )
       end
 

2797df0e556f2277ef0bdc35dbb1635755ec82a5
dtapiacl <dtapiagonzalez.cl@gmail.com>
2026-02-16 10:30:25 -0300

    (node/ovpn.cp) add outside vpn cp host

---
+++ spec/hosts/nodes/ovpn.cp.lsst.org_spec.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'ovpn.cp.lsst.org', :sitepp do
+  on_supported_os.each do |os, os_facts|
+    next unless os =~ %r{almalinux-9-x86_64}
+
+    context "on #{os}" do
+      let(:facts) do
+        lsst_override_facts(os_facts,
+                            is_virtual: false,
+                            virtual: 'physical',
+                            dmi: {
+                              'product' => {
+                                'name' => 'AS -1115HS-TNR',
+                              },
+                            })
+      end
+
+      let(:node_params) do
+        {
+          role: 'openvpnas',
+          site: 'cp',
+        }
+      end
+
+      it { is_expected.to compile.with_all_deps }
+
+      include_examples 'baremetal'
+      include_context 'with nm interface'
+
+      it { is_expected.to have_nm__connection_resource_count(3) }
+
+      %w[
+        enp65s0f1
+        enp12s0f4u1u2c2
+      ].each do |i|
+        context "with #{i}" do
+          let(:interface) { i }
+
+          it_behaves_like 'nm disabled interface'
+        end
+      end
+
+      context 'with enp65s0f0' do
+        let(:interface) { 'enp65s0f0' }
+
+        it_behaves_like 'nm enabled interface'
+        it_behaves_like 'nm ethernet interface'
+        it_behaves_like 'nm manual interface'
+        it { expect(nm_keyfile['ipv4']['address1']).to eq('139.229.160.126/24,139.229.160.254') }
+        it { expect(nm_keyfile['ipv4']['dns']).to eq('139.229.160.53;139.229.160.54;139.229.160.55;') }
+        it { expect(nm_keyfile['ipv4']['dns-search']).to eq('cp.lsst.org;') }
+      end
+    end # on os
+  end # on_supported_os
+end

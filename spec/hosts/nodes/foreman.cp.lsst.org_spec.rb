3c59c4df748bd41753df492ac7b9a0deb7dc8558
Joshua Hoblitt <josh@hoblitt.com>
2026-02-19 09:38:29 -0700

    (node/foreman.cp) change fqdn interface name to enp1s0; convert to nm

--- spec/hosts/nodes/foreman.cp.lsst.org_spec.rb
+++ spec/hosts/nodes/foreman.cp.lsst.org_spec.rb
@@ -4,8 +4,7 @@ require 'spec_helper'
 
 describe 'foreman.cp.lsst.org', :sitepp do
   on_supported_os.each do |os, os_facts|
-    # XXX networking needs to be updated to support EL8+
-    next unless os =~ %r{centos-7-x86_64}
+    next unless os =~ %r{almalinux-9-x86_64}
 
     context "on #{os}" do
       let(:facts) do
@@ -41,19 +40,26 @@ describe 'foreman.cp.lsst.org', :sitepp do
       end
       let(:dhcp_interfaces) do
         %w[
-          eth0
+          enp1s0
         ]
       end
+
+      include_context 'with nm interface'
 
       it { is_expected.to compile.with_all_deps }
 
       include_examples 'vm'
       include_examples 'dhcp server'
 
-      it do
-        is_expected.to contain_network__interface('eth0').with(
-          ipaddress: '139.229.160.5'
-        )
+      context 'with enp1s0' do
+        let(:interface) { 'enp1s0' }
+
+        it_behaves_like 'nm enabled interface'
+        it_behaves_like 'nm ethernet interface'
+        it { expect(nm_keyfile['ipv4']['address1']).to eq('139.229.160.5/24,139.229.160.254') }
+        it { expect(nm_keyfile['ipv4']['dns']).to eq('139.229.160.53;139.229.160.54;139.229.160.55;') }
+        it { expect(nm_keyfile['ipv4']['dns-search']).to eq('cp.lsst.org;') }
+        it { expect(nm_keyfile['ipv4']['method']).to eq('manual') }
       end
 
       it do

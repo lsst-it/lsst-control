7b3928b6b30c62fc2bbf79a4b48715b89bed9307
Baden Erb <baden.erb@gmail.com>
2026-03-16 09:25:59 -0300

    (rke2) bump dev rke2 k8s 1.33.4~rke2r1

--- spec/hosts/nodes/pukem01.dev.lsst.org_spec.rb
+++ spec/hosts/nodes/pukem01.dev.lsst.org_spec.rb
@@ -55,7 +55,7 @@ describe 'pukem01.dev.lsst.org', :sitepp do
         is_expected.to contain_class('rke2').with(
           node_type: 'server',
           release_series: '1.33',
-          version: '1.33.0~rke2r1',
+          version: '1.33.4~rke2r1',
         )
       end
 

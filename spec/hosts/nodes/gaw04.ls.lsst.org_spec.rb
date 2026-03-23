c2c74c37a624f850de91c26eeac3aa51c19485f1
Baden Erb <baden.erb@gmail.com>
2026-03-23 11:47:21 -0300

    (rke2) bump ls rke2 k8s 1.33.4~rke2r1

--- spec/hosts/nodes/gaw04.ls.lsst.org_spec.rb
+++ spec/hosts/nodes/gaw04.ls.lsst.org_spec.rb
@@ -55,7 +55,7 @@ describe 'gaw04.ls.lsst.org', :sitepp do
         is_expected.to contain_class('rke2').with(
           node_type: 'agent',
           release_series: '1.33',
-          version: '1.33.0~rke2r1',
+          version: '1.33.4~rke2r1',
         )
       end
 

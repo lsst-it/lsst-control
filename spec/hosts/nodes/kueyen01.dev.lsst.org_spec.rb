902363132d6c56743a9af177a8de885db63fe332
Baden Erb <baden.erb@gmail.com>
2026-03-24 12:38:57 -0300

    (rke2) upgrade kueyen to rke2

--- spec/hosts/nodes/kueyen01.dev.lsst.org_spec.rb
+++ spec/hosts/nodes/kueyen01.dev.lsst.org_spec.rb
@@ -2,7 +2,7 @@
 
 require 'spec_helper'
 
-describe 'kueyen01.dev.lsst.org', :sitepp do
+describe 'kueyen01.cp.lsst.org', :sitepp do
   on_supported_os.each do |os, os_facts|
     next unless os =~ %r{almalinux-9-x86_64}
 
@@ -13,13 +13,13 @@ describe 'kueyen01.dev.lsst.org', :sitepp do
                             virtual: 'physical',
                             dmi: {
                               'product' => {
-                                'name' => 'PowerEdge R440',
+                                'name' => 'PowerEdge C6420',
                               },
                             })
       end
       let(:node_params) do
         {
-          role: 'rke',
+          role: 'rke2server',
           site: 'dev',
           cluster: 'kueyen',
         }
@@ -27,81 +27,92 @@ describe 'kueyen01.dev.lsst.org', :sitepp do
 
       it { is_expected.to compile.with_all_deps }
 
-      it_behaves_like 'docker', docker_version: '25.0.3'
       it_behaves_like 'baremetal'
-      it_behaves_like 'ceph cluster'
       include_context 'with nm interface'
 
-      it do
-        is_expected.to contain_class('profile::core::sysctl::rp_filter').with_enable(false)
-      end
+      it { is_expected.to contain_class('tuned').with_active_profile('latency-performance') }
 
       it do
         is_expected.to contain_class('clustershell').with(
           groupmembers: {
             'kueyen' => {
               'group' => 'kueyen',
-              'member' => [
-                'kueyen[01-03]',
-                'kueyen[04-09]',
-              ],
+              'member' => ['kueyen[01-04]'],
             },
           },
         )
       end
 
       it do
-        is_expected.to contain_class('rke').with(
-          version: '1.8.0',
+        is_expected.to contain_class('rke2').with(
+          node_type: 'server',
+          release_series: '1.34',
+          version: '1.34.7~rke2r1',
         )
       end
 
       it do
-        is_expected.to contain_class('cni::plugins').with(
-          version: '1.2.0',
-          checksum: 'f3a841324845ca6bf0d4091b4fc7f97e18a623172158b72fc3fdcdb9d42d2d37',
-          enable: %w[macvlan static],
+        expect(catalogue.resource('class', 'nm')[:conf]).to include(
+          'device' => {
+            'keep-configuration' => 'no',
+            'allowed-connections' => 'except:origin:nm-initrd-generator',
+          },
         )
       end
 
-      it { is_expected.to contain_class('cni::plugins::dhcp') }
-
       it { is_expected.to have_nm__connection_resource_count(6) }
 
+      context 'with eno16' do
+        let(:interface) { 'eno16' }
+
+        it_behaves_like 'nm disabled interface'
+      end
+
       %w[
-        em1
-        em2
-        ens2f0
+        ens4f0
+        ens4f1
       ].each do |i|
         context "with #{i}" do
           let(:interface) { i }
 
-          it_behaves_like 'nm disabled interface'
+          it_behaves_like 'nm named interface'
+          it_behaves_like 'nm ethernet interface'
+          it_behaves_like 'nm no-ip interface'
+          it { expect(nm_keyfile['connection']['master']).to eq('bond0') }
+          it { expect(nm_keyfile['connection']['slave-type']).to eq('bond') }
+          it { expect(nm_keyfile_raw).to match(%r{^\[ethernet\]$}) }
+          it { expect(nm_keyfile_raw).to match(%r{^\[ipv4\]$}) }
+          it { expect(nm_keyfile_raw).to match(%r{^\[ipv6\]$}) }
         end
       end
 
-      context 'with ens2f1' do
-        let(:interface) { 'ens2f1' }
+      context 'with bond0' do
+        let(:interface) { 'bond0' }
 
-        it_behaves_like 'nm enabled interface'
-        it_behaves_like 'nm dhcp interface'
-        it_behaves_like 'nm ethernet interface'
+        it_behaves_like 'nm named interface'
+        it_behaves_like 'nm no-ip interface'
+        it { expect(nm_keyfile['connection']['type']).to eq('bond') }
+        it { expect(nm_keyfile['bond']['miimon']).to eq(100) }
+        it { expect(nm_keyfile['bond']['mode']).to eq('802.3ad') }
+        it { expect(nm_keyfile['bond']['xmit_hash_policy']).to eq('layer3+4') }
+        it { expect(nm_keyfile_raw).to match(%r{^\[ethernet\]$}) }
+        it { expect(nm_keyfile_raw).not_to match(%r{^\[proxy\]$}) }
       end
 
-      context 'with ens2f0.2301' do
-        let(:interface) { 'ens2f0.2301' }
+      context 'with bond0.2101' do
+        let(:interface) { 'bond0.2101' }
 
         it_behaves_like 'nm enabled interface'
-        it_behaves_like 'nm vlan interface', id: 2301, parent: 'ens2f0'
-        it_behaves_like 'nm bridge slave interface', master: 'br2301'
+        it_behaves_like 'nm vlan interface', id: 2101, parent: 'bond0'
+        it_behaves_like 'nm bridge slave interface', master: 'br2101'
       end
 
-      context 'with br2301' do
-        let(:interface) { 'br2301' }
+      context 'with br2101' do
+        let(:interface) { 'br2101' }
 
         it_behaves_like 'nm enabled interface'
-        it_behaves_like 'nm dhcp interface'
         it_behaves_like 'nm bridge interface'
+        it_behaves_like 'nm dhcp interface'
       end
     end # on os
   end # on_supported_os

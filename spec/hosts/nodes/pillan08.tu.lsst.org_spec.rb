ed5d9795eaf06e0116505c7ec69946dc7c195304
igonzalezcl <69816993+igonzalezcl@users.noreply.github.com>
2026-05-07 09:19:29 -0400

    (role/rke2) bump tu rke2 k8s 1.34.7~rke2r1

--- spec/hosts/nodes/pillan08.tu.lsst.org_spec.rb
+++ spec/hosts/nodes/pillan08.tu.lsst.org_spec.rb
@@ -59,8 +59,8 @@ describe 'pillan08.tu.lsst.org', :sitepp do
       it do
         is_expected.to contain_class('rke2').with(
           node_type: 'agent',
-          release_series: '1.33',
-          version: '1.33.4~rke2r1',
+          release_series: '1.34',
+          version: '1.34.7~rke2r1',
         )
       end
 

da889fc7be8e08cdc8d84e4ecec7cce3fe879d95
Baden Erb <baden.erb@gmail.com>
2026-05-15 13:02:15 -0400

    (lsstcam-turbotest.cp) add host and test

---
+++ spec/hosts/nodes/lsstcam-turbotest.cp.lsst.org_spec.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'lsstcam-turbotest.cp.lsst.org', :sitepp do
+  on_supported_os.each do |os, os_facts|
+    next unless os =~ %r{almalinux-9-x86_64}
+
+    context "on #{os}" do
+      let(:facts) do
+        lsst_override_facts(os_facts,
+                            is_virtual: false,
+                            virtual: 'physical',
+                            dmi: {
+                              'product' => {
+                                'name' => 'EPMe-42',
+                              },
+                            })
+      end
+      let(:node_params) do
+        {
+          role: 'ccs-hcu',
+          site: 'cp',
+        }
+      end
+
+      it { is_expected.to compile.with_all_deps }
+
+      it_behaves_like 'baremetal no bmc'
+      include_context 'with nm interface'
+
+      it { is_expected.to have_nm__connection_resource_count(2) }
+
+      context 'with enp3s0' do
+        let(:interface) { 'enp3s0' }
+
+        it_behaves_like 'nm disabled interface'
+      end
+
+      context 'with enp0s31f6' do
+        let(:interface) { 'enp0s31f6' }
+
+        it_behaves_like 'nm enabled interface'
+        it_behaves_like 'nm dhcp interface'
+        it_behaves_like 'nm ethernet interface'
+      end
+    end # on os
+  end # on_supported_os
+end

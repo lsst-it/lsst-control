4f9dcfdcb7bd2d9b447d450a4814a0972b04f85f
igonzalezcl <69816993+igonzalezcl@users.noreply.github.com>
2026-06-08 09:18:13 -0400

    (role/rke2) bump dev rke2 k8s 1.35.1~rke2r1

--- spec/hosts/nodes/kueyen01.dev.lsst.org_spec.rb
+++ spec/hosts/nodes/kueyen01.dev.lsst.org_spec.rb
@@ -53,8 +53,8 @@ describe 'kueyen01.cp.lsst.org', :sitepp do
       it do
         is_expected.to contain_class('rke2').with(
           node_type: 'server',
-          release_series: '1.34',
-          version: '1.34.7~rke2r1',
+          release_series: '1.35',
+          version: '1.35.1~rke2r1',
         )
       end
 
--- spec/hosts/nodes/ruka01.dev.lsst.org_spec.rb
+++ spec/hosts/nodes/ruka01.dev.lsst.org_spec.rb
@@ -63,8 +63,8 @@ describe 'ruka01.dev.lsst.org', :sitepp do
       it do
         is_expected.to contain_class('rke2').with(
           node_type: 'server',
-          release_series: '1.34',
-          version: '1.34.7~rke2r1',
+          release_series: '1.35',
+          version: '1.35.1~rke2r1',
         )
       end
 

b2b17cc664635972d7fb61ee05d314930f0cb7c0
Baden Erb <baden.erb@gmail.com>
2026-06-17 10:32:14 -0400

    (role/rke2) bump ls rke2 k8s 1.35.1~rke2r1

--- spec/hosts/nodes/machi06.ls.lsst.org_spec.rb
+++ spec/hosts/nodes/machi06.ls.lsst.org_spec.rb
@@ -62,8 +62,8 @@ describe 'machi06.ls.lsst.org', :sitepp do
       it do
         is_expected.to contain_class('rke2').with(
           node_type: 'agent',
-          release_series: '1.34',
-          version: '1.34.7~rke2r1',
+          release_series: '1.35',
+          version: '1.35.1~rke2r1',
         )
       end
 
